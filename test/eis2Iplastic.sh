0244432b18d8b82e932891f0e5813b8e625ed43c
Ed Bueler <elbueler@alaska.edu>
2007-11-14 23:39:32 +0000

    Yet more work on EIS II exper I and superposed plastic till SSA.

--- test/eis2Iplastic.sh
+++ test/eis2Iplastic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# script to play with EISMINT II experiment I and plastic till; for rev 195 and later
+# script to play with EISMINT II experiment I and plastic till SSA and superposition
 
-NN=2
+NN=8
 
-#mpiexec -n $NN pisms -eisII I -Mx 61 -My 61 -Mz 201 -y 100000 -o eis2I100k
+mpiexec -n $NN pisms -eisII I -Mx 61 -My 61 -Mz 201 -y 100000 -o eis2I100k
 
-#mpiexec -n $NN pisms -eisII I -if eis2I100k.nc -y 80000 -o eis2I180k
+mpiexec -n $NN pisms -eisII I -if eis2I100k.nc -y 80000 -o eis2I180k
 
 mpiexec -n $NN pisms -eisII I -if eis2I180k.nc -y 10000 -track_Hmelt -f3d -o eis2I190k
 
@@ -13,14 +13,14 @@ mpiexec -n $NN pisms -eisII I -Mx 121 -My 121 -Mz 301 -regrid eis2I190k.nc -regr
    -y 10000 -track_Hmelt -f3d -o eis2I_fine_wmelt
 
 mpiexec -n $NN pisms -eisII I -if eis2I_fine_wmelt.nc -ssa -plastic -super -verbose \
-   -till_phi 20.0,5.0 -y 5 -f3d -o eis2I_plastic5
+    -track_Hmelt -till_phi 20.0,5.0 -y 5 -f3d -o eis2I_plastic5
 
 mpiexec -n $NN pisms -eisII I -if eis2I_plastic5.nc -ssa -plastic -super -verbose \
-   -till_phi 20.0,5.0 -y 5 -f3d -o eis2I_plastic10
+    -track_Hmelt -till_phi 20.0,5.0 -y 5 -f3d -o eis2I_plastic10
 
 mpiexec -n $NN pisms -eisII I -if eis2I_plastic10.nc -ssa -plastic -super -verbose \
-   -till_phi 20.0,5.0 -y 10 -f3d -o eis2I_plastic20
+    -track_Hmelt -till_phi 20.0,5.0 -y 10 -f3d -o eis2I_plastic20
 
 mpiexec -n $NN pisms -eisII I -if eis2I_plastic20.nc -ssa -plastic -super -verbose \
-   -till_phi 20.0,5.0 -y 80 -f3d -o eis2I_plastic100
+    -track_Hmelt -till_phi 20.0,5.0 -y 80 -f3d -o eis2I_plastic100
 
