d4906d13ee141f819e49b3db9b2375c0e13fb9a8
Ed Bueler <elbueler@alaska.edu>
2009-02-27 05:24:27 +0000

    Update scripts in examples/eis{ross|green} to use -i, -boot_from.

--- examples/eisross/quickstart.sh
+++ examples/eisross/quickstart.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
-
-#   Downloads data and runs an EISMINT-Ross example in PISM.
+#   Downloads data and runs an EISMINT-Ross example in PISM.  See User's Manual.
 
 NN=1  # default number of processors
-if [ $# -gt 0 ] ; then  # if user says "ccl3.sh 8" then NN = 8
+if [ $# -gt 0 ] ; then  # if user says "./quickstart.sh 8" then NN = 8
   NN="$1"
 fi
 
 set -e  # exit on error
 
-echo "--------------------------------------------------"
-echo "Download the ASCII files from the EISMINT web site:"
+echo "-----  Download the ASCII files from the EISMINT web site:"
 for fname in "111by147Grid.dat" "kbc.dat" "inlets.dat"
 do
   wget http://homepages.vub.ac.be/~phuybrec/eismint/$fname
 done
 
-echo
-echo "--------------------------------------------------"
-echo "Run eis_ross.py to turn ascii data into NetCDF file:"
-echo
-./eis_ross.py -o ross.nc
+echo "-----  Run eis_ross.py to turn ascii data into NetCDF file ross.nc:"
+./eisross.py -o ross.nc
 
-echo
-echo "--------------------------------------------------"
-echo "Also create NetCDF version of RIGGS data; used later:"
-echo
-./eis_riggs.py -o riggs.nc
+echo "-----  Also create NetCDF version of RIGGS data; riggs.nc; used later:"
+./eisriggs.py -o riggs.nc
 
-echo
-echo "--------------------------------------------------"
-echo "Running PISM to compute velocity in Ross ice shelf:"
-echo
-mpiexec -n $NN pismd -ross -bif ross.nc -ssa -ssaBC ross.nc -Mx 147 -My 147 -Mz 3 -Lz 1e3 -o rossComputed.nc
+echo "-----  Running PISM to compute velocity in Ross ice shelf:"
+mpiexec -n $NN pismd -ross -boot_from ross.nc -ssa -ssaBC ross.nc -Mx 147 -My 147 -Mz 3 -Lz 1e3 -o rossComputed.nc
  
-echo 
-echo "--------------------------------------------------"
-echo "Done.  View input NetCDF file ross.nc and model output"
-echo "rossComputed.nc."
+echo "-----  Done.  Model output in rossComputed.nc."
 
