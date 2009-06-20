8e6364026157450e5bc0ed355051a7ec3a763417
Constantine Khroulev <ckhroulev@alaska.edu>
2009-06-20 21:14:15 +0000

    Added #include <algorithm> to Timeseries.cc to address bug #13731.

--- src/base/Timeseries.cc
+++ src/base/Timeseries.cc
@@ -17,6 +17,7 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "Timeseries.hh"
+#include <algorithm>
 
 Timeseries::Timeseries(IceGrid *g, string name, string dimension_name) {
   com = g->com;
