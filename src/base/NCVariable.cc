66caca20827182077adb450b0b2494573d073895
Andy Aschwanden <andy.aschwanden@gmail.com>
2009-08-05 13:45:03 +0000

    Squashed commit of the following:
    commit 5ecb8d6f0a74de003eb28ea1fc534553c681977e
    Author: Andy Aschwanden <andy.aschwanden@arsc.edu>
    Date:   Wed Aug 5 15:42:47 2009 +0200
    
        added #include <cstring>, now PISM compiles on my Mac OSX

--- src/base/NCVariable.cc
+++ src/base/NCVariable.cc
@@ -16,6 +16,7 @@
 // along with PISM; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+#include <cstring>
 #include "NCVariable.hh"
 #include "pism_const.hh"
 
