66129e25b618102cfbfe5873714d9aa198626f89
Constantine Khroulev <ckhroulev@alaska.edu>
2009-12-08 03:39:28 +0000

    Added vfnow.py, the new version of verifynow.py (with the same look-and-feel), moved vnreport.py to vnreport_old.py and vnreport_new.py to vnreport.py. Using vfnow.py shows that test I does work (see bug #14837).

---
+++ test/vfnow.py
@@ -0,0 +1,338 @@
+#!/usr/bin/env python
+## VERIFYNOW.PY is a script to verify several components of PISM.
+## It specifies a refinement path for each of Tests ABCDEFGIJKL and runs pismv accordingly.
+## Copyright (C) 2007--2009 Ed Bueler and Constantine Khroulev
+import sys, getopt, time, commands
+from numpy import array, double, int
+
+class PISMVerificationTest:
+    """A class describing a refinement path and command-line options for a
+    particular PISM verification test."""
+    N = 50                      # max number of levels that will work with
+                                # default Mz and Mbz; should be enough so far
+    name = ""                   # one-letter test name
+    test = ""                   # test description
+    path = ""                   # description of the refinement path
+    Mx = []
+    My = []
+    Mz = [31] * N               # 31 levels in the ice
+    Mbz = [1] * N               # no bedrock by default
+    opts = ""                   # extra options (such as -y, -ys, -ssa_rtol)
+        
+    def build_command(self, executable, level):
+        M = zip(self.Mx, self.My, self.Mz, self.Mbz)
+
+        if level > len(M):
+            print "Test %s: Invalid refinement level: %d (only %d are available)" % (
+                self.name, level, len(M))
+            return ""
+
+        grid_options = "-Mx %d -My %d -Mz %d -Mbz %d" % M[level - 1]
+        return "%s -test %s %s %s" % (executable, self.name, grid_options, self.opts)
+
+def run_test(executable, name, level, extra_options = "", debug = False):
+    try:
+        test = tests[name]
+    except:
+        print "Test %s not found." % name
+        return
+
+    if level == 1:
+        print " ++++  TEST %s:  verifying with %s exact solution  ++++\n %s" % (
+            test.name, test.test, test.path)
+
+    command = test.build_command(executable, level) + " " + extra_options
+
+    if debug:
+        print ' L%d: would try "%s"' % (level, command)
+        return
+    else:
+        print ' L%d: trying "%s"' % (level, command)
+
+    # run PISM:
+    try:
+        lasttime = time.time()
+        (status,output) = commands.getstatusoutput(command)
+        elapsetime = time.time() - lasttime      
+    except KeyboardInterrupt:
+        sys.exit(2)
+    if status:
+        sys.exit(status)
+    print ' finished in %7.4f seconds; reported numerical errors as follows:' % elapsetime
+
+    # process the output:
+    position = output.find('NUMERICAL ERRORS')
+    if position >= 0:
+        report = output[position:output.find('NUM ERRORS DONE')]
+        endline = report.find('\n')
+        print '    ' + report[0:endline]
+        report = report[endline+1:]
+        while (len(report) > 1) and (endline > 0):
+            endline = report.find('\n')
+            if endline == -1:
+                endline = len(report)
+            print '   #' + report[0:endline]
+            report = report[endline+1:]       
+            endline = report.find('\n')
+            if endline == -1:
+                endline = len(report)
+            print '   |' + report[0:endline]
+            report = report[endline+1:]       
+    else:
+        print " ERROR: can't find reported numerical error"
+        sys.exit(99)
+
+
+def define_refinement_paths(KSPRTOL, SSARTOL):
+    # Define all the supported refinement paths:
+    tests = {}
+    # A
+    A = PISMVerificationTest()
+    A.name = "A"
+    A.test = "isothermal SIA with a marine margin"
+    A.path = "(refine dx=53.33,40,26.67,20,13.33,km, dx=dy and Mx=My=31,41,61,81,121)"
+    A.Mx   = [31, 41, 61, 81, 121]
+    A.My   = A.Mx
+    A.opts = "-y 25000.0"
+    tests['A'] = A
+    # B
+    B = PISMVerificationTest()
+    B.name = "B"
+    B.test = "isothermal SIA with a moving margin"
+    B.path = "(refine dx=80,60,40,30,20,km, dx=dy and Mx=My=31,41,61,81,121)"
+    B.Mx   = [31, 41, 61, 81, 121]
+    B.My   = B.Mx
+    B.opts = "-ys 422.45 -y 25000.0"
+    tests['B'] = B
+    # C
+    C = PISMVerificationTest()
+    C.name = "C"
+    C.test = "isothermal SIA w moving margin"
+    C.path = "(refine dx=50,33.33,25,20,16,km, dx=dy and Mx=My=41,61,81,101,121)"
+    C.Mx   = [41, 61, 81, 101, 121]
+    C.My   = C.Mx
+    C.opts = "-y 15208.0"
+    tests['C'] = C
+    # D
+    D = PISMVerificationTest()
+    D.name = "D"
+    D.test = "isothermal SIA with variable accumulation"
+    D.path = "(refine dx=50,33.33,25,20,16.67,km, dx=dy and Mx=My=41,61,81,101,121)"
+    D.Mx   = [41, 61, 81, 101, 121]
+    D.My   = D.Mx
+    D.opts = "-y 25000.0"
+    tests['D'] = D
+    # E
+    E = PISMVerificationTest()
+    E.name = "E"
+    E.test = "isothermal SIA with sliding"
+    E.path = "(refine dx=53.33,40,26.67,20,13.33,km, dx=dy and Mx=My=31,41,61,81,121)"
+    E.Mx   = [31, 41, 61, 81, 121]
+    E.My   = E.Mx
+    E.opts = "-y 25000.0"
+    tests['E'] = E
+    # F
+    F = PISMVerificationTest()
+    F.name = "F"
+    F.test = "thermocoupled SIA"
+    F.path = "(refine dx=30,20,15,10,7.5,km, dx=dy, dz=66.67,44.44,33.33,22.22,16.67 m\n  and Mx=My=Mz=61,91,121,181,241)"
+    F.Mx   = [61, 91, 121, 181, 241]
+    F.My   = F.Mx
+    F.Mz   = F.Mx
+    F.opts = "-y 25000.0"
+    tests['F'] = F
+    # G
+    G = PISMVerificationTest()
+    G.name = "G"
+    G.test = "thermocoupled SIA with variable accumulation"
+    G.path = "(refine dx=30,20,15,10,7.5,km, dx=dy, dz=66.67,44.44,33.33,22.22,16.67 m\n  and Mx=My=Mz=61,91,121,181,241)"
+    G.Mx   = [61, 91, 121, 181, 241]
+    G.My   = G.Mx
+    G.Mz   = G.Mx
+    G.opts = "-y 25000.0"
+    tests['G'] = G
+    # H
+    H = PISMVerificationTest()
+    H.name = "H"
+    H.test = "isothermal SIA with a moving margin and isostatic bed deformation"
+    H.path = "(refine dx=80,60,40,30,20,km, dx=dy and Mx=My=31,41,61,81,121)"
+    H.Mx   = [31, 41, 61, 81, 121]
+    H.My   = H.Mx
+    H.opts = "-bed_def_iso -y 60000.0"
+    tests['H'] = H
+    # I
+    I = PISMVerificationTest()
+    I.name = "I"
+    I.test = "plastic till ice stream"
+    I.path = "(refine dy=5000,1250,312.5,78.13,19.53,m, My=49,193,769,3073,12289)"
+    I.Mx   = [5] * 5
+    I.My   = [49, 193, 769, 3073, 12289]
+    I.opts = "-ssa_rtol %1.e -ksp_rtol %1.e" % (SSARTOL, KSPRTOL)
+    tests['I'] = I
+    # J
+    J = PISMVerificationTest()
+    J.name = "J"
+    J.test = "linearized periodic ice shelf"
+    J.path = "(refine dy=5000,1250,312.5,78.13,19.53,m, Mx=49,193,769,3073,12289)"
+    J.Mx   = [49, 193, 769, 3073, 12289]
+    J.My   = J.Mx
+    J.Mz   = [11] * 5
+    J.opts = "-pc_type asm -sub_pc_type lu -ksp_rtol %1.e" % KSPRTOL
+    tests['J'] = J
+    # K
+    K = PISMVerificationTest()
+    K.name = "K"
+    K.test = "pure conduction problem in ice and bedrock"
+    K.path = "(refine dz=100,50,25,12.5,6.25,m, Mz=41,81,161,321,641)"
+    K.Mx   = [4] * 5
+    K.My   = K.Mx
+    K.Mz   = array([41, 81, 161, 321, 641])
+    K.Mbz  = (K.Mz - 1) / 4 + 1
+    K.opts = "-y 130000.0 -Lbz 1000"
+    tests['K'] = K
+    # L
+    L = PISMVerificationTest()
+    L.name = "L"
+    L.test = "isothermal SIA with a non-flat bed"
+    L.path = "(refine dx=60,30,20,15,10,km, dx=dy and Mx=My=31,61,91,121,181)"
+    L.Mx   = [31, 61, 91, 121, 181]
+    L.My   = L.Mx
+    L.opts = "-y 25000.0"
+    tests['L'] = L
+    # M
+    M = PISMVerificationTest()
+    M.name = "M"
+    M.test = "annular ice shelf with a calving front"
+    M.path = "(refine dx=50,25,16.666,12.5,8.333 km; dx=dy and My=31,61,91,121,181)"
+    M.Mx   = [31, 61, 91, 121, 181]
+    M.My   = M.Mx
+    M.Mz   = [11] * 5
+    M.opts = "-ssa_rtol %1.e -ksp_rtol %1.e" % (SSARTOL, KSPRTOL)
+    tests['M'] = M
+
+    # test K (for a figure in the User's Manual)
+    K = PISMVerificationTest()
+    K.name = "K"
+    K.test = "pure conduction problem in ice and bedrock"
+    K.path = "(lots of levels)"
+    K.Mz   = array([21, 41, 61, 81, 101, 121, 141, 161, 181, 201, 221, 241, 261, 281, 301, 321])
+    K.Mbz  = (K.Mz - 1) / 4 + 1
+    K.Mx   = [4] * len(K.Mz)
+    K.My   = K.Mx
+    tests['K_userman'] = K
+
+    # test B (for a figure in the User's Manual)
+    B = PISMVerificationTest()
+    B.name = "B"
+    B.test = "isothermal SIA with a moving margin"
+    B.path = "(lots of levels)"
+    B.Mx   = [31, 41, 51, 61, 71, 81, 91, 101, 111, 121]
+    B.My   = B.Mx
+    B.Mz   = [31] * len(B.Mx)
+    B.Mbz  = [1]  * len(B.Mx)
+    tests['B_userman'] = B
+
+    # test G (for a figure in the User's Manual)
+    G = PISMVerificationTest()
+    G.name = "G"
+    G.test = "thermocoupled SIA with variable accumulation"
+    G.path = "(lots of levels)"
+    G.Mx   = [61, 71, 81, 91, 101, 111, 121, 151, 181]
+    G.My   = G.Mx
+    G.Mz   = G.Mx
+    tests['G_userman'] = G
+    
+    # test I (for a figure in the User's Manual)
+    I = PISMVerificationTest()
+    I.name = "I"
+    I.test = "plastic till ice stream"
+    I.path = "(lots of levels)"
+    I.My   = [51, 101, 151, 201, 401, 601, 801, 1001, 1501, 2001, 2501, 3073]
+    I.Mx   = [5] * len(I.My)
+    tests['I_userman'] = I
+
+    return tests
+
+#####
+## get options; see --help msg for meaning
+## default settings
+KSPRTOL = 1e-12 # for tests I, J, M
+SSARTOL = 5e-7  # ditto
+nproc   = 2  ## default; will not use 'mpiexec' if equal to one
+levels  = 2
+mpi     = "mpiexec -np"
+prefix  = ""
+test_names    = "CGIJ"
+userman_tests = ["B_userman", "G_userman", "K_userman", "I_userman"]
+extra_options = "-verbose 1"
+do_userman = False
+debug      = False
+try:
+  opts, args = getopt.getopt(sys.argv[1:], "ep:n:l:t:ur:",
+     ["eta","prefix=","nproc=","levels=","tests=","userman", "debug",
+      "uneq","mpido=","report_file=","help"])
+  for opt, arg in opts:
+    if opt in ("-p", "--prefix"):
+        prefix = arg
+    elif opt == "--userman":
+        do_userman = True
+    elif opt == "--debug":
+        debug = True
+    elif opt in ("-m", "--mpido"):
+        mpi = arg
+    elif opt in ("-n", "--nproc"):
+        nproc = int(arg)
+    elif opt in ("-l", "--levels"):
+        levels = int(arg)
+    elif opt in ("-t", "--tests"):
+        test_names = arg.upper()
+    elif opt in ("-u", "--uneq"):
+        extra_options += " -z_spacing quadratic"
+    elif opt in ("-e", "--eta"):
+        extra_options += " -eta"
+    elif opt in ("-r", "--report_file"):
+        extra_options += " -report_file %s" % arg
+    elif opt == "--help":
+        print """PISM verification script; usage:
+  -e,--eta=     to add '-eta' option to pismv call
+  -l,--levels=  number of levels of verification; '-l 1' fast, '-l 5' slowest
+  --mpido=      specify MPI executable
+                  (e.g. 'mpirun -np' or 'aprun -n' instead of default 'mpiexec -np')
+  -n,--nproc=   specify number of processors to MPI
+  -p,--prefix=  path prefix to pismv executable
+  -r,--report_file=  name of the NetCDF error report file
+  -t,--tests=   verification tests to use: A,B,C,D,E,F,G,H,I,J,K,L,M
+  -u            use unequal spaced (quadratic) vertical spacing
+  --userman     run tests necessary to produce figures in the User's Manual
+  --debug       do not run PISM, just print commands
+  --help        prints this message"""
+        sys.exit(0)
+except getopt.GetoptError:
+    print 'Incorrect command line arguments'
+    sys.exit(2)
+# should do range checking on option arguments here
+
+if nproc > 1:
+  predo = "%s %d " % (mpi, nproc)
+else:
+  predo = ""
+executable = predo + prefix + 'pismv'
+#####
+
+tests = define_refinement_paths(KSPRTOL, SSARTOL)
+
+if do_userman:
+    print " VERIFYNOW.PY: test(s) %s, using '%s'\n" % (userman_tests, executable) + \
+          "               and ignoring options -t and -l" 
+    for test in userman_tests:
+        N = len(tests[test].Mx)
+        for j in range(1, N + 1):
+            run_test(executable, test, j, extra_options, debug)
+else:
+    print " VERIFYNOW.PY: test(s) %s, %d refinement level(s), using '%s'" % (
+        test_names, levels, executable)
+
+    for test in test_names:
+        for j in range(1, levels + 1):
+            run_test(executable, test, j, extra_options, debug)
+
--- test/vnreport.py
+++ test/vnreport.py
@@ -1,187 +1,191 @@
-#! /usr/bin/env python
-## This script takes the standard out from verifynow.py and produces graphs 
-## using gnuplot.
-
+#!/usr/bin/env python
+from pylab import close, figure, clf, hold, plot, xlabel, ylabel, xticks, yticks, axis, legend, title, grid, show, savefig
+from numpy import array, polyfit, polyval, log10, floor, ceil, unique
+#from matplotlib.font_manager import FontProperties
 import getopt
 import sys
-import numpy
-from pylab import *
 
-# defaults
-PISM_REV = -1
-IN_FILE = 'foo.txt'
-OUT_FILE_START = 'report'
-TEST_NAME = 'A'
-EXCLUDE = ''
-
-##### process command line arguments #####
-infilename = IN_FILE
-outfilestart = OUT_FILE_START
-testname = TEST_NAME
-errtype = 'geometry'
-exclude = EXCLUDE
-pismrev = PISM_REV
-graphformat = 'png'
 try:
-  opts, args = getopt.getopt(sys.argv[1:], "f:t:o:e:x:r:g:",
-                             ["file=", "test=", "out=", "error=", 
-                              "exclude=", "pismrev=", "graphformat="])
-  for opt, arg in opts:
-    if opt in ("-e", "--error"):
-      errtype = arg
-    elif opt in ("-f", "--file"):
-      infilename = arg
-    elif opt in ("-g", "--graphformat"):
-      graphformat = arg
-    elif opt in ("-o", "--out"):
-      outfilestart = arg
-    elif opt in ("-r", "--pismrev"):
-      pismrev = arg
-    elif opt in ("-t", "--test"):
-      testname = arg
-    elif opt in ("-x", "--exclude"):
-      exclude = arg
+    from netCDF4 import Dataset as NC
+except:
+    from netCDF3 import Dataset as NC
+
+def plot_errors(nc, x, vars, testname, plot_title, filename = None):
+    # This mask lets us choose data corresponding to a particular test:
+    test = array(map(chr, nc.variables['test'][:]))
+    mask = (test == testname)
+
+    # If we have less than 2 points to plot, then bail.
+    if (sum(mask) < 2):
+        print "Skipping Test %s %s (not enough data to plot)" % (testname, plot_title)
+        return
+
+    # Get the independent variable and transform it. Note that everywhere here
+    # I assume that neither dx (dy, dz) nor errors can be zero or negative.
+    dx = nc.variables[x][mask]
+    dim = log10(dx)
+    
+    figure(figsize=(10,6));clf();hold(True)
+
+    colors = ['red', 'blue', 'green', 'black', 'brown', 'cyan']
+    for (v,c) in zip(vars,colors):
+        # Get a particular variable, transform and fit a line through it:
+        data = log10(nc.variables[v][mask])
+        p = polyfit(dim, data, 1)
+
+        # Try to get the long_name, use short_name if it fails:
+        try:
+            name = nc.variables[v].long_name
+        except:
+            name = v
+
+        # Create a label for the independent variable:
+        if (x == "dx"):
+            dim_name = "\Delta x"
+        if (x == "dy"):
+            dim_name = "\Delta y"
+        if (x == "dz"):
+            dim_name = "\Delta z"
+        if (x == "dzb"):
+            dim_name = "\Delta z_{bed}"
+
+        # Variable label:
+        var_label = "%s, $O(%s^{%1.2f})$" % (name, dim_name, p[0])
+
+        # Plot errors and the linear fit:
+        plot(dim, data, label=var_label, marker='o', color=c)
+        plot(dim, polyval(p, dim), ls="--", color=c)
+
+    # Shrink axes, then expand vertically to have integer powers of 10:
+    axis('tight')
+    _,_,ymin,ymax = axis()
+    axis(ymin = floor(ymin), ymax = ceil(ymax))
+
+    # Switch to km if dx (dy, dz) are big:
+    units = nc.variables[x].units
+    if (dx.min() > 1000.0 and (units == "meters")):
+        dx = dx / 1000.0
+        units = "km"
+    # Round grid spacing in x-ticks:
+    xticks(dim, map(lambda(x): "%d" % x, dx))
+    xlabel("$%s$ (%s)" % (dim_name, units))
+
+    # Use default (figured out by matplotlib) locations, but change labels for y-ticks:
+    loc,_ = yticks()
+    yticks(loc, map(lambda(x) : "$10^{%1.1f}$" % x, loc))
+
+    # Make sure that all variables given have the same units:
+    try:
+        ylabels = array(map(lambda(x): nc.variables[x].units, vars))
+        if (any(ylabels != ylabels[0])):
+            print "Incompatible units!"
+        else:
+            ylabel(ylabels[0])
+    except:
+        pass
+
+    # Legend, grid and the title:
+    legend(loc='best', pad=0.05, labelsep = 0, handletextsep = 0.02, handlelen = 0.02)
+    #  prop = FontProperties(size='smaller'),
+    grid(True)
+    title("Test %s %s (%s)" % (testname, plot_title, nc.source))
+
+    if (filename):
+        savefig(filename)
+
+def plot_tests(nc, list_of_tests):
+    for test_name in list_of_tests:
+        # thickness, volume and eta errors:
+        if test_name in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'L']:
+            plot_errors(nc, 'dx', ["maximum_thickness", "average_thickness"], test_name, "ice thickness errors")
+            plot_errors(nc, 'dx', ["relative_volume"], test_name, "relative ice volume errors")
+            plot_errors(nc, 'dx', ["relative_max_eta"], test_name, r"relative max $\eta$ errors")
+
+        # errors that are reported for test E only:
+        if (test_name == 'E'):
+            plot_errors(nc, 'dx', ["maximum_basal_velocity", "average_basal_velocity"], 'E' , r"basal velocity errors")
+            plot_errors(nc, 'dx', ["maximum_basal_u", "maximum_basal_v"], 'E' , "basal velocity (ub and vb) errors")
+            plot_errors(nc, 'dx', ["relative_basal_velocity"], 'E', "relative basal velocity errors")
+
+        # F and G temperature, sigma and velocity errors:
+        if test_name in ['F', 'G']:
+            plot_errors(nc, 'dx', ["maximum_sigma", "average_sigma"],
+                        test_name, "strain heating errors")
+            plot_errors(nc, 'dx', ["maximum_temperature", "average_temperature",
+                                   "maximum_basal_temperature", "average_basal_temperature"],
+                        test_name, "ice temperature errors")
+
+            plot_errors(nc, 'dx', ["maximum_surface_velocity", "maximum_surface_w"],
+                        test_name, "maximum ice surface velocity errors")
+            plot_errors(nc, 'dx', ["average_surface_velocity", "average_surface_w"],
+                        test_name, "average ice surface velocity errors")
+
+
+        # test I: plot only the u component
+        if test_name == 'I':
+            plot_errors(nc, 'dy', ["relative_velocity"],
+                        test_name, "relative velocity errors")
+            plot_errors(nc, 'dy', ["maximum_u", "average_u"],
+                        test_name, "velocity errors")
+            
+        # tests J and M:
+        if test_name in ['J', 'M']:
+            plot_errors(nc, 'dx', ["relative_velocity"],
+                        test_name, "relative velocity errors")
+            plot_errors(nc, 'dx', ["maximum_velocity", "maximum_u", "average_u", "maximum_v", "average_v"],
+                        test_name, "velocity errors")
+
+        # test K temperature errors:
+        if (test_name == 'K'):
+            plot_errors(nc, 'dz', ["maximum_temperature", "average_temperature",
+                                   "maximum_bedrock_temperature", "average_bedrock_temperature"],
+                        'K', "temperature errors")
+
+def print_help():
+    print """Usage:
+-i            specifies an input file (which should be a result of running pismv with the -report_file option)
+-t,--tests=   specifies a comma-separated list of tests. Use -t all to plot all the tests available in the -i file.
+--help        prints this message
+"""
+
+tests_to_plot = None
+input = None
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "i:t:", ["tests=", "help"])
+    for opt, arg in opts:
+        if opt in ["-t", "--tests"]:
+            tests_to_plot = arg.split(',')
+        if opt == "-i":
+            input = arg
+        if opt == "--help":
+            print_help()
+            sys.exit(0)
+    if input:
+        nc = NC(input, 'r')
+        available_tests = unique(array(map(chr, nc.variables['test'][:])))
+
+        if len(available_tests) == 1:
+            if tests_to_plot == None:
+                tests_to_plot = available_tests
+        else:
+            if (tests_to_plot == None):
+                print """Please choose tests to plot using the -t option.
+(Input file %s has reports for tests %s available.)""" % (input, str(available_tests))
+                sys.exit(0)
+
+        if (tests_to_plot[0] == "all"):
+            tests_to_plot = available_tests
+
+        close('all')
+        plot_tests(nc, tests_to_plot)
+        try:
+            # show() will break if we didn't plot anything
+            show()
+        except:
+            pass
+    else:
+        print_help()
 except getopt.GetoptError:
-  print 'Incorrect command line arguments'
-  sys.exit(2)
-
-# read file
-print "reading from " + infilename + " (= standard output from verifynow.py)"
-try:
-  infile = open(infilename, 'r');
-except IOError:
-  print 'ERROR: File: ' + infilename + ' could not be found.'
-  sys.exit(2)
-
-# search for numerical errors from chosen test
-while True:
-  myline = infile.readline()
-  if not myline: # stop if nothing left to read
-    foundmytest = False
-    break
-  if myline.find('TEST ' + testname) >= 0:
-    foundmytest = True
-    break
-if foundmytest == False:
-  print 'FAILED: numerical errors for test ' + testname + ' not found in file ' + infilename
-  sys.exit(3)
-
-# the next line will contain the refinement path
-myline = infile.readline()
-pos = myline.find('refine ')
-if pos < 0:
-  print 'FAILED: refinement path not found in expected spot'
-  sys.exit(5)
-dxname = myline[pos+7:pos+9]
-myline = myline[pos+10:]
-strpath = myline.split(',')  # split on COMMAS!
-path = []
-for kk in range(5):  # expect exactly 5 pts on refinement path!
-  path.append(float(strpath[kk]))
-units = strpath[5]  # expect units to be given next
-
-# read and parse error report into tokens
-tags=[]   # the names of the error values
-vals=[]   # the values themselves
-titles=[] # category of the error; will be title on plot
-count = 0
-while True:
-  myline = infile.readline()
-  # stop if nothing left to read or we run into another test report
-  if (not myline) or (myline.find('TEST') >= 0): 
-    break
-  while myline[0] == ' ': # strip off leading spaces
-    myline = myline[1:]
-  if myline[0] == '#':
-    titles.append(myline[1:11])
-    myline = myline[12:] #strip off 12
-    tags.append(myline.split())
-  elif myline[0] == '|':
-    myline = myline[12:] #strip off 12
-    myvals = []
-    for num in myline.split():
-      myvals.append(float(num))
-    vals.append(myvals)
-    count = count + len(myvals)
-print str(count) + ' total numerical error values read for test ' + testname
-infile.close()
-
-# go through and eliminate all but the desired error type
-NN = 0
-while len(titles) > NN:
-  if titles[NN].find(errtype) >= 0:
-    NN = NN + 1
-  else:
-    #remove title and corresponding tags and errors
-    titles = titles[0:NN] + titles[NN+1:]
-    tags = tags[0:NN] + tags[NN+1:]
-    vals = vals[0:NN] + vals[NN+1:]
-if NN == 0:
-  print 'FAILED: no errors of type "' + errtype + '" found'
-  sys.exit(5)
-
-# check that tags are consistent; then discard redundant tags
-for kk in range(len(tags[0])):
-  firsttag = tags[0][kk]
-  for ll in range(len(tags)):
-    if not (firsttag == tags[ll][kk]):
-      print 'FAILED: inconsistent tags'
-      sys.exit(4)
-tags = tags[0]
-
-# reorganize errors; now err[N] is along refinement path
-err = numpy.array(vals).transpose()
-print 'found ' + str(len(err)*len(err[0])) + \
-      ' "' + errtype + '"-type  numerical error values for plotting'
-
-# plot to PNG or PDF files
-style = '-o'
-markersize = 2.0
-base = 10
-loc = 'lower right'
-
-figure(1,figsize=(8, 4)); hold(True);
-for nn in range(len(tags)):
-  xdata = array(path[0:len(err[nn])])
-  ydata = array(err[nn])
-  if exclude.find(tags[nn]) < 0: # if the tag is not in the exclude string, then plot
-    plot(xdata[::-1], ydata[::-1], style, label=(tags[nn] + ' error'))
-    ## loglog does not work well:
-    #loglog(xdata[::-1], ydata[::-1], style, label=(tags[nn] + ' error'),
-    #       markersize=markersize,
-    #       basex=base,
-    #       basey=base)
-  else:
-    print 'excluding "' + tags[nn] + '" errors from plot'
-hold(False)
-
-imagetitle = titles[0] + ' numerical errors in test ' + testname
-if pismrev > 0:
-  imagetitle += ' (PISM revision ' + str(pismrev) + ')'
-else:
-  imagetitle += ' (PISM)'
-title(imagetitle);
-
-axis('tight');
-grid(True)
-usexlabel = dxname + '  (' + units + ')'
-xlabel(usexlabel)
-legend(loc=loc);
-xticks(xdata[::-1]);
-
-#show()
-
-outfilename = outfilestart + '_' + testname
-if graphformat.find('pdf') >= 0:
-  outfilename += '.pdf'
-  savefig(outfilename)
-  print 'PDF file ' + outfilename + ' written'
-else:
-  if graphformat.find('png') < 0:
-    print 'unknown graphic output format; attempting PNG'
-  outfilename += '.png'
-  savefig(outfilename)
-  print 'PNG file ' + outfilename + ' written'
-# done
-
-
+    print "Processing command-line options failed."
+    print_help()
+    sys.exit(1)
