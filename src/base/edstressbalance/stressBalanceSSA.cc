5e019102aa3c20595d07c6b1ecfd4a08a08ec453
Ed Bueler <elbueler@alaska.edu>
2009-12-20 03:21:57 +0000

    Start on modularization of SSA, i.e. the move out of IceModel.

---
+++ src/base/edstressbalance/stressBalanceSSA.cc
@@ -0,0 +1,219 @@
+// Copyright (C) 2006-2009 Ed Bueler, Constantine Khroulev, and Jed Brown
+//
+// This file is part of PISM.
+//
+// PISM is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// PISM is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PISM; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#include <petsc.h>
+#include "pism_const.hh"
+#include "grid.hh"
+#include "iceModelVec.hh"
+
+#include "stressBalanceSSA.hh"
+
+
+newSSAStrengthExtension::newSSAStrengthExtension() {
+  min_thickness = 50.0;   // m
+          // minimum thickness (for SSA velocity computation) at which 
+          // NuH switches from vertical integral to constant value
+          // this value strongly related to calving front
+          // force balance, but the geometry itself is not affected by this value
+  const PetscReal
+    DEFAULT_CONSTANT_HARDNESS_FOR_SSA = 1.9e8,  // Pa s^{1/3}; see p. 49 of MacAyeal et al 1996
+    DEFAULT_TYPICAL_STRAIN_RATE = (100.0 / secpera) / (100.0 * 1.0e3);  // typical strain rate is 100 m/yr per 
+  nuH = min_thickness * DEFAULT_CONSTANT_HARDNESS_FOR_SSA
+                       / (2.0 * pow(DEFAULT_TYPICAL_STRAIN_RATE,2./3.)); // Pa s m
+          // COMPARE: 30.0 * 1e6 * secpera = 9.45e14 is Ritz et al (2001) value of
+          //          30 MPa yr for \bar\nu
+}
+
+
+PetscErrorCode newSSAStrengthExtension::set_notional_strength(PetscReal my_nuH) {
+  nuH = my_nuH;
+  return 0;
+}
+
+PetscErrorCode newSSAStrengthExtension::set_min_thickness(PetscReal my_min_thickness) {
+  min_thickness = my_min_thickness;
+  return 0;
+}
+
+
+StressBalanceSSA::StressBalanceSSA(
+           IceGrid* g, IceFlowLaw* ssa_ice, IceBasalResistancePlasticLaw* ssa_basal,
+           IceModelVec2 *ssa_tauc, IceModelVec2 *ssa_mask, IceModelVec2 *ssa_hardav) {
+  initAndAllocate(g);
+  ice = ssa_ice;
+  basal = ssa_basal;
+  vtauc = ssa_tauc;
+  vmask = ssa_mask;
+  vhardav = ssa_hardav;
+}
+
+
+StressBalanceSSA::~StressBalanceSSA() {
+  deallocate();
+}
+
+
+PetscErrorCode StressBalanceSSA::initAndAllocate(IceGrid* g) {
+  PetscErrorCode ierr;
+  
+  grid = g;
+
+  config.init("pism_config", grid->com, grid->rank);
+  char alt_config[PETSC_MAX_PATH_LEN];
+  PetscTruth use_alt_config;
+  ierr = PetscOptionsGetString(PETSC_NULL, "-config", alt_config, 
+                               PETSC_MAX_PATH_LEN, &use_alt_config); CHKERRQ(ierr);
+  if (use_alt_config) {
+    ierr = config.read(alt_config); CHKERRQ(ierr);
+  } else {
+    ierr = config.read(PISM_DefaultConfigFile); CHKERRQ(ierr);
+  }
+
+  char overridename[PETSC_MAX_PATH_LEN];
+  PetscTruth use_override_config;
+  ierr = PetscOptionsGetString(PETSC_NULL, "-config_override", overridename,
+			       PETSC_MAX_PATH_LEN, &use_override_config);
+  if (use_override_config) {
+    NCConfigVariable overrides;
+    overrides.init("pism_overrides", grid->com, grid->rank);
+    ierr = overrides.read(overridename); CHKERRQ(ierr);
+    config.import_from(overrides);
+  }
+
+  config.print();
+
+  // setup (classical) SSA tools
+  const PetscInt M = 2 * grid->Mx * grid->My;
+
+  ierr = MatCreateMPIAIJ(grid->com, PETSC_DECIDE, PETSC_DECIDE, M, M,
+                         13, PETSC_NULL, 13, PETSC_NULL,
+                         &SSAStiffnessMatrix); CHKERRQ(ierr);
+
+  ierr = VecCreateMPI(grid->com, PETSC_DECIDE, M, &SSAX); CHKERRQ(ierr);
+  ierr = VecDuplicate(SSAX, &SSARHS); CHKERRQ(ierr);
+
+  ierr = VecCreateSeq(PETSC_COMM_SELF, M, &SSAXLocal);
+  ierr = VecScatterCreate(SSAX, PETSC_NULL, SSAXLocal, PETSC_NULL,
+                          &SSAScatterGlobalToLocal); CHKERRQ(ierr);
+
+  ierr = KSPCreate(grid->com, &SSAKSP); CHKERRQ(ierr);
+  ierr = KSPSetFromOptions(SSAKSP); CHKERRQ(ierr);
+
+  // initial guesses of SSA velocities
+  ierr = vubarSSA.create(*grid, "vubarSSA", true);
+  ierr = vubarSSA.set_attrs("ssa_internal_restart", 
+                            "SSA model ice velocity in the X direction",
+                            "m s-1", ""); CHKERRQ(ierr);
+  ierr = vubarSSA.set_glaciological_units("m year-1"); CHKERRQ(ierr);
+
+  ierr = vvbarSSA.create(*grid, "vvbarSSA", true);
+  ierr = vvbarSSA.set_attrs("ssa_internal_restart",
+                            "SSA model ice velocity in the Y direction",
+                            "m s-1", ""); CHKERRQ(ierr);
+  ierr = vvbarSSA.set_glaciological_units("m year-1"); CHKERRQ(ierr);
+
+  ierr = vaveragedhardness.create(*grid, "hardavSSA", true); CHKERRQ(ierr);
+  const PetscScalar power = 1.0 / ice->exponent();
+  char unitstr[TEMPORARY_STRING_LENGTH];
+  snprintf(unitstr, sizeof(unitstr), "Pa s%f", power);
+  ierr = vaveragedhardness.set_attrs("ssa_internal",
+                                     "vertical average of ice hardness",
+			             unitstr, ""); CHKERRQ(ierr);
+  ierr = vaveragedhardness.set_attr("valid_min",0.0); CHKERRQ(ierr);
+
+  return 0;
+}
+
+
+PetscErrorCode StressBalanceSSA::deallocate() {
+  PetscErrorCode ierr;
+
+  ierr = KSPDestroy(SSAKSP); CHKERRQ(ierr);
+  ierr = MatDestroy(SSAStiffnessMatrix); CHKERRQ(ierr);
+  ierr = VecDestroy(SSAX); CHKERRQ(ierr);
+  ierr = VecDestroy(SSARHS); CHKERRQ(ierr);
+  ierr = VecDestroy(SSAXLocal); CHKERRQ(ierr);
+  ierr = VecScatterDestroy(SSAScatterGlobalToLocal); CHKERRQ(ierr);
+
+  return 0;
+}
+
+
+PetscErrorCode StressBalanceSSA::setGuessZero() {
+  PetscErrorCode ierr;
+  ierr = vubarSSA.set(0.0); CHKERRQ(ierr);
+  ierr = vvbarSSA.set(0.0); CHKERRQ(ierr);
+  return 0;
+}
+
+
+PetscErrorCode StressBalanceSSA::setGuess(IceModelVec2 *ubar_guess, IceModelVec2 *vbar_guess) {
+  PetscErrorCode ierr;
+  ierr = vubarSSA.copy_from(*ubar_guess); CHKERRQ(ierr);
+  ierr = vvbarSSA.copy_from(*vbar_guess); CHKERRQ(ierr);
+  return 0;
+}
+
+
+PetscScalar StressBalanceSSA::basalDragx(PetscScalar **tauc, PetscScalar **u, PetscScalar **v,
+                                         PetscInt i, PetscInt j) const {
+  return basal->drag(tauc[i][j], u[i][j], v[i][j]);
+}
+
+
+PetscScalar StressBalanceSSA::basalDragy(PetscScalar **tauc, PetscScalar **u, PetscScalar **v,
+                                         PetscInt i, PetscInt j) const {                         
+  return basal->drag(tauc[i][j], u[i][j], v[i][j]);
+};
+
+
+/*!
+Compute compoents of the basal stress <em>applied to the base of the ice</em>:
+  \f[\tau_{b,x} = - C(\tau_c,u,v) u,]\f
+  \f[\tau_{b,y} = - C(\tau_c,u,v) v,]\f
+ */
+PetscErrorCode StressBalanceSSA::getBasalStress(IceModelVec2 *vbs_x, IceModelVec2 *vbs_y) {
+  PetscErrorCode ierr;
+  
+  PetscScalar **u, **v, **tauc, **mask, **bs_x, **bs_y;
+  ierr = vubarSSA.get_array(u); CHKERRQ(ierr);
+  ierr = vvbarSSA.get_array(v); CHKERRQ(ierr);
+  ierr = vtauc->get_array(tauc); CHKERRQ(ierr);
+  ierr = vmask->get_array(mask); CHKERRQ(ierr);
+  ierr = vbs_x->get_array(bs_x); CHKERRQ(ierr);
+  ierr = vbs_y->get_array(bs_y); CHKERRQ(ierr);
+
+  for (PetscInt i=grid->xs; i<grid->xs+grid->xm; ++i) {
+    for (PetscInt j=grid->ys; j<grid->ys+grid->ym; ++j) {
+      bs_x[i][j] = - basalDragx(tauc, u, v, i, j) * u[i][j],
+      bs_y[i][j] = - basalDragy(tauc, u, v, i, j) * v[i][j];
+    }
+  }
+
+  ierr = vubarSSA.end_access(); CHKERRQ(ierr);
+  ierr = vvbarSSA.end_access(); CHKERRQ(ierr);
+  ierr = vtauc->end_access(); CHKERRQ(ierr);
+  ierr = vmask->end_access(); CHKERRQ(ierr);
+  ierr = vbs_x->end_access(); CHKERRQ(ierr);
+  ierr = vbs_y->end_access(); CHKERRQ(ierr);
+  
+  return 0;
+}
+
+
+
---
+++ src/base/edstressbalance/stressBalanceSSA.hh
@@ -0,0 +1,135 @@
+// Copyright (C) 2006-2009 Ed Bueler, Constantine Khroulev, and Jed Brown
+//
+// This file is part of PISM.
+//
+// PISM is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// PISM is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PISM; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#ifndef __stressBalanceSSA_hh
+#define __stressBalanceSSA_hh
+
+#include <petscksp.h>
+#include "pism_const.hh"
+#include "grid.hh"
+#include "materials.hh"
+#include "iceModelVec.hh"
+
+
+//! Where ice thickness is zero the SSA is no longer "elliptic".  This class provides an extension coefficient to maintain well-posedness/ellipticity.
+/*!
+More specifically, the SSA equations are
+\latexonly
+\def\ddt#1{\ensuremath{\frac{\partial #1}{\partial t}}}
+\def\ddx#1{\ensuremath{\frac{\partial #1}{\partial x}}}
+\def\ddy#1{\ensuremath{\frac{\partial #1}{\partial y}}}
+\begin{equation*}
+  - 2 \ddx{}\left[\nu H \left(2 \ddx{u} + \ddy{v}\right)\right]
+        - \ddy{}\left[\nu H \left(\ddy{u} + \ddx{v}\right)\right]
+        + \tau_{(b)x}  =  - \rho g H \ddx{h},
+\end{equation*}
+\endlatexonly
+and another similar equation.  Schoof \ref SchoofStream shows that these PDEs
+are the variational equations for a functional.
+
+The quantity \f$\nu H\f$ is the nonlinear coefficient in this (roughly-speaking)
+elliptic pair of PDEs.  Conceptually it is a membrane strength.  Well-posedness of the SSA problem 
+requires either a precisely-defined boundary and an appropriate boundary condition
+\e or a nonzero value of \f$\nu H\f$ at all points.  This class provides that nonzero value.
+ */
+class newSSAStrengthExtension {
+public:
+  newSSAStrengthExtension();
+  //! Set strength with units (viscosity times thickness).
+  PetscErrorCode set_notional_strength(PetscReal my_nuH);
+  //! Set minimum thickness to trigger use of extension.
+  PetscErrorCode set_min_thickness(PetscReal my_min_thickness);
+  //! Returns strength with units (viscosity times thickness).
+  PetscReal      get_notional_strength() const { return nuH; }
+  //! Returns minimum thickness to trigger use of extension.
+  PetscReal      get_min_thickness_for_extension() const { return min_thickness; }
+private:
+  PetscReal  min_thickness, nuH;
+};
+
+
+//! Solve shallow shelf approximation (SSA) ice sheet stress balance for PISM.
+class StressBalanceSSA {
+
+public:
+  StressBalanceSSA(IceGrid* g, IceFlowLaw* ssa_ice, IceBasalResistancePlasticLaw* ssa_basal,
+                   IceModelVec2 *ssa_tauc, IceModelVec2 *ssa_mask, IceModelVec2 *ssa_hardav);
+  virtual ~StressBalanceSSA();
+
+  //! Sets initial-guess velocities to zero.
+  virtual PetscErrorCode setGuessZero();
+  //! Provide initial-guess velocities.
+  virtual PetscErrorCode setGuess(IceModelVec2 *ubar_guess, IceModelVec2 *vbar_guess);
+
+  //! Solve stress balance for vertically-integrated horizontal velocity.
+  virtual PetscErrorCode solve(PetscInt *numiter);
+  //! Solve stress balance for vertically-integrated horizontal velocity, but provide the effective viscosity (two components on the staggered grid).
+  virtual PetscErrorCode solve(IceModelVec2 vNuH[2], PetscInt *numiter);
+
+  //! Put components of basal stress, applied by bedrock to base of ice, in provided IceModelVec2.
+  virtual PetscErrorCode getBasalStress(IceModelVec2 *vbs_x, IceModelVec2 *vbs_y);
+
+  //! Modify/update the provided field (e.g. by adding SSA solution to current SIA solution).
+  virtual PetscErrorCode modifyHorizontalVelocityComponents(IceModelVec3 *u, IceModelVec3 *v);
+
+  //! Modify/update the provided field of strain heating values.
+  virtual PetscErrorCode modifySigma(IceModelVec3 *sigma);
+
+  //! Modify/update the provided field of basal friction rates.
+  virtual PetscErrorCode modifyBasalFrictionalHeating();
+
+protected:
+  virtual PetscErrorCode updateAveragedHardness();
+  virtual PetscErrorCode computeEffectiveViscosity(IceModelVec2 vNuH[2], PetscReal epsilon);
+  virtual PetscErrorCode testConvergenceOfNu(IceModelVec2 vNuH[2], IceModelVec2 vNuHOld[2],
+                                             PetscReal *norm, PetscReal *normChange);
+  virtual PetscErrorCode assembleSSAMatrix(bool includeBasalShear, IceModelVec2 vNuH[2], Mat A);
+  virtual PetscErrorCode assembleSSARhs(bool surfGradInward, Vec rhs);
+  virtual PetscErrorCode moveVelocityToDAVectors(Vec x);
+
+  IceModelVec2 vubarSSA, vvbarSSA,
+               vaveragedhardness;
+  
+  KSP        SSAKSP;
+  Mat        SSAStiffnessMatrix;
+  Vec        SSAX,      //!< Global vector for solution of the linear system  Ax=b.
+             SSARHS;    //!< Global vector for right-hand side of the linear system  Ax=b.
+  Vec        SSAXLocal; //!< Local copy of the solution uses this to map back to a DA-based vector.
+  VecScatter SSAScatterGlobalToLocal;
+
+  IceGrid*         grid;
+  NCConfigVariable config;
+
+  IceFlowLaw*                   ice;
+  IceBasalResistancePlasticLaw* basal;
+  SSAStrengthExtension          ssaStrengthExtend;
+
+  IceModelVec2 *vtauc, *vmask, *vhardav;  // pointers to external IMVecs (e.g. from IceModel)
+
+private:
+  PetscErrorCode initAndAllocate(IceGrid* g);
+  PetscErrorCode deallocate();
+
+  PetscScalar basalDragx(PetscScalar **tauc, PetscScalar **u, PetscScalar **v,
+                         PetscInt i, PetscInt j) const;
+  PetscScalar basalDragy(PetscScalar **tauc, PetscScalar **u, PetscScalar **v,
+                         PetscInt i, PetscInt j) const;
+};
+
+#endif
+
