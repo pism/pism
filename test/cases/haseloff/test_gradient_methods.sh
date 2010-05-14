6e5a66c2f668f90c2f68216fff13642b79282d9d
Ed Bueler <elbueler@alaska.edu>
2010-05-14 08:53:27 +0000

    Added the case Marianne contributed, which shows strongly the difference among the three -gradient {haseloff|mahaffy|eta} methods.  Note the eta method does really badly here.  Demonstrates and fixes bug #12650.

---
+++ test/cases/haseloff/test_gradient_methods.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NN=2
+
+INFILE=startSMALLablate.nc
+
+OPTS="-Lz 3500 -Mx 51 -My 51 -Mz 61 -no_temp -y 500 -surface constant -extra_vars thk,csurf -extra_times 0:10:500 -ts_times 0:1:500"
+
+mpiexec -n $NN pismr -boot_from $INFILE -gradient haseloff $OPTS -extra_file Hex.nc -ts_file Hts.nc -o H.nc
+
+mpiexec -n $NN pismr -boot_from $INFILE -gradient mahaffy $OPTS -extra_file Mex.nc -ts_file Mts.nc -o M.nc
+
+mpiexec -n $NN pismr -boot_from $INFILE -gradient eta $OPTS -extra_file Eex.nc -ts_file Ets.nc -o E.nc
+
