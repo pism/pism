7a43f192ed4cb2faf7bfc8d9b5c98a5fe4ae7353
Ed Bueler <elbueler@alaska.edu>
2010-10-28 20:53:58 +0000

    Draft of line search which will replace a long list  in dotune.sh.  I think the 'special sauce' is a longitudinal lapse rate for the pdd_std_dev variable, to parameterize the variability-of-climate that seems to come into the temperature fields.

--- examples/pddtune/dotune.sh
+++ examples/pddtune/dotune.sh
@@ -16,17 +16,23 @@ set -e  # exit on error
 #./preprocess.sh # generates pism_Greenland_5km_v1.1.nc and base_config.nc
 #./boot.sh  # creates start.nc, which contains 'thk' used in masking in objective.py
 
-# harder: 5^4 = 625 cases
+# harder: 5^3 * 10 = 1250 cases
 THRESHOLDRANGE="268 269 270 271 273"           # default 273.15
 DDFSNOWRANGE="0.001 0.002 0.003 0.005 0.009"   # default 0.003
 REFREEZERANGE="0.4 0.5 0.6 0.7 0.8"            # default 0.6
-STDDEVRANGE="1.0 1.5 2.5 4.0 6.0"              # default 2.53
+STDDEVRANGE="0.5 1.0 1.25 1.5 1.75 2.5 3.0 4.0 5.0 6.0"              # default 2.53
 
 # simpler: 3^3 * 5 = 135 cases
 THRESHOLDRANGE="268 270 273"       # default 273.15
 DDFSNOWRANGE="0.001 0.002 0.003 0.005 0.009"   # default 0.003
 REFREEZERANGE="0.4 0.6 0.8"        # default 0.6
 STDDEVRANGE="1.0 2.5 6.0"          # default 2.53
+
+# harder: 5^3 * 10 = 1250 cases
+THRESHOLDRANGE="268 269 270 271 273"           # default 273.15
+DDFSNOWRANGE="0.001 0.002 0.003 0.005 0.009"   # default 0.003
+REFREEZERANGE="0.4 0.5 0.6 0.7 0.8"            # default 0.6
+STDDEVRANGE="0.5 1.0 1.25 1.5 1.75 2.5 3.0 4.0 5.0 6.0"              # default 2.53
 
 export DELETECLIMATE=1    # causes .nc produced by pclimate to be deleted
 
