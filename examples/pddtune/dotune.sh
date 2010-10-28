3fa54c6509c6f625a7077ea9435de7e85706627a
Ed Bueler <elbueler@alaska.edu>
2010-10-28 07:10:19 +0000

    Searches are a bit simpler because I have tied the snow and ice DDFs:  DDFice = 2.0 * DDFsnow.

--- examples/pddtune/dotune.sh
+++ examples/pddtune/dotune.sh
@@ -16,19 +16,17 @@ set -e  # exit on error
 #./preprocess.sh # generates pism_Greenland_5km_v1.1.nc and base_config.nc
 #./boot.sh  # creates start.nc, which contains 'thk' used in masking in objective.py
 
-# harder: 5^5 = 3125 cases
+# harder: 5^4 = 625 cases
 THRESHOLDRANGE="268 269 270 271 273"           # default 273.15
 DDFSNOWRANGE="0.001 0.002 0.003 0.005 0.009"   # default 0.003
-DDFICERANGE="0.002 0.005 0.008 0.012 0.024"    # default 0.008
 REFREEZERANGE="0.4 0.5 0.6 0.7 0.8"            # default 0.6
 STDDEVRANGE="1.0 1.5 2.5 4.0 6.0"              # default 2.53
 
-# simpler: 3^5 = 243 cases
+# simpler: 3^3 * 5 = 135 cases
 THRESHOLDRANGE="268 270 273"       # default 273.15
-DDFSNOWRANGE="0.001 0.003 0.009"   # default 0.003
-DDFICERANGE="0.002 0.008 0.024"    # default 0.008
+DDFSNOWRANGE="0.001 0.002 0.003 0.005 0.009"   # default 0.003
 REFREEZERANGE="0.4 0.6 0.8"        # default 0.6
-STDDEVRANGE="1.0 2.5  6.0"         # default 2.53
+STDDEVRANGE="1.0 2.5 6.0"          # default 2.53
 
 export DELETECLIMATE=1    # causes .nc produced by pclimate to be deleted
 
@@ -36,20 +34,17 @@ for THRESHOLD in $THRESHOLDRANGE
 do
   for DDFSNOW in $DDFSNOWRANGE
   do
-    for DDFICE in $DDFICERANGE
+    for REFREEZE in $REFREEZERANGE
     do
-      for REFREEZE in $REFREEZERANGE
+      for STDDEV in $STDDEVRANGE
       do
-        for STDDEV in $STDDEVRANGE
-        do
 
-          ## if not deleted, output of runcase.sh is 
-          ##   clim_${THRESHOLD}_${DDFSNOW}_${DDFICE}_${REFREEZE}_${STDDEV}.nc
+        ## if not deleted, output of runcase.sh is 
+        ##   clim_${THRESHOLD}_${DDFSNOW}_${REFREEZE}_${STDDEV}.nc
 
-          # run case adds case to diffs.txt:
-          ./runcase.sh $THRESHOLD $DDFSNOW $DDFICE $REFREEZE $STDDEV diffs.txt
+        # run case adds case to diffs.txt:
+        ./runcase.sh $THRESHOLD $DDFSNOW $REFREEZE $STDDEV diffs.txt
 
-        done
       done
     done
   done
