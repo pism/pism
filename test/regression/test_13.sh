d2d81665704ef377e495a212edefaf6600dff0ca
Constantine Khroulev <ckhroulev@alaska.edu>
2011-03-07 17:40:54 +0000

    Minor cleanup
    - Updated the Installation Manual
    - Fixed test_13.sh (so that it works with NCO 4.0.5)
    - Made CMake write .petscrc so that we can run PISM from the build tree without putting pism_config.nc in its install location
    - Fixed doc/CMakeLists.txt.

--- test/regression/test_13.sh
+++ test/regression/test_13.sh
@@ -15,11 +15,8 @@ $MPIEXEC -n 2 $PISM_PATH/pisms -eisII A -Mx 31 -My 31 -Mz 31 -Lbz 1000 -Mbz 11 \
     -y 6e3 -temp_pa -o foo.nc
 
 # extract only what is needed for comparison
-ncks -v temp -d z,0m foo.nc temp.nc
-ncks -v litho_temp -d zb,-0.000001m foo.nc litho_temp.nc
-# neither of these seems to work:
-#run ncks -v litho_temp -d zb,0m foo.nc litho_temp.nc
-#run ncks -v litho_temp -d zb,-0m foo.nc litho_temp.nc
+ncks -v temp -d z,0 foo.nc temp.nc
+ncks -v litho_temp -d zb,10 foo.nc litho_temp.nc
 
 # compare
 ncrename -O -v litho_temp,temp litho_temp.nc
