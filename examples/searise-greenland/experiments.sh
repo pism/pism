dbfde0055a44368d1f21b508b86da45ced775cc3
Ed Bueler <elbueler@alaska.edu>
2011-06-15 01:12:06 +0000

    Trivial edit to .bib.  For experiments.sh, added ability to use env vars PISM_SKIP and PISM_INITIALS.

--- examples/searise-greenland/experiments.sh
+++ examples/searise-greenland/experiments.sh
@@ -5,11 +5,11 @@
 # PISM SeaRISE Greenland experiments
 
 # Before using this script, run preprocess.sh and then initialization.sh
-# recommended way to run with N processors is "./experiments.sh N >& out.experiments &"
+# recommended way to run with N processors is
+#    "./experiments.sh N foo.nc >& out.experiments &"
+# to initialize from foo.nc.
 
-
-INITIALS=UAF${2}  # output file names will be ...UAF1_G_D3_C1_E0..., etc.
-SPINUPRESULT=$3  # spinup 
+SPINUPRESULT=$2  # spinup 
 
 echo
 echo "# ============================================================================="
@@ -33,7 +33,7 @@ fi
 
 PISM_CONFIG=searise_config.nc
 
-for INPUT in $INITRESULT $PISM_CONFIG; do
+for INPUT in $SPINUPRESULT $PISM_CONFIG; do
   if [ -e "$INPUT" ] ; then  # check if file exist
     echo "$SCRIPTNAME INPUT   $INPUT FOUND"
   else
@@ -43,6 +43,7 @@ for INPUT in $INITRESULT $PISM_CONFIG; do
     echo
   fi
 done
+echo
 
 echo "$SCRIPTNAME              NN = $NN"
 
@@ -79,6 +80,24 @@ else
   echo "$SCRIPTNAME       PISM_EXEC = $PISM_EXEC"
 fi
 
+# all runs will use  -skip $PISM_SKIP
+if [ -n "${PISM_SKIP:+1}" ] ; then
+  echo "$SCRIPTNAME       PISM_SKIP = $PISM_SKIP (already set)"
+else
+  PISM_SKIP=200
+  echo "$SCRIPTNAME       PISM_SKIP = $PISM_SKIP"
+fi
+SKIP=$PISM_SKIP
+
+# output file names will be ...UAF1_G_D3_C1_E0..., etc. if PISM_INITIALS=UAF1
+if [ -n "${PISM_INITIALS:+1}" ] ; then
+  echo "$SCRIPTNAME   PISM_INITIALS = $PISM_INITIALS (already set)"
+else
+  PISM_INITIALS=UAF1
+  echo "$SCRIPTNAME   PISM_INITIALS = $PISM_INITIALS"
+fi
+INITIALS=$PISM_INITIALS 
+
 echo
 
 
@@ -92,22 +111,22 @@ TITLE="SeaRISE Greenland Experiment"
 # cat prefix and exec together
 PISM="${PISM_PREFIX}${PISM_EXEC} -ocean_kill -config_override $PISM_CONFIG -title \"$TITLE\" $FULLPHYS"
 
+echo "$SCRIPTNAME         tillphi = '$TILLPHI'"
+echo "$SCRIPTNAME    full physics = '$FULLPHYS'"
+echo "$SCRIPTNAME      executable = '$PISM'"
+
 
 # coupler settings
 COUPLER_CTRL="-ocean constant -atmosphere searise_greenland -surface pdd"
 # coupler settings for spin-up (i.e. with forcing)
 COUPLER_AR4="-ocean constant -atmosphere searise_greenland,anomaly -surface pdd"
 
-echo "$SCRIPTNAME      executable = '$PISM'"
-echo "$SCRIPTNAME         tillphi = '$TILLPHI'"
-echo "$SCRIPTNAME    full physics = '$FULLPHYS'"
+echo
 echo "$SCRIPTNAME control coupler = '$COUPLER_CTRL'"
 echo "$SCRIPTNAME     AR4 coupler = '$COUPLER_AR4'"
 
 expackage="-extra_vars usurf,topg,thk,bmelt,bwat,bwp,mask,uvelsurf,vvelsurf,wvelsurf,uvelbase,vvelbase,wvelbase,tempsurf,tempbase,diffusivity,acab"
 tspackage="-ts_vars ivol,iareag,iareaf"
-
-SKIP=200
 
 
 STARTTIME=0
