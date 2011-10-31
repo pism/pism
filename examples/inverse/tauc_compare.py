0b69043e9309c7a5311a99d9eadfd4eb94262dc2
Ed Bueler <elbueler@alaska.edu>
2011-10-31 04:41:29 +0000

    Added visualization that I found helpful.

--- examples/inverse/tauc_compare.py
+++ examples/inverse/tauc_compare.py
@@ -3,6 +3,7 @@ try:
 except:
   import netCDF3 as netCDF
 from matplotlib import pyplot as pp
+from matplotlib import colors as mc
 from optparse import OptionParser
 from siple.reporting import endpause
 import PISM
@@ -36,6 +37,7 @@ tauc_diff[not_ice] = 0.
 
 u_computed = ds.variables['u_computed'][...].squeeze()*PISM.secpera
 v_computed = ds.variables['v_computed'][...].squeeze()*PISM.secpera
+cbase_computed = np.sqrt(u_computed * u_computed + v_computed * v_computed)
 
 not_sliding = np.logical_and( (abs(u_computed) < 10.) , (abs(v_computed) < 10.) )
 tauc[not_ice]  = 0
@@ -52,18 +54,31 @@ if not options.cap is None:
   # 
   # tauc_true = np.maximum(tauc_true,-10*C)
   # tauc_true = np.minimum(tauc_true,10*C)
-  
 
+# difference figure  
+pp.clf()
 pp.imshow(tauc_diff.transpose()/tauc_true.transpose(),origin='lower')
+pp.title(r'$(\tau_c$ - true) / true')
 pp.colorbar()
 
+# side-by-side comparison like from 'vel2tauc.py -inv_final_plot'
 pp.figure()
-pp.imshow(tauc.transpose(),origin='lower')
+pp.subplot(1,2,1)
+pp.imshow(tauc.transpose(),origin='lower',vmin=0.0,vmax=200000.0)
+pp.title(r'$\tau_c$  [from inversion]')
+pp.colorbar()
+pp.subplot(1,2,2)
+pp.imshow(tauc_true.transpose(),origin='lower',vmin=0.0,vmax=200000.0)
+pp.title(r'true $\tau_c$   [prior]')
 pp.colorbar()
 
+# show computed sliding speed
 pp.figure()
-pp.imshow(tauc.transpose(),origin='lower')
-pp.colorbar()
+im = pp.imshow(cbase_computed.transpose(),origin='lower',
+               norm=mc.LogNorm(vmin=0.1, vmax=1000.0))
+pp.title('computed sliding speed')
+t = [0.1, 1.0, 10.0, 100.0, 1000.0]
+pp.colorbar(im, ticks=t, format='$%.1f$')
 
 # pp.ion()
 pp.show()
