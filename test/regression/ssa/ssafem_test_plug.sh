5e182da6f7d7cb587c28e9583e95bd2374330a3b
David Maxwell <damaxwell@alaska.edu>
2012-01-29 13:13:42 -0900

    Addressed weirdness with mpiexec and ssa python tests: Call mpiexec without an absolute path and it inherits a PATH from the environment. Call it with an absolute path  (/usr/bin/mpiexec) and you set the PATH to /usr/bin. Our tests call mpiexec with an absolute path to ensure we use the correct version, but this means there is a path side effect. Calling a python script with a shebang with mpiexec can then result in a different python executable being used than the user's standard environment would find.  If a different python version is picked up (e.g. 2.6 rather than 2.7), you can fail to find python modules such as PISM and petsc4py if they were compiled for a mismatched version of python. Consequently, tests fail. Now any mpiexec calls in the test suite that run a python script actually run python explicitly, with an absolute path to the user's python executable. (i.e /usr/bin/mpiexec /opt/local/bin/python my_test.py rather than /usr/bin/mpiexec mytest.py)
    Fixed bug in parallel SSA test reporting messages.
    
    All SSA tests are now run in parallel.  Only prose was being run in parallel.  Maybe I unintentionally broke this last summer.

--- test/regression/ssa/ssafem_test_plug.sh
+++ test/regression/ssa/ssafem_test_plug.sh
@@ -4,10 +4,13 @@
 
 PISM_PATH=$1
 MPIEXEC=$2
+MPIEXEC_COMMAND="$MPIEXEC -n 2"
 PISM_SOURCE_DIR=$3
 EXT=""
 if [ $# -ge 4 ] && [ "$4" == "-python" ]
 then
+  PYTHONEXEC=$5
+  MPIEXEC_COMMAND="$MPIEXEC_COMMAND $PYTHONEXEC"
   PYTHONPATH=${PISM_PATH}
   PISM_PATH=${PISM_SOURCE_DIR}/examples/python/ssa_tests
   EXT=".py"
@@ -23,8 +26,8 @@ set -e
 OPTS="-verbose 1 -ssa_method fem -o foo.nc"
 
 # do stuff
-$PISM_PATH/ssa_test_plug${EXT} -Mx 22 -My 31 $OPTS > test-out.txt
-$PISM_PATH/ssa_test_plug${EXT} -Mx 61 -My 61 $OPTS >> test-out.txt
+$MPIEXEC_COMMAND $PISM_PATH/ssa_test_plug${EXT} -Mx 22 -My 31 $OPTS > test-out.txt
+$MPIEXEC_COMMAND $PISM_PATH/ssa_test_plug${EXT} -Mx 61 -My 61 $OPTS >> test-out.txt
 
 set +e
 
