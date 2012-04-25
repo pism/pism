7d421acc053bcc8bfc97c867952d14afebe3e9e7
Ed Bueler <elbueler@alaska.edu>
2012-04-25 11:51:59 -0800

    Have a run that runs.  Need more diagnostics to proceed ;)

--- examples/jako/preprocess.sh
+++ examples/jako/preprocess.sh
@@ -17,8 +17,6 @@ DATASIZE=80Mb
 
 echo "fetching $DATASIZE master file $DATANAME ... "
 wget -nc ${DATAURL}${DATANAME}
-echo "  ... done."
-echo
 
 WORKING=gr1km.nc
 echo "creating PISM-readable file $WORKING from master ..."
@@ -28,16 +26,31 @@ ncks -O $DATANAME $WORKING
 
 # remove time dimension
 ncwa -O -a t $WORKING $WORKING
-# create usurf
-ncap -O -s 'usurf=thk+topg' $WORKING $WORKING
 
-echo "copying geometry related fields to use as boundary conditions in no_model area..."
+# rename climate fields to work for "-atmosphere given -surface pdd"
+ncap -O -s "precipitation=presprcp*(1000.0/910.0)" $WORKING $WORKING
+ncatted -O -a units,precipitation,a,c,"m a-1" $WORKING
+ncatted -a standard_name,precipitation,d,, $WORKING  # remove standard_name; none available
+ncatted -O -a long_name,precipitation,a,c,"ice-equivalent mean annual precipitation rate" $WORKING
+ncatted -a standard_name,bheatflx,d,, $WORKING  # remove standard_name; none available
+ncks -O -v x,y,bheatflx,topg,thk,precipitation,presartm,mapping $WORKING $WORKING
+
+# create usurf, needed by regional-tools not pismo
+ncap -O -s 'usurf=thk+topg' $WORKING $WORKING
+ncatted -O -a units,usurf,a,c,"m" $WORKING
+ncatted -O -a long_name,usurf,a,c,"ice surface elevation" $WORKING
+
+echo "copying geometry fields as boundary conditions in no_model area..."
 # create fields thkstore and usrfstore so that pismo is able to appropriately
 # assign Dirichlet b.c. for surface gradient & driving stress
 ncap -O -s "usurfstore=1.0*usurf" $WORKING $WORKING
 ncatted -a standard_name,usurfstore,d,, $WORKING # remove it
+ncatted -O -a units,usurfstore,a,c,"m" $WORKING
+ncatted -O -a long_name,usurfstore,a,c,"stored ice surface elevation for regional boundary condition" $WORKING
 ncap -O -s "thkstore=1.0*thk" $WORKING $WORKING
 ncatted -a standard_name,thkstore,d,, $WORKING # remove it
+ncatted -O -a units,thkstore,a,c,"m" $WORKING
+ncatted -O -a long_name,thkstore,a,c,"stored ice thickness for regional boundary condition" $WORKING
 
 echo "done with cleaning file $WORKING"
 echo "now do 'python pism_regional.py' and open $WORKING"
