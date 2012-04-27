2486165495b00e39205d908af0a35d8e6ecbeaf7
Ed Bueler <elbueler@alaska.edu>
2012-04-27 14:49:01 -0800

    minor edits to run.sh as a way of keeping notes

--- examples/ross/run.sh
+++ examples/ross/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# -no_sia takes care of high velocities at inland boundaries (SIA "sees" discontinuities of surface and bedrock elevation fields).
-# -ssa_sliding has a twofold effect: 1) essentially prescribing the 0 velocity inland and 2) (potentially) allowing some sliding over the Roosevelt island
+# -no_sia takes care of high velocities at inland boundaries (SIA "sees"
+#     discontinuities of surface and bedrock elevation fields).
+# -ssa_sliding has a twofold effect: 1) essentially prescribing the zero
+#     velocity inland and 2) (potentially) allowing some sliding over
+#     Roosevelt island
 # -pik sets the calving front boundary condition
+#     (and can be replaced by '-cfbc -part_grid -kill_icebergs' but
+#     NOT(?) '-cfbc' alone or '-cfbc -part_grid'j or '-cfbc -kill_icebergs')
+# -ssa_method fem seems not to work?
 mpiexec -n 4 pismr -boot_file Ross_combined.nc -Mx 211 -My 211 -Mz 21 -Lz 3000 \
-                   -no_sia -ssa_sliding -cfbc -kill_icebergs -ssa_dirichlet_bc -y 0 -o out.nc -no_energy \
-                   -o_order zyx -surface given
+  -surface given -no_sia -no_energy \
+  -ssa_sliding -pik -ssa_dirichlet_bc \
+  -y 0 -o out.nc -o_order zyx
+
