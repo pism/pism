63fc9b1c7028d16c7d6aa735c0ca5b18b41d7512
Ed Bueler <elbueler@alaska.edu>
2012-12-04 10:40:12 -0900

    the way I want to test

--- testhydro.sh
+++ testhydro.sh
@@ -5,9 +5,18 @@
 MPIDO="mpiexec -n 4"
 PATHTO="./"
 
-$MPIDO ${PATHTO}pisms -o stage1.nc -y 10
-$MPIDO ${PATHTO}pismr -i stage1.nc -ye 6000 -o stage2-tillcan.nc
-$MPIDO ${PATHTO}pismr -i stage1.nc -diffuse_bwat -ye 6000 -o stage2-diffusebwat.nc
+if [ "" ]; then      # zero length string is false
+#if [ "pre" ]; then
+  $MPIDO ${PATHTO}pisms -Mx 121 -My 121 -Mz 101 -y 10 -o stage1.nc
+  $MPIDO ${PATHTO}pismr -i stage1.nc -ye 8000 -o stage2-tillcan.nc
+  cp stage2-tillcan.nc start.nc
+fi
+
+# only goal of this test script is to *LOOK* at the results; verification is another matter
+$MPIDO ${PATHTO}pismr -i start.nc                        -y 100 -o tillcan.nc
+#$MPIDO ${PATHTO}pismr -i start.nc -diffuse_bwat          -y 100 -o diffbwat.nc
+#$MPIDO ${PATHTO}pismr -i start.nc -distributed_hydrology -y 100 -o distributed.nc
+#$MPIDO ${PATHTO}pismr -i start.nc -lakes_hydrology       -y 100 -o distributed.nc
 
 #set +v
 
