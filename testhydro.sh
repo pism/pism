381c2699534ecb3751b4a938da17ebab0c6a280a
Ed Bueler <elbueler@alaska.edu>
2012-12-06 14:46:36 -0900

    update test script to new options and 20a runs

--- testhydro.sh
+++ testhydro.sh
@@ -7,16 +7,16 @@ PATHTO="./"
 
 if [ "" ]; then      # zero length string is false
 #if [ "pre" ]; then  # <-- do this to create "start.n" for comparison runs
-  $MPIDO ${PATHTO}pisms -Mx 121 -My 121 -Mz 101 -y 10 -o stage1.nc
-  $MPIDO ${PATHTO}pismr -i stage1.nc -ye 8000 -o stage2-tillcan.nc
-  cp stage2-tillcan.nc start.nc
+  $MPIDO ${PATHTO}pisms -Mx 121 -My 121 -Mz 101 -y 10 -o pre.nc
+  $MPIDO ${PATHTO}pismr -i pre.nc -ye 8000 -o start.nc
 fi
 
 # only goal of this test script is to *LOOK* at the results; verification is another matter
-$MPIDO ${PATHTO}pismr -i start.nc                        -y 100 -o tillcan.nc
-$MPIDO ${PATHTO}pismr -i start.nc -diffuse_bwat          -y 100 -o diffuse.nc
-#$MPIDO ${PATHTO}pismr -i start.nc -lakes_hydrology       -y 100 -o lakes.nc
-#$MPIDO ${PATHTO}pismr -i start.nc -distributed_hydrology -y 100 -o distributed.nc
+#$MPIDO ${PATHTO}pismr -i start.nc                        -y 20 -o tillcandefault.nc
+$MPIDO ${PATHTO}pismr -i start.nc -hydrology tillcan     -y 20 -o tillcan.nc
+$MPIDO ${PATHTO}pismr -i start.nc -hydrology diffuseonly -y 20 -o diffuse.nc
+$MPIDO ${PATHTO}pismr -i start.nc -hydrology lakes       -y 20 -o lakes.nc
+#$MPIDO ${PATHTO}pismr -i start.nc -hydrology distributed -y 20 -o distributed.nc
 
 #set +v
 
