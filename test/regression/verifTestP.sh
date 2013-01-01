00b7a6b71cb5623e278c78e5ffd92f29a91dd36a
Ed Bueler <elbueler@alaska.edu>
2013-01-01 07:43:27 -0900

    amend last commit with execute permission

--- test/regression/verifTestP.sh
+++ test/regression/verifTestP.sh
@@ -5,6 +5,7 @@ for M in 26 51 101 201;
 do
   rm -f foo.txt
   ./test_29.py ../../buildbwp "mpiexec -n 4" $M &> foo.txt
+  echo "results for M=${M}:"
   cat foo.txt |grep errbwat
   cat foo.txt |grep errbwp
 done
