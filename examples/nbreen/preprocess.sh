af0334263d307090f98f0719e27d47e8abed2065
Ed Bueler <elbueler@alaska.edu>
2013-01-01 13:54:55 -0900

    scripts cleaned up; basically run as expected

--- examples/nbreen/preprocess.sh
+++ examples/nbreen/preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# depends on NCO (ncrename, ncap, ncatted, ncpdq, ncks)
+# depends on NCO (http://nco.sourceforge.net/)
 
 set -e  # exit on error
 
@@ -11,7 +11,6 @@ echo "making PISM-readable file $PISMDATA by copying $DATA and adjusting metadat
 rm -f $PISMDATA
 cp $DATA $PISMDATA
 
-# following use NCO (http://nco.sourceforge.net/)
 ncap2 -O -s "climatic_mass_balance=0.0*topg" $PISMDATA $PISMDATA
 ncatted -O -a units,climatic_mass_balance,m,c,"m year-1" $PISMDATA
 ncatted -O -a standard_name,climatic_mass_balance,m,c,"land_ice_surface_specific_mass_balance" $PISMDATA
@@ -20,6 +19,13 @@ ncatted -O -a units,ice_surface_temp,m,c,"K" $PISMDATA
 ncatted -O -a standard_name,ice_surface_temp,d,c, $PISMDATA
 # Phi0(i,j) = (3.0/900.0)*(900.0-min(usurf(i,j),900.0))/p.spera;
 ncap2 -O -s "bmelt=(3.0/900.0)*(900.0-usurf)" $PISMDATA $PISMDATA
+ncap2 -O -s "where(usurf>900.0) bmelt=0.0" $PISMDATA $PISMDATA
+# outline(i,j)>0.5, Phi(i,j) = Phi0(i,j); end
+ncap2 -O -s "where(nbreen<=0.5) bmelt=0.0" $PISMDATA $PISMDATA
 ncatted -O -a units,bmelt,m,c,"m year-1" $PISMDATA
-echo "  PISM-readable file $PISMDATA created."
+
+CONF=nbreen_config
+echo "creating PISM-readable config override file $CONF.nc ..."
+rm -f $CONF.nc
+ncgen -o $CONF.nc $CONF.cdl
 
--- examples/nbreen/run.sh
+++ examples/nbreen/run.sh
@@ -2,15 +2,28 @@
 
 NN=4
 
-rm -f nbreen.nc
-ncgen -o nbreen_config.nc nbreen_config.cdl
-
-# -topg_to_phi 5.0,20.0,-300.0,700.0
-
+#dx=125
 #myMx=264
 #myMy=207
-myMx=133
-myMy=104
 
-mpiexec -n $NN pismo -config_override nbreen_config.nc -no_mass -no_energy -hydrology distributed -report_mass_accounting -no_model_strip 1.0 -boot_file pismnbreen.nc -skip -skip_max 10 -Mx $myMx -My $myMy -Mz 121 -z_spacing equal -Lz 600 -max_dt 0.02 -y 1.0 -o nbreen_y1.nc
+#dx=250
+#myMx=133
+#myMy=104
+
+dx=500
+myMx=67
+myMy=52
+
+grid="-Mx $myMx -My $myMy -Mz 11 -z_spacing equal -Lz 600"
+
+physics="-config_override nbreen_config.nc -no_mass -no_energy"
+
+hydro="-hydrology distributed -report_mass_accounting"
+
+pismexec="pismo -no_model_strip 1.0"
+
+YY=1
+
+mpiexec -n $NN $pismexec -boot_file pismnbreen.nc $physics $hydro \
+  $grid -max_dt 0.1 -y $YY -o nbreen_y${YY}_${dx}m.nc
 
