62e7bd5d35fe954d23118fe7a63351d3f811d97e
Ed Bueler <elbueler@alaska.edu>
2013-01-10 19:50:41 -0900

    put back with nice improvements from Constantine

--- test/test_hydrology/verifTestP.sh
+++ test/test_hydrology/verifTestP.sh
@@ -7,8 +7,9 @@ pismdir=../../buildbwp
 for MM in 11 21 41 81;
 do
   rm -f foo.txt
-  ./runTestP.py $pismdir "mpiexec -n ${NN}" $MM &> foo.txt
+  #./runTestP.py $pismdir "mpiexec -n ${NN}" $MM &> foo.txt
+  ./runTestP.py --pism_path=$pismdir --mpiexec="mpiexec -n ${NN}" --Mx=$MM &> foo.txt
   echo "results for Mx=My=${MM}:"
-  cat foo.txt |grep errbw
+  cat foo.txt |grep "Drift in"
 done
 
