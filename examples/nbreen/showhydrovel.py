6aea86e1ffe2e364c622567326c8eae28e91ca1f
Ed Bueler <elbueler@alaska.edu>
2013-03-18 09:59:06 -0800

    plot script in good shape

--- examples/nbreen/showhydrovel.py
+++ examples/nbreen/showhydrovel.py
@@ -2,35 +2,38 @@
 
 from numpy import *
 from matplotlib.pyplot import *
-from sys import exit
+
+import sys
+import argparse
 
 try:
     from netCDF4 import Dataset as NC
 except:
     from netCDF3 import Dataset as NC
 
-import argparse
 
 parser = argparse.ArgumentParser( \
     description='show quiver for the subglacial water velocity field (bwatvel) from a PISM file')
 parser.add_argument('filename',
                     help='file from which to get bwatvel')
-parser.add_argument('-c', nargs=4, type=int, metavar='x',
-                    help='crop to rectangle [xmin xmax ymin ymax]; in km')
+parser.add_argument('-d', type=int, default=-1,
+                    help='index of frame (defaults to last frame D=-1)')
 parser.add_argument('-s', action='store_true',
                     help='show second figure with pcolor on components of velocity')
 parser.add_argument('-t', action='store_true',
-                    help='use transpose on x,y axes')
+                    help='transpose the x,y axes')
+parser.add_argument('-x', action='store_true',
+                    help='reverse order on x-axis')
+parser.add_argument('-y', action='store_true',
+                    help='reverse order on y-axis')
 
 args = parser.parse_args()
 
 try:
     nc = NC(args.filename, 'r')
 except:
-    print "  ERROR: can't read from file ..."
-    exit(1)
-
-#FIXME:  add option to crop to rectangle given in km
+    print "ERROR: can't read from file ..."
+    sys.exit(1)
 
 print "  reading x,y axes from file %s ..." % (args.filename)
 if args.t:
@@ -46,25 +49,42 @@ print "  reading 'bwatvel[2]' field from file %s ..." % (args.filename)
 try:
     velx = nc.variables["bwatvel[0]"]
 except:
-    print "  ERROR variable 'bwatvel[0]' not found ..."
-    exit(2)
+    print "ERROR: variable 'bwatvel[0]' not found ..."
+    sys.exit(2)
 try:
     vely = nc.variables["bwatvel[1]"]
 except:
-    print "  ERROR variable 'bwatvel[1]' not found ..."
-    exit(3)
+    print "ERROR: variable 'bwatvel[1]' not found ..."
+    sys.exit(3)
 
-units = "m hr-1"  #FIXME: make this merely the default scale
+if args.d >= 0:
+   if shape(velx)[0] <= args.d:
+       print "ERROR: frame %d not available in variable velx" % args.d
+       sys.exit(3)
+   if shape(vely)[0] <= args.d:
+       print "ERROR: frame %d not available in variable vely" % args.d
+       sys.exit(4)
+   print "  reading frame %d from velocity with %d frames" % (args.d, shape(velx)[0])
+else:
+   args.d = -1
+   print "  reading last frame from velocity with %d frames" % (shape(velx)[0])
+
+units = "m hr-1"  #FIXME: make this merely the default scale?
 scale = 3.1556926e7 / 3600.0
-velx = asarray(squeeze(velx[-1,:,:])).transpose() / scale
-vely = asarray(squeeze(vely[-1,:,:])).transpose() / scale
+velx = asarray(squeeze(velx[args.d,:,:])).transpose() / scale
+vely = asarray(squeeze(vely[args.d,:,:])).transpose() / scale
+nc.close()
 
 if args.t:
     xytmp = velx.copy()
     velx = vely.transpose()
     vely = xytmp.transpose()
-
-print args.c
+if args.x:
+    x = x[::-1]
+    velx = -velx
+if args.y:
+    y = y[::-1]
+    vely = -vely
 
 if args.s:
     figure(2)
@@ -76,7 +96,7 @@ if args.s:
         else:
             data = vely
             name = "vely"
-        print "  %s stats:  min = %9.3f %s,  max = %9.3f %s,  av = %8.3f %s" % \
+        print "  %s stats:\n    min = %9.3f %s,  max = %9.3f %s,  av = %8.3f %s" % \
               (name,data.min(),units,data.max(),units,data.sum()/(x.size*y.size),units)
         subplot(1,2,j)
         pcolor(x/1000.0,y/1000.0,data,vmin=data.min(),vmax=data.max())
@@ -99,8 +119,4 @@ ylabel('y  (km)')
 
 show()
 
-#savefig(pngfilename, dpi=300, bbox_inches='tight')
-#close()
-
-nc.close()
 print "  done."
