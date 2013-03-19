dc222a904ad7fbc34461baa268b346cf3b306c79
Constantine Khroulev <ckhroulev@alaska.edu>
2013-03-19 15:01:12 -0800

    Removes an unneeded class member.

--- src/coupler/atmosphere/PAGivenClimate.hh
+++ src/coupler/atmosphere/PAGivenClimate.hh
@@ -41,8 +41,6 @@ public:
   virtual PetscErrorCode init_timeseries(PetscReal *ts, int N);
   virtual PetscErrorCode temp_time_series(int i, int j, PetscReal *values);
   virtual PetscErrorCode precip_time_series(int i, int j, PetscReal *values);
-protected:
-  vector<PetscReal> ts_mod;
 private:
   virtual PetscErrorCode allocate_PAGivenClimate();
 };
