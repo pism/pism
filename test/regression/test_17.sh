4c7a72acc0d3331b897006ae42d27cb951a01339
Constantine Khroulev <ckhroulev@alaska.edu>
2013-04-11 12:34:14 -0800

    Re-organizes the src/stressbalance directory and updates the test G regression script.

--- test/regression/test_17.sh
+++ test/regression/test_17.sh
@@ -19,23 +19,23 @@ $PISM_PATH/pismv -Mx 41 -My 41 $OPTS  >> test_17-G-out.txt
 diff test_17-G-out.txt -  <<END-OF-OUTPUT
 NUMERICAL ERRORS evaluated at final time (relative to exact solution):
 geometry  :    prcntVOL        maxH         avH   relmaxETA
-               0.780389   32.423434    7.148981    0.016282
+               0.780389   32.443567    7.149022    0.016300
 temp      :        maxT         avT    basemaxT     baseavT
-               0.835721    0.250719    0.765509    0.153551
+               0.835466    0.249417    0.747461    0.151139
 Sigma     :      maxSig       avSig
-               8.715311    1.012199
+               7.208338    0.955225
 surf vels :     maxUvec      avUvec        maxW         avW
-               0.944746    0.200100    0.028355    0.004030
+               0.945140    0.199932    0.028361    0.004027
 NUM ERRORS DONE
 NUMERICAL ERRORS evaluated at final time (relative to exact solution):
 geometry  :    prcntVOL        maxH         avH   relmaxETA
-               0.782888   30.761282    7.242044    0.015465
+               0.782888   30.772167    7.242054    0.015477
 temp      :        maxT         avT    basemaxT     baseavT
-               0.895267    0.249156    0.757865    0.157201
+               0.894900    0.248302    0.746664    0.155312
 Sigma     :      maxSig       avSig
-               8.294728    0.954785
+               7.185588    0.913964
 surf vels :     maxUvec      avUvec        maxW         avW
-               0.885667    0.194997    0.027396    0.004221
+               0.886013    0.194881    0.027400    0.004219
 NUM ERRORS DONE
 END-OF-OUTPUT
 
