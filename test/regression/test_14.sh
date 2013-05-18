aa107b06b65bd019832a4dfaba0d81fbc236ea3b
Ed Bueler <elbueler@alaska.edu>
2013-05-17 16:40:49 -0800

    Update test E and test L regressions.  Very small changes.  Attempt to make test L faster by doing 21,31 grids instead of 31,43.

--- test/regression/test_14.sh
+++ test/regression/test_14.sh
@@ -21,15 +21,15 @@ $PISM_PATH/pismv -Mx 41 -My 41 -Mz 3 $OPTS >> test_14-E-out.txt
 diff test_14-E-out.txt -  <<END-OF-OUTPUT
 NUMERICAL ERRORS evaluated at final time (relative to exact solution):
 geometry  :    prcntVOL        maxH         avH   relmaxETA
-               0.848651  368.057068   32.711326    0.041252
+               0.848937  368.203505   32.724467    0.041279
 base vels :  maxvector   avvector  prcntavvec     maxub     maxvb
-                4.1538    0.22008     0.43350    4.0511    2.0747
+                4.1525    0.22000     0.43334    4.0495    2.0718
 NUM ERRORS DONE
 NUMERICAL ERRORS evaluated at final time (relative to exact solution):
 geometry  :    prcntVOL        maxH         avH   relmaxETA
-               0.352172  526.115752   26.312808    0.040403
+               0.352302  526.145853   26.314556    0.040406
 base vels :  maxvector   avvector  prcntavvec     maxub     maxvb
-                1.3782    0.10231     0.20151    1.3573    1.0278
+                1.3801    0.10244     0.20178    1.3599    1.0328
 NUM ERRORS DONE
 END-OF-OUTPUT
 
