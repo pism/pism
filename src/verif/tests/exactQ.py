5080f43f813604c9eb1d1be274918aa877697c5e
Ed Bueler <elbueler@alaska.edu>
2013-05-20 23:15:18 -0800

    The script is now better-suited for verification, but the PISM run is not so good.  Need to check on the ice hardness.

--- src/verif/tests/exactQ.py
+++ src/verif/tests/exactQ.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
-# Computes and plots exact similarity solution "test Q".  See equation
-# (3.19) in [\ref PeglerListerWorster2012] = PLW2012:
+# Computes exact similarity solution "test Q" at an initial time and after
+# a specified runtime.  Suggests a PISM run for this runtime.  The exact
+# solution for velocity and for thickness can be compared to PISM output.
+#
+# For the similarity solution see equation (3.19) in
+# [\ref PeglerListerWorster2012] = PLW2012:
 #   S. Pegler, J. Lister, and M. G. Worster, 2012.  Release of a viscous
 #   power-law fluid over an inviscid ocean", J. Fluid Mech. 700, 63--76.
+
+# notes:
+#   don't use  -pik  because it will do  -kill_icebergs  and the whole thing is an iceberg
+#   could use  -ssa_view_nuh  if desired
+
+# FIXME:  need to carefully set hardness
 
 from pylab import *
 import sys
@@ -14,58 +24,76 @@ try:
 except:
     from netCDF3 import Dataset as CDF
 
-#   $ ./exactQ.py -o foo.png ts_lab51.nc
-
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.usage = "usage: %prog Mx OUTFILE"
-parser.description = "A script which builds bootstrap file for Test Q."
+parser.usage = \
+"""%prog MX DURATION
 
+where MX       is number of grid points,
+      DURATION is time in years for run,
+
+Example:  Try this diagnostic-only run:
+  $ export MX=101 YY=0
+  $ ./exactQ.py $MX $YY
+  $ pismr -o outQ$MX.nc -y $YY -boot_file initQ$MX.nc -Mx $MX -My $MX -Mz 21 -Lz 1500 -z_spacing equal -surface given -no_sia -no_energy -ssa_floating_only -ssa_dirichlet_bc -cfbc -part_grid -part_redist -o_order zyx -ssa_e 1.0 -ssa_flow_law isothermal_glen
+"""
+parser.description = "A script which runs Test Q."
 (options, args) = parser.parse_args()
-
-nts = len(args)
-if (nts<2) | (nts>2):
-  print "ERROR; needs two arguments:  exactQ.py Mx OUTFILE"
-  print "  ... EXITING"
+if (len(args) < 2) | (len(args) > 2):
+  print "ERROR; exactQ.py needs two arguments; run with --help to see usage"
+  print "... EXITING"
   exit(-1)
-
-Mx = int(args[0])
-My = Mx
 
 SperA= 31556926.0
 
+Mx = int(args[0])
+My = Mx
+runtime = float(args[1]) * SperA
+
+ncfile = "initQ%d.nc" % Mx
+
+# basic parameters
 g    = 9.81
 rho  = 910.0    # density of ice; kg/m^3
 rhow = 1028.0   # density of ocean water; kg/m^3
-
-n = 3.0
+n    = 3.0
 barB = 1.9e8    # strength of shelf; Pa s^(1/3); from MacAyeal et al 1996;
-                # is this equal to \tilde mu or not?
+                # FIXME is this equal to \tilde mu or not?
 
-def timeQ(H0,R0):
-  m = (1.0/n) - 1.0
-  gprime = (rhow - rho) * g / rhow     # see just after (2.1) in PLW2012
-  nurescale = 3.0**(m/2) * barB / rho  # see just after (3.19) in PLW2012
-  C0 = 12.0 * nurescale / gprime       # (3.19)
-  t = (1.0 / (2.0 * n)) * (C0 / H0)**n # (3.19)
+# derived parameters
+m = (1.0/n) - 1.0
+gprime = (rhow - rho) * g / rhow       # see just after (2.1) in PLW2012
+nurescale = 3.0**(m/2.0) * barB / rho  # see just after (3.19) in PLW2012
+C0 = 12.0 * nurescale / gprime         # constant in (3.19) in PLW2012
+
+def timeQ(H0):
+  # invert the first formula in (3.19) in PLW2012 to get t = f(H0)
+  t = (1.0 / (2.0 * n)) * (C0 / H0)**n
   return t
+
+def geomvelQ(t,r,V):
+  # computes (3.19) in PLW2012, assuming t,V are scalar and r is array
+  # returns H,u with same size as r, but R is scalar
+  tnt = 2.0 * n * t
+  H = C0 * tnt**(-1.0/n) * ones(shape(r))
+  u = r / tnt
+  R = (V / (C0 * pi))**0.5 * tnt**(1.0/(2.0*n))
+  return H, u, R
 
 # similarity solution: choose dimensions, get told the time and volume
 H0 = 1000.0     # m
 R0 = 100.0e3    # m; 100 km
-t = timeQ(H0,R0)
 V = pi * R0**2 * H0
+t0 = timeQ(H0)
 
-print 'exact Test Q has the following parameters for the start time:'
-print '  time      t  = %.3e s = %f a' % (t, t/SperA)
-print '  volume    V  = %.3e m^3 = %.3e km^3' % (V, V/1.0e9)
+t = t0 + runtime
+
+print 'exact Test Q has the following parameters for the start time t=t0:'
+print '  time      t0 = %.3e s = %f a' % (t0, t0/SperA)
 print '  thickness H0 = %.3f m' % H0
-print '  radius    R0 = %.3f m = %.3f km' % (R0, R0/1.0e3)
-
-ncfile = args[1]
-
-print 'building PISM-bootstrapable file %s with Mx = %d and My = %d grid points ...' % (ncfile,Mx,My)
+print '  radius    R0 = %.3f km' % (R0/1.0e3)
+print 'building PISM bootstrap file %s with Mx = %d and My = %d grid points ...' % (ncfile,Mx,My)
 
 # set up the grid:
 Lx = 200.0e3
@@ -75,15 +103,26 @@ y = linspace(-Ly,Ly,My)
 [xx,yy] = meshgrid(x,y)         # if there were "ndgrid" in numpy?
 rr = sqrt(xx**2 + yy**2)
 
-# create fields
-#floatdepth = (rho / rhow) * H0
-#topg = zeros((Mx,My)) - depth + 10.0 - ...  # so it is floating
+fill_value = nan
+
+# create initial fields
 topg = zeros((Mx,My)) - 1200.0  # so it is floating
 artm = zeros((Mx,My)) + 263.15  # -10 degrees Celsius; just a guess
 acab = zeros((Mx,My))
 thk  = zeros((Mx,My))
-thk[xx**2 + yy**2 <= R0**2] = H0;
+thk[rr <= R0] = H0;
 zerossabc = zeros((Mx,My))
+
+# create exact solution fields
+thk_exact, c_exact, R_exact = geomvelQ(t,rr,V)
+thk_exact[rr > R_exact] = 0.0
+c_exact *= SperA
+c_exact[rr > R_exact] = 0.0
+
+print 'exact Test Q at time t=%f years is in these variables:' % (t / SperA)
+print '  c_exact, with max = %.3e' % c_exact.max()
+print '  thk_exact, with max = %.3e' % thk_exact.max()
+print 'and R_exact = %.3f km' % (R_exact / 1.0e3)
 
 # Write the data:
 nc = CDF(ncfile, "w",format='NETCDF3_CLASSIC') # for netCDF4 module
@@ -103,8 +142,6 @@ y_var.units = "m";
 y_var.long_name = "northing"
 y_var.standard_name = "projection_y_coordinate"
 y_var[:] = y
-
-fill_value = nan
 
 def def_var(nc, name, units, fillvalue):
     # dimension transpose is standard: "float thk(y, x)" in NetCDF file
@@ -136,6 +173,11 @@ v_ssa_bc_var[:] = zerossabc.copy()
 bcflag_var = nc.createVariable("bcflag", "i", dimensions=("y", "x"))
 bcflag_var[:] = ((xx == 0.0) & (yy == 0.0))
 
+thk_exact_var = def_var(nc, "thk_exact", "m", fill_value)
+thk_exact_var[:] = thk_exact
+
+c_exact_var = def_var(nc, "c_exact", "m year-1", fill_value)
+c_exact_var[:] = c_exact
 
 # set global attributes
 nc.Conventions = 'CF-1.4'
@@ -144,16 +186,6 @@ historystr = time.asctime() + ': ' + historysep.join(sys.argv) + '\n'
 setattr(nc, 'history', historystr)
 
 nc.close()
-print('PISM-bootstrappable NetCDF file %s written' % ncfile)
+print('file %s written ...' % ncfile)
 print('  ... now run   FIXME')
-
-
-# don't use  -pik  because it will do  -kill_icebergs  and the whole thing is an iceberg
-
-# could use  -ssa_view_nuh  if desired
-
-# trial run:
-#   $ MX=101
-#   $ ./exactQ.py $MX initQ$MX.nc
-#   $ pismr -o outQ$MX.nc -boot_file initQ$MX.nc -Mx $MX -My $MX -Mz 21 -Lz 1500 -z_spacing equal -surface given -no_sia -no_energy -ssa_floating_only -ssa_dirichlet_bc -cfbc -part_grid -part_redist -y 0 -o_order zyx -ssa_e 1.0 -ssa_flow_law isothermal_glen
 
