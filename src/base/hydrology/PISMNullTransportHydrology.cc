3c2c0625489c900d9cf57ef80b9f7ce8261a4c6b
Ed Bueler <elbueler@alaska.edu>
2013-09-17 14:24:35 -0800

    In -hydrology null the floating or ice-free ocean areas have no water in till.

--- src/base/hydrology/PISMNullTransportHydrology.cc
+++ src/base/hydrology/PISMNullTransportHydrology.cc
@@ -16,6 +16,7 @@
 // along with PISM; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+#include "Mask.hh"
 #include "PISMHydrology.hh"
 #include "hydrology_diagnostics.hh"
 
@@ -43,13 +44,18 @@ PetscErrorCode PISMNullTransportHydrology::subglacial_water_pressure(IceModelVec
 }
 
 
-//! Update the till water thickness by simply integrating the melt input.  There is no tranportable water thickness variable and no interaction with it.
+//! Update the till water thickness by simply integrating the melt input.
 /*!
 Does an explicit (Euler) step of the integration
   \f[ \frac{\partial W_{til}}{\partial t} = \frac{m}{\rho_w} - C\f]
 where \f$C=\f$`hydrology_tillwat_decay_rate_null`.  Enforces bounds
 \f$0 \le W_{til} \le W_{til}^{max}\f$ where the upper bound is
 `hydrology_tillwat_max`.  Here \f$m/\rho_w\f$ is `total_input`.
+
+There is no attempt to report on conservation errors because the model does
+not conserve water.
+
+There is no tranportable water thickness variable and no interaction with it.
  */
 PetscErrorCode PISMNullTransportHydrology::update(PetscReal icet, PetscReal icedt) {
   // if asked for the identical time interval as last time, then do nothing
@@ -71,17 +77,21 @@ PetscErrorCode PISMNullTransportHydrology::update(PetscReal icet, PetscReal iced
     PISMEnd();
   }
 
-// FIXME: use mask.  if mask is ice-free ocean or floating then set to zero
-// (note that there is no "boundary_mass_changes()" method in PISMNullTransportHydrology)
-
+  MaskQuery M(*mask);
+  ierr = mask->begin_access(); CHKERRQ(ierr);
   ierr = Wtil.begin_access(); CHKERRQ(ierr);
   ierr = total_input.begin_access(); CHKERRQ(ierr);
   for (PetscInt i=grid.xs; i<grid.xs+grid.xm; ++i) {
     for (PetscInt j=grid.ys; j<grid.ys+grid.ym; ++j) {
-      Wtil(i,j) += icedt * (total_input(i,j) - C);
-      Wtil(i,j) = PetscMin(PetscMax(0.0, Wtil(i,j)), Wtilmax);
+      if (M.ocean(i,j)) {
+        Wtil(i,j) = 0.0;
+      } else {
+        Wtil(i,j) += icedt * (total_input(i,j) - C);
+        Wtil(i,j) = PetscMin(PetscMax(0.0, Wtil(i,j)), Wtilmax);
+      }
     }
   }
+  ierr = mask->end_access(); CHKERRQ(ierr);
   ierr = Wtil.end_access(); CHKERRQ(ierr);
   ierr = total_input.end_access(); CHKERRQ(ierr);
   return 0;
