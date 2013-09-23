e0c11a55b73316d656e75d69d3a600086cab8b4c
Ed Bueler <elbueler@alaska.edu>
2013-09-22 23:13:49 -0800

    Testing routing is interesting.

--- examples/searise-greenland/testrouting.sh
+++ examples/searise-greenland/testrouting.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-N=4
+N=6
 
 PREFIX=g10km
 
 climate="-atmosphere searise_greenland -surface pdd -ocean_kill pism_Greenland_5km_v1.1.nc"
 
-hydro="-hydrology routing -report_mass_accounting"
+hydro="-hydrology routing -report_mass_accounting -hydrology_tillwat_transfer_proportion 200.0"
 
 YY=50
 DT=0.25
 etimes="0:$DT:$YY"
-evarlist="thk,bmelt,hydroinput,bwat,bwp,bwatvel,wallmelt,tillwat"  # revised
 
 
 # work with ..._steady.nc file and use SIA
 PRESTART=${PREFIX}_steady
 STARTFILE=${PRESTART}_bwat.nc
 oname=${PREFIX}_routing.nc
+evarlist="thk,mask,csurf,bmelt,hydroinput,bwat,bwp,bwatvel,wallmelt,tillwat"
 diagnostics="-extra_file extras_$oname -extra_times $etimes -extra_vars $evarlist"
 ncap2 -O -s "bwat=0.0*tillwat" ${PRESTART}.nc $STARTFILE
 ncatted -O -a long_name,bwat,m,c,"effective thickness of transportable subglacial water" $STARTFILE
@@ -28,14 +28,15 @@ echo "TESTING -hydrology routing ON ${PRESTART}.nc:"
 echo
 
 cmd="mpiexec -n $N pismr -config_override searise_config.nc -i $STARTFILE $climate -ys 0 -y $YY $hydro $diagnostics -o $oname"
-$cmd
+#$cmd
 
-exit
+#exit
 
 # work with ..._0.nc file and use SIA+SSA
 PRESTART=${PREFIX}_0
 STARTFILE=${PRESTART}_bwat.nc
 oname=${PREFIX}_routing.nc
+evarlist="thk,mask,cbase,csurf,tauc,bmelt,hydroinput,bwat,bwp,bwatvel,wallmelt,tillwat"
 diagnostics="-extra_file extras_$oname -extra_times $etimes -extra_vars $evarlist"
 ncap2 -O -s "bwat=0.0*tillwat" ${PRESTART}.nc $STARTFILE
 ncatted -O -a long_name,bwat,m,c,"effective thickness of transportable subglacial water" $STARTFILE
