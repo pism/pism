bcc57fd8cf2cf326c819ecd963b26d0d79fe1a53
Constantine Khroulev <ckhroulev@alaska.edu>
2013-10-07 10:28:52 -0800

    Adds a Maxima script to support recent changes.
    This script (together with test_ssafd.py) shows that PISM's SSAFD code
    can handle "ice noses" just fine and we don't need to remove them. There
    are only 16 configurations that cause SSAFD failures:
    
    X O X
    O I O
    X O X
    
    i.e. an icy cell (I) with zero basal drag (floating, for example) with
    four ocean neighbors (O). It does not matter whether diagonal
    neighbors (X) are icy or ice-free.
    
    In these 16 cases the implementation of the calving front boundary
    conditions (CFBC) assembles the SSAFD matrix with zeros on the diagonal
    for all 4 equations (2 equations for the u component and 2 for v).

--- test/test_ssafd/test_ssafd.py
+++ test/test_ssafd/test_ssafd.py
@@ -3,7 +3,7 @@
 from PISMNC import PISMDataset as NC
 from optparse import OptionParser
 import numpy as np
-import subprocess, shlex
+import subprocess, shlex, sys
 
 parser = OptionParser()
 
@@ -13,16 +13,15 @@ parser.add_option("-o", dest="output_file_prefix", default="ssafd_test",
                   help="output file prefix")
 parser.add_option("-L", dest="L", type=float, default=10.0,
                   help="horizontal domain dimensions, km")
-parser.add_option("-M", dest="M", type=int, default=5,
-                  help="number of grid points per side")
 parser.add_option("-H", dest="H", type=float, default=500.0,
                   help="ice thickness in icy areas")
 
 (options, args) = parser.parse_args()
 
-M = options.M
+M = 3
 
-def generate_file(N):
+def generate_input(N):
+    """Generate a PISM bootstrapping file for a neighborhood number N."""
     output_filename = options.output_file_prefix + "_%08d.nc" % N
     pism_output_filename = options.output_file_prefix + "_o_%08d.nc" % N
     nc = NC(output_filename, 'w')
@@ -36,7 +35,7 @@ def generate_file(N):
     thk  = np.zeros(M * M)
 
     topg = zeros.copy() - 0.1*options.H
-    topg[M/2,M/2] = -options.H
+    topg[1,1] = -options.H
 
     for j in xrange(M*M):
         if string[j] == '1':
@@ -59,4 +58,4 @@ def run_pismr((input_filename, output_filename)):
     subprocess.call(shlex.split(command))
 
 for k in xrange(2**(M*M)):
-    run_pismr(generate_file(k))
+    run_pismr(generate_input(k))
