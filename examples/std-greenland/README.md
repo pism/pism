c7892861c89189c6310234a85321f3c3ac0b4b1c
Ed Bueler <elbueler@alaska.edu>
2013-11-03 09:56:20 -0900

    I think this is the preprocessing I want

--- examples/std-greenland/README.md
+++ examples/std-greenland/README.md
@@ -1,20 +1,23 @@
-# Greenland SeaRISE-like simulation example
+## "Standard" Greenland spinup examples
 
-The first section of the PISM _User's Manual_ documents this example.
+This directory contains scripts for standard Greenland ice dynamics simulations.
+The first section of the PISM _User's Manual_ documents these examples.  There
+is one major run script `spinup.sh` but it takes options to determine major (and
+minor) spinup choices.
 
-This directory contains scripts for a Greenland ice dynamics simulation which
-uses the SeaRISE data (see ) and which is similar to the PISM submitted runs
-for the actual SeaRISE process.  The submitted runs are described, in the
-context of evaluating and comparing all SeaRISE simulations, in
+These runs are motivated primarily by the "constant-climate" and "paleo-climate"
+runs which are evaluated in
 
-  * S. Nowicki and others (2013). _Insights into spatial sensitivities of ice_
-    _mass response to environmental change from the SeaRISE ice sheet modeling_
-    _project: II. Greenland._ J. Geophys. Res.: Earth Surface 118 (2),
-    doi:10.1002/jgrf.20076, 1025--1044.
+  * A. Aschwanden, G. Adalgeirsdottir, and C. Khroulev (2013). _Hindcasting to_
+    _measure ice sheet model sensitivity to initial states._ The Cryosphere 7,
+    doi:10.5194/tc-7-1083-2013, 1083--1093.
 
-  * R. Bindshadler and others (2013). _Ice-sheet model sensitivities to_
-    _environmental forcing and their use in projecting future sea-level_
-    _(The SeaRISE Project)._ J. Glaciol. 59 (214), 195--224.
+However, the runs here use 5km SeaRISE data including present-day bedrock
+topography, ice thickness, and basal heat flux; see references at bottom.  Also,
+in contrast to the above _Hindcasting_ paper, runs here use publicly-available
+RACMO atmospheric/ocean circulation model outputs, including modeled present-day
+2m air temperature, precipitation, and surface mass balance, as distributed by
+the SeaRISE project; see reference at bottom.
 
 ## Basic usage
 
@@ -24,8 +27,11 @@ First do
 
 This downloads the file `Greenland_5km_v1.1.nc` from the SeaRISE site and it
 builds PISM-readable NetCDF file `pism_Greenland_5km_v1.1.nc`.  It also
-generates additional climate-forcing files and it converts the configuration
-overrides file `searise_config.cdl` to get `searise_config.nc`.
+generates additional climate-forcing files `pism_dT.nc` and `pism_dSL.nc`
+and it converts the configuration overrides file `std_config.cdl` to get
+`std_config.nc`.
+
+FIXME
 
 To run the spinup in the default (coarsest) grid resolution on 8 processes, for
 example, and run it in the background and keep a copy of the stdout information
@@ -42,5 +48,33 @@ but without maintained documentation.
 
 ## Changing configuration constants
 
-Edit `searise_config.cdl` to set your preferred value and rerun `preprocess.sh`
-and then `spinup.sh` or `experiments.sh` as desired.
+Edit `std_config.cdl` to set your preferred value and rerun `preprocess.sh`
+and then `spinup.sh` as desired.
+
+# SeaRISE Greenland background information
+
+The SeaRISE assessment process included PISM Greenland simulations.  These runs
+are described in these papers, which evaluate and compare all SeaRISE
+simulations:
+
+  * S. Nowicki and others (2013). _Insights into spatial sensitivities of ice_
+    _mass response to environmental change from the SeaRISE ice sheet modeling_
+    _project: II. Greenland._ J. Geophys. Res.: Earth Surface 118 (2),
+    doi:10.1002/jgrf.20076, 1025--1044.
+
+  * R. Bindshadler and others (2013). _Ice-sheet model sensitivities to_
+    _environmental forcing and their use in projecting future sea-level_
+    _(The SeaRISE Project)._ J. Glaciol. 59 (214), 195--224.
+
+The reference for RACMO model outputs is:
+
+  * J. Ettema, M.R. van den Broeke, E. van Meigaard, W.J. van de Berg,
+    J.L. Bamber, J.E. Box, and R.C. Bales (2009). _Higher surface mass balance_
+    _of the Greenland ice sheet revealed by high-resolution climate modeling._
+    Geophys. Res. Lett. 36 (L12501), doi:10.1029/2009GL038110.
+
+Scripts for the actual PISM submitted runs to SeaRISE can be found in the
+`stable0.5` branch of PISM.  See
+
+    https://github.com/pism/pism/tree/stable0.5/examples/searise-greenland
+
--- examples/std-greenland/preprocess.sh
+++ examples/std-greenland/preprocess.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Copyright (C) 2009-2013 Ed Bueler and Andy Aschwanden
+# Copyright (C) 2009-2013 The PISM Authors
 
-# PISM SeaRISE Greenland
-#
-# downloads SeaRISE "Present Day Greenland" master dataset NetCDF file, adjusts metadata,
-# saves under new name, ready for PISM
+# Downloads SeaRISE "Present Day Greenland" master dataset NetCDF file, adjusts
+# metadata, and saves under new name ready for PISM.  Also converts .cdl for
+# configuration parameters to .nc form.  See README.md.
 
-# depends on wget and NCO (ncrename, ncap, ncatted, ncpdq, ncks)
+# depends on wget and NCO (ncrename, ncap, ncatted, ncpdq, ncks; see
+# http://nco.sourceforge.net/)
 
 set -e  # exit on error
 
 echo "# =================================================================================="
-echo "# PISM SeaRISE Greenland: preprocessing"
+echo "# PISM std Greenland example: preprocessing"
 echo "# =================================================================================="
 echo
 
@@ -22,77 +22,73 @@ DATAURL=http://websrv.cs.umt.edu/isis/images/a/a5/
 DATANAME=Greenland_5km_v$DATAVERSION.nc
 
 echo "fetching master file ... "
-wget -nc ${DATAURL}${DATANAME}
+wget -nc ${DATAURL}${DATANAME}   # -nc is "no clobber"
 echo "  ... done."
 echo
 
 PISMVERSION=pism_$DATANAME
-echo -n "creating bootstrapable $PISMVERSION from $DATANAME ..."
-ncks -O $DATANAME $PISMVERSION  # just copies over, but preserves history and global attrs
-
-# adjust metadata; uses NCO (http://nco.sourceforge.net/)
+echo -n "creating bootstrapable $PISMVERSION from $DATANAME ... "
+# copy the vars we want, and preserve history and global attrs
+ncks -O -v mapping,lat,lon,bheatflx,topg,thk,presprcp,smb,airtemp2m $DATANAME $PISMVERSION
 # convert from water equiv to ice thickness change rate; assumes ice density 910.0 kg m-3
 ncap2 -O -s "precipitation=presprcp*(1000.0/910.0)" $PISMVERSION $PISMVERSION
 ncatted -O -a units,precipitation,c,c,"m/year" $PISMVERSION
 ncatted -O -a long_name,precipitation,c,c,"ice-equivalent mean annual precipitation rate" $PISMVERSION
 # delete incorrect standard_name attribute from bheatflx; there is no known standard_name
 ncatted -a standard_name,bheatflx,d,, $PISMVERSION
-ncks -O -v lat,lon,bheatflx,topg,thk,precipitation,mapping \
+# use pism-recognized name for 2m air temp
+ncrename -O -v airtemp2m,ice_surface_temp $PISMVERSION
+ncatted -O -a units,ice_surface_temp,a,c,"Celsius" $PISMVERSION
+# use pism-recognized name and standard_name for surface mass balance, after
+# converting units
+ncap -O -s "climatic_mass_balance=(1000.0/910.0)*smb" $PISMVERSION $PISMVERSION
+ncatted -O -a standard_name,climatic_mass_balance,a,c,"land_ice_surface_specific_mass_balance" $PISMVERSION
+ncatted -O -a units,climatic_mass_balance,a,c,"meters/year" $PISMVERSION
+# de-clutter by only keeping vars we want
+ncks -O -v mapping,lat,lon,bheatflx,topg,thk,precipitation,ice_surface_temp,climatic_mass_balance \
   $PISMVERSION $PISMVERSION
 echo "done."
 echo
 
-# extract time series into files suitable for -atmosphere ...,delta_T and -ocean ...,delta_SL
-# compare resulting files to grip_dT.nc and specmap_dSL.nc in pism-dev/examples/eisgreen/
+# extract paleo-climate time series into files suitable for option
+# -atmosphere ...,delta_T
 TEMPSERIES=pism_dT.nc
-SLSERIES=pism_dSL.nc
-echo -n "creating paleo-temperature file $TEMPSERIES from $DATANAME for option -atmosphere ...,delta_T ... "
+echo -n "creating paleo-temperature file $TEMPSERIES from $DATANAME ... "
 ncks -O -v oisotopestimes,temp_time_series $DATANAME $TEMPSERIES
-ncrename -O -d oisotopestimes,time -v oisotopestimes,time -v temp_time_series,delta_T $TEMPSERIES
-ncpdq -O --rdr=-time $TEMPSERIES $TEMPSERIES  # reverse time dimension
-ncap2 -O -s "time=-time" $TEMPSERIES $TEMPSERIES  # make times follow same convention as PISM
+ncrename -O -d oisotopestimes,time \
+            -v oisotopestimes,time \
+            -v temp_time_series,delta_T $TEMPSERIES
+# reverse time dimension
+ncpdq -O --rdr=-time $TEMPSERIES $TEMPSERIES
+# make times follow same convention as PISM
+ncap2 -O -s "time=-time" $TEMPSERIES $TEMPSERIES
 ncatted -O -a units,time,m,c,"years since 1-1-1" $TEMPSERIES
 ncatted -O -a calendar,time,c,c,"365_day" $TEMPSERIES
 ncatted -O -a units,delta_T,m,c,"Kelvin" $TEMPSERIES
 echo "done."
 echo
-echo -n "creating paleo-sea-level file $SLSERIES from $DATANAME for option -ocean ...,delta_SL ... "
+
+# extract paleo-climate time series into files suitable for option
+# -ocean ...,delta_SL
+SLSERIES=pism_dSL.nc
+echo -n "creating paleo-sea-level file $SLSERIES from $DATANAME ... "
 ncks -O -v sealeveltimes,sealevel_time_series $DATANAME $SLSERIES
-ncrename -O -d sealeveltimes,time -v sealeveltimes,time -v sealevel_time_series,delta_SL $SLSERIES
-ncpdq -O --rdr=-time $SLSERIES $SLSERIES  # reverse time dimension
-ncap2 -O -s "time=-time" $SLSERIES $SLSERIES  # make times follow same convention as PISM
+ncrename -O -d sealeveltimes,time \
+            -v sealeveltimes,time \
+            -v sealevel_time_series,delta_SL $SLSERIES
+# reverse time dimension
+ncpdq -O --rdr=-time $SLSERIES $SLSERIES
+# make times follow same convention as PISM
+ncap2 -O -s "time=-time" $SLSERIES $SLSERIES
 ncatted -O -a units,time,m,c,"years since 1-1-1" $SLSERIES
 ncatted -O -a calendar,time,c,c,"365_day" $SLSERIES
 echo "done."
 echo
 
 # generate config file
-CONFIG=searise_config
+CONFIG=std_config
 echo -n "generating config file $CONFIG.nc from ascii file $CONFIG.cdl ... "
 ncgen -o ${CONFIG}.nc ${CONFIG}.cdl
-echo "done."
-echo
-
-echo "fetching anomaly files ..."
-echo "   (these files can be re-generated from scripts in subdirectory future_forcing/) "
-URL=http://www.pism-docs.org/download
-PRECIP=ar4_precip_anomaly.nc
-TEMP=ar4_temp_anomaly.nc
-wget -nc ${URL}/$PRECIP
-wget -nc ${URL}/$TEMP
-echo "  ... done."
-echo
-
-ANOMALY=ar4_anomaly
-echo -n "creating scaled anomaly files ... "
-ncks -O $PRECIP ${ANOMALY}.nc
-ncks -A $TEMP ${ANOMALY}.nc
-
-ncks -O ${ANOMALY}.nc ${ANOMALY}_scalefactor_1.0.nc
-
-ncap2 -O -s "precipitation_anomaly = 1.5 * precipitation_anomaly; air_temp_anomaly = 1.5 * air_temp_anomaly" ${ANOMALY}.nc ${ANOMALY}_scalefactor_1.5.nc
-ncap2 -O -s "precipitation_anomaly = 2.0 * precipitation_anomaly; air_temp_anomaly = 2.0 * air_temp_anomaly" ${ANOMALY}.nc ${ANOMALY}_scalefactor_2.0.nc
-
 echo "done."
 echo
 
