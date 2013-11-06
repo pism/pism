3659b52e6a2d1133ad67f970f557ec4c562d7d13
Constantine Khroulev <ckhroulev@alaska.edu>
2013-11-06 10:26:05 -0900

    Faster circular shelf setup script.

--- examples/piktests/circular/test_calving_at_thickness.sh
+++ examples/piktests/circular/test_calving_at_thickness.sh
@@ -4,23 +4,19 @@ N=4
 M=101
 xx=$M
 yy=$M
-length=400
+length=600
 
 infile="circular_noshelf.nc"
 
 output_basename="test_calving_at_thickness"
 
-if [[ ! -r $infile ]]
-then
-    echo "generating the input file..."
-    ./circular_dirichlet.py -o $infile
-fi
+./circular_dirichlet.py -o $infile
 
-grid="-Mx $xx -My $yy -Mz 31 -Mbz 5 -Lz 1500 -Lbz 1000"
+grid="-Mx $xx -My $yy -Mz 3 -Mbz 1 -Lz 1500 -Lbz 0"
 
-stressbalance="-ssa_method fd -ssa_sliding -ssa_dirichlet_bc -no_sia -ssafd_ksp_type gmres -ssafd_ksp_norm_type unpreconditioned -ssafd_ksp_pc_side right -ssafd_pc_type asm -ssafd_sub_pc_type lu"
+stressbalance="-ssa_method fd -ssa_sliding -ssa_dirichlet_bc -no_sia $pc"
 
-calving="-thickness_calving -calving_at_thickness 200"
+calving="-thickness_calving -calving_at_thickness 250"
 
 diagnostics="thk,mask,cbar,Href,velbar,discharge_flux_cumulative"
 
@@ -28,11 +24,12 @@ viewers="-view_map $diagnostics"
 
 extra="-extra_times 10 -extra_vars $diagnostics -extra_file ${output_basename}_ex.nc"
 
-misc_options="-cfbc -part_grid -part_redist -o_order zyx"
+misc_options="-cfbc -part_grid -part_redist -o_order zyx -no_energy"
 
 pismopts="-boot_file $infile $grid $stressbalance $calving $viewers $extra $misc_options"
 
 doit="mpiexec -n $N pismr $pismopts"
 
-# run with CFBC and part_grid
+set -x
+
 $doit $pismopts -y $length -o ${output_basename}_o.nc
