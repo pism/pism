9063dd75c65660d918c9c1c7e49e4439652691da
Ed Bueler <elbueler@alaska.edu>
2013-11-29 18:16:58 -0900

    continuing refinement of the gridseq.sh sample script

--- examples/std-greenland/gridseq.sh
+++ examples/std-greenland/gridseq.sh
@@ -3,8 +3,8 @@ NN=4
 
 export REGRIDFILE=g20km_10ka_hy.nc
 export EXSTEP=100
-./spinup.sh $NN const 1000  10 hybrid g10km_gridseq.nc
+./spinup.sh $NN const 2000  10 hybrid g10km_gridseq.nc
 
 export REGRIDFILE=g10km_gridseq.nc
 export EXSTEP=10
-./spinup.sh $NN const 100    5 hybrid  g5km_gridseq.nc
+./spinup.sh $NN const 200    5 hybrid  g5km_gridseq.nc
