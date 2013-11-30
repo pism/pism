57018ba8433eadee4f8142b5534ab315b75c6d69
Ed Bueler <elbueler@alaska.edu>
2013-11-29 16:45:03 -0900

    fix gridseq.sh script and edit text on NetCDF tools

--- examples/std-greenland/gridseq.sh
+++ examples/std-greenland/gridseq.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+NN=4
 
-NN=6
+export REGRIDFILE=g20km_10ka_hy.nc
+export EXSTEP=100
+./spinup.sh $NN const 1000  10 hybrid g10km_gridseq.nc
 
-export REGRIDFILE=
-export EXSTEP=500
-./spinup.sh $NN const 50000 20 hybrid g20km.nc
-
-export REGRIDFILE=g20km.nc
-export EXSTEP=50
-./spinup.sh $NN const 5000  10 hybrid g10km.nc
-
-export REGRIDFILE=g10km.nc
-export EXSTEP=1
-./spinup.sh $NN const 200    5 hybrid  g5km.nc
-
+export REGRIDFILE=g10km_gridseq.nc
+export EXSTEP=10
+./spinup.sh $NN const 100    5 hybrid  g5km_gridseq.nc
