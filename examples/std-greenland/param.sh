28ff9492afcf7f347d829476287e732df659454e
Ed Bueler <elbueler@alaska.edu>
2013-12-03 14:59:09 -0900

    scripts for parameter study running now; suitable for small machine (and supercomputer will be an exercise)

--- examples/std-greenland/param.sh
+++ examples/std-greenland/param.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-NN=2
-DUR=10000
+NN=4
+DUR=1000
+START=g10km_gridseq.nc
 
 for PPQ in 0.1 0.5 1.0 ; do
-  for SIAE in 1 3 9 ; do
-     PARAM_PPQ=$PPQ PARAM_SIAE=$SIAE ./spinup.sh $NN const $DUR 20 hybrid par20km_${PPQ}_${SIAE}.nc
+  for SIAE in 1 3 6 ; do
+     PISM_DO=echo REGRIDFILE=$START PARAM_PPQ=$PPQ PARAM_SIAE=$SIAE \
+     ./spinup.sh $NN const $DUR 10 hybrid p10km_${PPQ}_${SIAE}.nc \
+     &> p10km_${PPQ}_${SIAE}.sh
   done
 done
---
+++ examples/std-greenland/runsequentially.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for scriptname in $(ls p10km*sh) ; do
+  echo
+  echo "running ${scriptname} ..."
+  bash $scriptname                   # will wait for completion
+done
