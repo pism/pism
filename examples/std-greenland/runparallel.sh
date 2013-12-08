9c7b282c63eace159411751c1341bf8831096262
Ed Bueler <elbueler@alaska.edu>
2013-12-07 16:15:25 -0900

    revise/add some scripts for sections 2.8 and 2.9

---
+++ examples/std-greenland/runparallel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for scriptname in $(ls p10km*sh) ; do
+  echo
+  echo "starting ${scriptname} ..."
+  bash $scriptname &> out.$scriptname &  # start immediately in background
+done
---
+++ examples/std-greenland/tsshow.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+"""
+Creates graph of the modeled time series from multiple files.  See figures
+in the "Getting Started" section of the User's Manual.
+"""
+
+from numpy import *
+import pylab as plt
+import sys
+try:
+    import netCDF4 as netCDF
+except:
+    import netCDF3 as netCDF
+NC = netCDF.Dataset
+
+if len(sys.argv) < 5:
+    print "tsshow.py ERROR: at least 4 arguments needed"
+    print "usage:"
+    print
+    print "   $ python tsboth.py FIELD OUTIMAGE TSFILE1 LABEL1 ... TSFILEn LABELn"
+    print
+    print "where strings LABEL1 ... LABELn go in the legend"
+    print "example:"
+    print "   $ python tsshow.py ivol foo.png ts_g20km.nc '20 km' ts_g10km.nc '10 km'"
+    sys.exit(1)
+
+field = sys.argv[1]
+outimage = sys.argv[2]
+
+secpera = 31556926.0
+vfactor = 1.0e6 * 1.0e9
+
+n = (len(sys.argv) - 3) / 2
+labels = []
+plt.figure(figsize=(9,4))
+
+for k in range(n):
+
+  tsfile = sys.argv[2*k+3]
+  labels.append(sys.argv[2*k+4])
+
+  try:
+    ncfile = NC(tsfile, "r")
+  except:
+    print "ERROR: can't read from file %s ..." % tsfile
+    sys.exit(2)
+  t = ncfile.variables["time"][:] / secpera
+  var = ncfile.variables[field][:]
+  ncfile.close()
+  print "read variable '%s' from time-series file '%s' ..." % (field,tsfile)
+
+  plt.plot(t, var / vfactor, linewidth=2.5)  # automatic colors; default order
+                                             # blue, green, red, cyan, magenta, ...
+  plt.hold(True)
+
+plt.hold(False)
+plt.legend(labels, loc='lower right')
+plt.xlabel("t (years)", size=16)
+plt.ylabel("%s ($10^6$ km$^3$)" % field, size=16)
+plt.grid(True)
+print "saving image to file '%s' ..." % outimage
+plt.savefig(outimage, bbox_inches='tight')
+
