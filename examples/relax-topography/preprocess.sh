f71d7a0c1edc5b47e988246636551953811906c8
Constantine Khroulev <ckhroulev@alaska.edu>
2014-02-06 14:11:33 -0900

    Fix SMB units in examples/relax-topography. See #16.

--- examples/relax-topography/preprocess.sh
+++ examples/relax-topography/preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2009-2013 Ed Bueler and Andy Aschwanden
+# Copyright (C) 2009-2014 Ed Bueler and Andy Aschwanden
 
 # downloads SeaRISE "Present Day Greenland" master dataset NetCDF file, adjusts metadata,
 # saves under new name, ready for PISM
@@ -37,14 +37,16 @@ ncatted -O -a long_name,precipitation,c,c,"ice-equivalent mean annual precipitat
 # delete incorrect standard_name attribute from bheatflx; there is no known standard_name
 ncatted -a standard_name,bheatflx,d,, $PISMVERSION
 ncrename -O -v smb,climatic_mass_balance -v airtemp2m,ice_surface_temp $PISMVERSION $PISMVERSION
-ncks -O -v lat,lon,bheatflx,topg,thk,precipitation,mapping,\
-climatic_mass_balance,ice_surface_temp  $PISMVERSION $PISMVERSION
+ncks -O -v lat,lon,bheatflx,topg,thk,precipitation,mapping,climatic_mass_balance,ice_surface_temp  $PISMVERSION $PISMVERSION
+# convert from [m/year] ice equivalent to [kg m-2 year-1]; assumes ice density of 910.0 [kg m-3]
+ncap2 -O -s "climatic_mass_balance=climatic_mass_balance*910.0" $PISMVERSION $PISMVERSION
+ncatted -O -a units,climatic_mass_balance,m,c,"kg m-2 year-1" $PISMVERSION
 echo "done."
 echo
 
 echo "create target ice thickness file"
-ncap2 -O -s "thk=thk*0.; climatic_mass_balance[\$time,\$y1,\$x1]=-500.f" $PISMVERSION target_$PISMVERSION
-ncatted -a units,climatic_mass_balance,o,c,"m year-1" target_$PISMVERSION
+ncap2 -O -s "thk=thk*0.; climatic_mass_balance[\$time,\$y1,\$x1]=-500.f*910.0" $PISMVERSION target_$PISMVERSION
+ncatted -a units,climatic_mass_balance,o,c,"kg m-2 year-1" target_$PISMVERSION
 echo "done."
 echo
 
