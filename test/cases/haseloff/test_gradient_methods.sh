0408d3590f0f92dbeade8dbc39dd6898d92d929f
Constantine Khroulev <ckhroulev@alaska.edu>
2014-03-26 11:07:24 -0800

    Update the gradient method test case.

--- test/cases/haseloff/test_gradient_methods.sh
+++ test/cases/haseloff/test_gradient_methods.sh
@@ -7,14 +7,14 @@ GRID="-Lz 3500 -Mx 51 -My 51 -Mz 61 -y 500"
 OPTS="-no_temp -extra_vars thk,usurf,h_x,h_y -extra_times 0:10:500  -o_size big -o_order zyx"
 
 # With the bed smoother (default)
-for method in mahaffy eta haseloff new;
+for method in mahaffy eta haseloff;
 do
     mpiexec -n $NN pismr -boot_file $INFILE $GRID -gradient $method $OPTS -extra_file ex-$method.nc -o o-$method.nc
 done;
 
 # Without the bed smoother
 OPTS="$OPTS -config_override ./no_bed_smoother.nc"
-for method in mahaffy haseloff new;
+for method in mahaffy haseloff;
 do
-    mpiexec -n $NN pismr -boot_file $INFILE $GRID -gradient $method $OPTS -extra_file ex-ns-$method.nc -o o-ns-$method.nc
+    mpiexec -n $NN pismr -boot_file $INFILE $GRID -gradient $method $OPTS -extra_file ex-no-smoother-$method.nc -o o-no-smoother-$method.nc
 done;
