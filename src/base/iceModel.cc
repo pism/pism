872e6bc45d551be1b2987930d4685f766b9bf577
Ed Bueler <elbueler@alaska.edu>
2014-05-07 12:33:14 -0800

    Add _flux to standard name for climatic_mass_balance.  See 995aac.

--- src/base/iceModel.cc
+++ src/base/iceModel.cc
@@ -496,7 +496,7 @@ PetscErrorCode IceModel::createVecs() {
                         "climate_from_SurfaceModel",  // FIXME: can we do better?
                         "ice-equivalent surface mass balance (accumulation/ablation) rate",
                         "kg m-2 s-1",
-                        "land_ice_surface_specific_mass_balance");  // CF standard_name
+                        "land_ice_surface_specific_mass_balance_flux");  // CF standard_name
   CHKERRQ(ierr);
   ierr = climatic_mass_balance.set_glaciological_units("kg m-2 year-1"); CHKERRQ(ierr);
   climatic_mass_balance.write_in_glaciological_units = true;
