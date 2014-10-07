pism/pism

commit 6f1fe04e8b802b09cc3f647bf492c1148f489d7b

Add an assertion (PISMRoutingHydrology::V has to have ghosts).

src/base/hydrology/PISMRoutingHydrology.cc
@@ -619,6 +619,8 @@ PetscErrorCode RoutingHydrology::advective_fluxes(IceModelVec2Stag &result) {
   list.add(V);
   list.add(result);
 
+  assert(V.get_stencil_width() >= 1);
+
   for (Points p(grid); p; p.next()) {
     const int i = p.i(), j = p.j();
 
@@ -852,7 +854,7 @@ PetscErrorCode RoutingHydrology_bwatvel::compute(IceModelVec* &output) {
   PetscErrorCode ierr;
 
   IceModelVec2Stag *result = new IceModelVec2Stag;
-  ierr = result->create(grid, "bwatvel", WITH_GHOSTS); CHKERRQ(ierr);
+  ierr = result->create(grid, "bwatvel", WITHOUT_GHOSTS); CHKERRQ(ierr);
   result->metadata(0) = vars[0];
   result->metadata(1) = vars[1];
   result->write_in_glaciological_units = true;

commit c1ed681bab71383a5e4593571a1af22abf172e69

Fix a typo introduced in 6f1fe04.
(V does not need ghosts, W does.)

src/base/hydrology/PISMRoutingHydrology.cc
@@ -619,7 +619,7 @@ PetscErrorCode RoutingHydrology::advective_fluxes(IceModelVec2Stag &result) {
   list.add(V);
   list.add(result);
 
-  assert(V.get_stencil_width() >= 1);
+  assert(W.get_stencil_width() >= 1);
 
   for (Points p(grid); p; p.next()) {
     const int i = p.i(), j = p.j();