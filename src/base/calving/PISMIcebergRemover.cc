pism/pism

commit ff45421e00f0a7ea7a8048f6ca37b5a6f9d24965

Remove duplicate code in IcebergRemover.
We can use IceModelVec2S methods to transfer to/from processor 0.

src/base/calving/PISMIcebergRemover.cc
@@ -54,9 +54,8 @@ PetscErrorCode IcebergRemover::init(Vars &vars) {
  * @param[in,out] ice_thickness ice thickness
  */
 PetscErrorCode IcebergRemover::update(IceModelVec2Int &pism_mask,
-                                          IceModelVec2S &ice_thickness) {
+                                      IceModelVec2S &ice_thickness) {
   PetscErrorCode ierr;
-  double **iceberg_mask;
   const int
     mask_grounded_ice = 1,
     mask_floating_ice = 2;
@@ -65,20 +64,19 @@ PetscErrorCode IcebergRemover::update(IceModelVec2Int &pism_mask,
   // prepare the mask that will be handed to the connected component
   // labeling code:
   {
-    ierr = VecSet(m_g2, 0.0); CHKERRQ(ierr);
-
-    ierr = DMDAVecGetArray(m_da2->get(), m_g2, &iceberg_mask); CHKERRQ(ierr);
+    ierr = m_iceberg_mask.set(0.0); CHKERRQ(ierr);
 
     IceModelVec::AccessList list;
     list.add(pism_mask);
+    list.add(m_iceberg_mask);
 
     for (Points p(grid); p; p.next()) {
       const int i = p.i(), j = p.j();
 
       if (M.grounded_ice(i,j) == true)
-        iceberg_mask[i][j] = mask_grounded_ice;
+        m_iceberg_mask(i,j) = mask_grounded_ice;
       else if (M.floating_ice(i,j) == true)
-        iceberg_mask[i][j] = mask_floating_ice;
+        m_iceberg_mask(i,j) = mask_floating_ice;
     }
 
     // Mark icy SSA Dirichlet B.C. cells as "grounded" because we
@@ -89,16 +87,15 @@ PetscErrorCode IcebergRemover::update(IceModelVec2Int &pism_mask,
       for (Points p(grid); p; p.next()) {
         const int i = p.i(), j = p.j();
 
-        if (m_bcflag->as_int(i,j) == 1 && M.icy(i,j))
-          iceberg_mask[i][j] = mask_grounded_ice;
+        if ((*m_bcflag)(i,j) > 0.5 && M.icy(i,j))
+          m_iceberg_mask(i,j) = mask_grounded_ice;
       }
     }
-    ierr = DMDAVecRestoreArray(m_da2->get(), m_g2, &iceberg_mask); CHKERRQ(ierr);
   }
 
   // identify icebergs using serial code on processor 0:
   {
-    ierr = transfer_to_proc0(); CHKERRQ(ierr);
+    ierr = m_iceberg_mask.put_on_proc0(m_mask_p0, m_scatter, NULL, m_g2natural); CHKERRQ(ierr);
 
     if (grid.rank == 0) {
       double *mask;
@@ -109,17 +106,16 @@ PetscErrorCode IcebergRemover::update(IceModelVec2Int &pism_mask,
       ierr = VecRestoreArray(m_mask_p0, &mask); CHKERRQ(ierr);
     }
 
-    ierr = transfer_from_proc0(); CHKERRQ(ierr);
+    ierr = m_iceberg_mask.get_from_proc0(m_mask_p0, m_scatter, NULL, m_g2natural); CHKERRQ(ierr);
   }
 
   // correct ice thickness and the cell type mask using the resulting
   // "iceberg" mask:
   {
-    ierr = DMDAVecGetArray(m_da2->get(), m_g2, &iceberg_mask); CHKERRQ(ierr);
-
     IceModelVec::AccessList list;
     list.add(ice_thickness);
     list.add(pism_mask);
+    list.add(m_iceberg_mask);
 
     if (m_bcflag != NULL) {
       // if SSA Dirichlet B.C. are in use, do not modify mask and ice
@@ -129,7 +125,7 @@ PetscErrorCode IcebergRemover::update(IceModelVec2Int &pism_mask,
       for (Points p(grid); p; p.next()) {
         const int i = p.i(), j = p.j();
 
-        if (iceberg_mask[i][j] > 0.5 && (*m_bcflag)(i,j) < 0.5) {
+        if (m_iceberg_mask(i,j) > 0.5 && (*m_bcflag)(i,j) < 0.5) {
           ice_thickness(i,j) = 0.0;
           pism_mask(i,j)     = MASK_ICE_FREE_OCEAN;
         }
@@ -139,13 +135,12 @@ PetscErrorCode IcebergRemover::update(IceModelVec2Int &pism_mask,
       for (Points p(grid); p; p.next()) {
         const int i = p.i(), j = p.j();
 
-        if (iceberg_mask[i][j] > 0.5) {
+        if (m_iceberg_mask(i,j) > 0.5) {
           ice_thickness(i,j) = 0.0;
           pism_mask(i,j)     = MASK_ICE_FREE_OCEAN;
         }
       }
     }
-    ierr = DMDAVecRestoreArray(m_da2->get(), m_g2, &iceberg_mask); CHKERRQ(ierr);
   }
 
   // update ghosts of the mask and the ice thickness (then surface
@@ -163,7 +158,7 @@ PetscErrorCode IcebergRemover::allocate() {
                      1,         // stencil width
                      m_da2); CHKERRQ(ierr);
 
-  ierr = DMCreateGlobalVector(m_da2->get(), &m_g2); CHKERRQ(ierr);
+  ierr = m_iceberg_mask.create(grid, "iceberg_mask", WITHOUT_GHOSTS); CHKERRQ(ierr);
 
   // We want a global Vec but reordered in the natural ordering so
   // when it is scattered to proc zero it is not all messed up
@@ -178,45 +173,9 @@ PetscErrorCode IcebergRemover::allocate() {
 PetscErrorCode IcebergRemover::deallocate() {
   PetscErrorCode ierr;
 
-  ierr = VecDestroy(&m_g2); CHKERRQ(ierr);
   ierr = VecDestroy(&m_g2natural); CHKERRQ(ierr);
   ierr = VecScatterDestroy(&m_scatter); CHKERRQ(ierr);
   ierr = VecDestroy(&m_mask_p0); CHKERRQ(ierr);
-
-  return 0;
-}
-
-/**
- * Transfer the m_g2 data member to m_mask_p0 on rank 0.
- */
-PetscErrorCode IcebergRemover::transfer_to_proc0() {
-  PetscErrorCode ierr;
-
-  ierr = DMDAGlobalToNaturalBegin(m_da2->get(), m_g2, INSERT_VALUES, m_g2natural); CHKERRQ(ierr);
-  ierr =   DMDAGlobalToNaturalEnd(m_da2->get(), m_g2, INSERT_VALUES, m_g2natural); CHKERRQ(ierr);
-
-  ierr = VecScatterBegin(m_scatter, m_g2natural, m_mask_p0,
-                         INSERT_VALUES, SCATTER_FORWARD); CHKERRQ(ierr);
-  ierr =   VecScatterEnd(m_scatter, m_g2natural, m_mask_p0,
-                         INSERT_VALUES, SCATTER_FORWARD); CHKERRQ(ierr);
-
-  return 0;
-}
-
-
-/**
- * Transfer the m_mask_p0 data member from rank 0 to m_g2 (distributed).
- */
-PetscErrorCode IcebergRemover::transfer_from_proc0() {
-  PetscErrorCode ierr;
-
-  ierr = VecScatterBegin(m_scatter, m_mask_p0, m_g2natural,
-                         INSERT_VALUES, SCATTER_REVERSE); CHKERRQ(ierr);
-  ierr =   VecScatterEnd(m_scatter, m_mask_p0, m_g2natural,
-                         INSERT_VALUES, SCATTER_REVERSE); CHKERRQ(ierr);
-
-  ierr = DMDANaturalToGlobalBegin(m_da2->get(), m_g2natural, INSERT_VALUES, m_g2); CHKERRQ(ierr);
-  ierr =   DMDANaturalToGlobalEnd(m_da2->get(), m_g2natural, INSERT_VALUES, m_g2); CHKERRQ(ierr);
 
   return 0;
 }
@@ -226,7 +185,7 @@ void IcebergRemover::add_vars_to_output(const std::string &, std::set<std::strin
 }
 
 PetscErrorCode IcebergRemover::define_variables(const std::set<std::string> &, const PIO &,
-                                                    IO_Type) {
+                                                IO_Type) {
   // empty
   return 0;
 }

src/base/calving/PISMIcebergRemover.hh
@@ -54,15 +54,13 @@ protected:
   PetscErrorCode allocate();
   PetscErrorCode deallocate();
 
-  PetscErrorCode transfer_to_proc0();
-  PetscErrorCode transfer_from_proc0();
-
   virtual void add_vars_to_output(const std::string &keyword, std::set<std::string> &result);
   virtual PetscErrorCode define_variables(const std::set<std::string> &vars, const PIO &nc,
                                           IO_Type nctype);
   virtual PetscErrorCode write_variables(const std::set<std::string> &vars, const PIO& nc);
   
-  Vec m_g2, m_g2natural;  //!< global Vecs used to transfer data to/from processor 0.
+  IceModelVec2S m_iceberg_mask;
+  Vec m_g2natural;  //!< global Vec used to transfer data to/from processor 0.
   VecScatter m_scatter; //!< VecScatter used to transfer data to/from processor 0.
   Vec m_mask_p0;
   PISMDM::Ptr m_da2;

src/base/util/iceModelVec2.cc
@@ -51,56 +51,62 @@ PetscErrorCode IceModelVec2S::get_array(double** &a) {
 
 //! Puts a local IceModelVec2S on processor 0.
 /*!
- <ul>
- <li> onp0 and ctx should be created by calling VecScatterCreateToZero or be identical to one,
- <li> g2 is a preallocated temporary global vector,
- <li> g2natural is a preallocated temporary global vector with natural ordering.
- </ul>
+ - onp0 and ctx should be created by calling VecScatterCreateToZero or
+   be identical to one,
+ - g2 is a preallocated temporary global vector (not used and can be
+   NULL if this IceModelVec has no ghosts)
+ - g2natural is a preallocated temporary global vector with natural
+   ordering.
 */
-PetscErrorCode IceModelVec2S::put_on_proc0(Vec onp0, VecScatter ctx, Vec g2, Vec g2natural) const {
+PetscErrorCode IceModelVec2S::put_on_proc0(Vec onp0, VecScatter scatter, Vec global_work,
+                                           Vec natural_work) const {
   PetscErrorCode ierr;
   assert(m_v != NULL);
 
   PISMDM::Ptr da2;
   ierr = grid->get_dm(1, this->get_stencil_width(), da2); CHKERRQ(ierr);
 
-  ierr = this->copy_to_vec(g2); CHKERRQ(ierr);
+  Vec global = m_has_ghosts ? global_work : m_v;
 
-  ierr = DMDAGlobalToNaturalBegin(da2->get(), g2, INSERT_VALUES, g2natural); CHKERRQ(ierr);
-  ierr =   DMDAGlobalToNaturalEnd(da2->get(), g2, INSERT_VALUES, g2natural); CHKERRQ(ierr);
+  if (m_has_ghosts) {
+    ierr = this->copy_to_vec(global); CHKERRQ(ierr);
+  }
 
-  ierr = VecScatterBegin(ctx, g2natural, onp0, INSERT_VALUES, SCATTER_FORWARD); CHKERRQ(ierr);
-  ierr =   VecScatterEnd(ctx, g2natural, onp0, INSERT_VALUES, SCATTER_FORWARD); CHKERRQ(ierr);
+  ierr = DMDAGlobalToNaturalBegin(da2->get(), global, INSERT_VALUES, natural_work); CHKERRQ(ierr);
+  ierr =   DMDAGlobalToNaturalEnd(da2->get(), global, INSERT_VALUES, natural_work); CHKERRQ(ierr);
+
+  ierr = VecScatterBegin(scatter, natural_work, onp0, INSERT_VALUES, SCATTER_FORWARD); CHKERRQ(ierr);
+  ierr =   VecScatterEnd(scatter, natural_work, onp0, INSERT_VALUES, SCATTER_FORWARD); CHKERRQ(ierr);
 
   return 0;
 }
 
 //! Gets a local IceModelVec2 from processor 0.
 /*!
- <ul>
- <li> onp0 and ctx should be created by calling VecScatterCreateToZero or be identical to one,
- <li> g2 is a preallocated temporary global vector,
- <li> g2natural is a preallocated temporary global vector with natural ordering.
- </ul>
+ - onp0 and ctx should be created by calling VecScatterCreateToZero or
+   be identical to one,
+ - g2 is a preallocated temporary global vector (not used if this
+   IceModelVec has no ghosts)
+ - g2natural is a preallocated temporary global vector with natural
+   ordering.
 */
-PetscErrorCode IceModelVec2S::get_from_proc0(Vec onp0, VecScatter ctx, Vec g2, Vec g2natural) {
+PetscErrorCode IceModelVec2S::get_from_proc0(Vec onp0, VecScatter scatter, Vec global_work, Vec natural_work) {
   PetscErrorCode ierr;
   assert(m_v != NULL);
 
   PISMDM::Ptr da2;
   ierr = grid->get_dm(1, grid->config.get("grid_max_stencil_width"), da2); CHKERRQ(ierr);
 
-  ierr = VecScatterBegin(ctx, onp0, g2natural, INSERT_VALUES, SCATTER_REVERSE); CHKERRQ(ierr);
-  ierr =   VecScatterEnd(ctx, onp0, g2natural, INSERT_VALUES, SCATTER_REVERSE); CHKERRQ(ierr);
+  ierr = VecScatterBegin(scatter, onp0, natural_work, INSERT_VALUES, SCATTER_REVERSE); CHKERRQ(ierr);
+  ierr =   VecScatterEnd(scatter, onp0, natural_work, INSERT_VALUES, SCATTER_REVERSE); CHKERRQ(ierr);
 
-  ierr = DMDANaturalToGlobalBegin(da2->get(), g2natural, INSERT_VALUES, g2); CHKERRQ(ierr);
-  ierr =   DMDANaturalToGlobalEnd(da2->get(), g2natural, INSERT_VALUES, g2); CHKERRQ(ierr);
+  Vec global = m_has_ghosts ? global_work : m_v;
 
-  if (m_has_ghosts == true) {
-    ierr =   DMGlobalToLocalBegin(m_da->get(), g2, INSERT_VALUES, m_v); CHKERRQ(ierr);
-    ierr =     DMGlobalToLocalEnd(m_da->get(), g2, INSERT_VALUES, m_v); CHKERRQ(ierr);
-  } else {
-    ierr = this->copy_from_vec(g2); CHKERRQ(ierr);
+  ierr = DMDANaturalToGlobalBegin(da2->get(), natural_work, INSERT_VALUES, global); CHKERRQ(ierr);
+  ierr =   DMDANaturalToGlobalEnd(da2->get(), natural_work, INSERT_VALUES, global); CHKERRQ(ierr);
+
+  if (m_has_ghosts) {
+    ierr = this->copy_from_vec(global); CHKERRQ(ierr);
   }
 
   inc_state_counter();          // mark as modified

commit f00d03a3af1120bc1c03dfee80fe3617832a19d7

Commit ff45421 broke IcebergRemover. This should fix it.

src/base/calving/PISMIcebergRemover.cc
@@ -154,15 +154,13 @@ PetscErrorCode IcebergRemover::update(IceModelVec2Int &pism_mask,
 PetscErrorCode IcebergRemover::allocate() {
   PetscErrorCode ierr;
 
-  ierr = grid.get_dm(1,         // dof
-                     1,         // stencil width
-                     m_da2); CHKERRQ(ierr);
-
   ierr = m_iceberg_mask.create(grid, "iceberg_mask", WITHOUT_GHOSTS); CHKERRQ(ierr);
+
+  PISMDM::Ptr da2 = m_iceberg_mask.get_dm();
 
   // We want a global Vec but reordered in the natural ordering so
   // when it is scattered to proc zero it is not all messed up
-  ierr = DMDACreateNaturalVector(m_da2->get(), &m_g2natural); CHKERRQ(ierr);
+  ierr = DMDACreateNaturalVector(da2->get(), &m_g2natural); CHKERRQ(ierr);
 
   // Get scatter context *and* allocate mask on processor 0:
   ierr = VecScatterCreateToZero(m_g2natural, &m_scatter, &m_mask_p0); CHKERRQ(ierr);

src/base/calving/PISMIcebergRemover.hh
@@ -63,7 +63,6 @@ protected:
   Vec m_g2natural;  //!< global Vec used to transfer data to/from processor 0.
   VecScatter m_scatter; //!< VecScatter used to transfer data to/from processor 0.
   Vec m_mask_p0;
-  PISMDM::Ptr m_da2;
 
   IceModelVec2Int *m_bcflag;
 };