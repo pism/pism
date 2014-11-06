3ceac4f8163739b0dbcd12a75754b755ec5072f7
Constantine Khroulev <ckhroulev@alaska.edu>
2014-11-05 15:25:14 -0900

    One needs to turn on a model for it to work.

--- examples/FEvoRslab/runFevorSlab.sh
+++ examples/FEvoRslab/runFevorSlab.sh
@@ -9,6 +9,7 @@ flowline.py -o fevor-slab-in.nc --expand -d y fevor-slab.nc
 pismr -surface given -boot_file fevor-slab-in.nc -periodicity xy \
  -Mx 21 -My 11 -Lx 10.05 -Mz 11 -Lz 1100 -y 1 \
  -bed_smoother_range 0 \
+ -stress_balance sia_fevor \
  -sia_fevor_use_constant_slope \
  -sia_fevor_constant_slope_degrees -5.0 \
  -extra_file ex.nc -extra_times 0.1 -extra_vars distributions,recrystallizations,thk,flux_mag,velsurf_mag,h_x,h_y,taud_mag \
