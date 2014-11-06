5dab729a4d5193ec73f90b78b8f70dc7dd668bf6
Joseph H Kennedy <me@jhkennedy.org>
2014-11-05 21:23:00 -0900

    Added enhancement factor to extra vars and more naming

--- examples/FEvoRslab/runFevorSlab.sh
+++ examples/FEvoRslab/runFevorSlab.sh
@@ -4,15 +4,16 @@ set -x
 
 ./FEvoRslab.py 
 
-flowline.py -o fevor-slab.nc --expand -d y fevor-slab-in.nc
+flowline.py -o pism-in.nc --expand -d y fevor-slab-in.nc
 
-pismr -surface given -boot_file fevor-slab.nc -periodicity xy \
+pismr -surface given -boot_file pism-in.nc -periodicity xy \
  -Mx 21 -My 11 -Lx 10.05 -Mz 11 -Lz 210 -y 1 \
  -bed_smoother_range 0 \
  -stress_balance sia_fevor \
  -sia_fevor_use_constant_slope \
  -sia_fevor_constant_slope_degrees -0.1 \
- -extra_file ex.nc -extra_times 0.1 -extra_vars distributions,recrystallizations,thk,flux_mag,velsurf_mag,h_x,h_y,taud_mag \
+ -extra_file ex.nc -extra_times 0.1 \
+ -extra_vars distributions,recrystallizations,thk,flux_mag,velsurf_mag,h_x,h_y,taud_mag,enhancement_factor \
  -o pism-out.nc
 
 flowline.py -o fevor-slab-out.nc --collapse -d y pism-out.nc
