a06779a14507299dd96f50066c17c1059f53a7b5
Constantine Khroulev <ckhroulev@alaska.edu>
2014-11-18 10:03:54 -0900

    Update run script.
    - More diagnostics
    - Turn off enthalpy evolution (for now)
    - Save in z,y,x order

--- examples/FEvoRslab/runFevorSlab.sh
+++ examples/FEvoRslab/runFevorSlab.sh
@@ -23,10 +23,11 @@ ${2%/}/pismr -surface given -boot_file pism-in.nc -periodicity xy \
  -Mx ${gridMx} -My ${gridMx} -Lx 50.05 -Ly 50.5 -Mz ${gridMz} -Lz 1000 -y 150 \
  -bed_smoother_range 0 -z_spacing equal \
  -stress_balance sia_fevor \
- -sia_fevor_use_constant_slope -sia_fevor_surface_slope_degrees -3 \
- -extra_file ex.nc -extra_times 50 -extra_vars distributions,enhancement_factor,thk,velbar,velbase,velsurf \
+ -energy none \
+ -sia_fevor_use_constant_slope -sia_fevor_bed_slope_degrees -3 \
+ -extra_file ex.nc -extra_times 10 -extra_vars distributions,enhancement_factor,thk,velsurf,flux_divergence,diffusivity_staggered,taud_mag,enhancement_factor,h_x,enthalpysurf,ice_surface_temp,climatic_mass_balance,enthalpy \
  -fevor_n_particles ${nP} \
- -o pism-out.nc
+ -o pism-out.nc -o_order zyx
 
 flowline.py -o fevor-slab-out.nc --collapse -d y pism-out.nc
 
