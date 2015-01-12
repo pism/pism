pism/pism

commit 42a8151657a5a980c7069bf654766b52342337f8

More work on options.

src/base/iMtimeseries.cc
@@ -54,7 +54,7 @@ void IceModel::init_timeseries() {
     throw RuntimeError("you need to specity both -ts_file and -ts_times to save diagnostic time-series.");
   }
 
-  // If neither -ts_filen nor -ts_times is set, we're done.
+  // If neither -ts_file nor -ts_times is set, we're done.
   if (not ts_file.is_set() && not times.is_set()) {
     save_ts = false;
     return;
@@ -69,14 +69,14 @@ void IceModel::init_timeseries() {
     throw;
   }
 
-  if (times.value().size() == 0) {
+  if (times->empty()) {
     throw RuntimeError("no argument for -ts_times option.");
   }
 
   verbPrintf(2, grid.com, "saving scalar time-series to '%s'; ",
-             ts_file.c_str());
+             ts_file->c_str());
 
-  verbPrintf(2, grid.com, "times requested: %s\n", times.c_str());
+  verbPrintf(2, grid.com, "times requested: %s\n", times->c_str());
 
   current_ts = 0;
 
@@ -110,7 +110,7 @@ void IceModel::init_timeseries() {
       if (current_ts > 0) {
         verbPrintf(2, grid.com,
                    "skipping times before the last record in %s (at %s)\n",
-                   ts_file.c_str(), grid.time->date(time_max).c_str());
+                   ts_file->c_str(), grid.time->date(time_max).c_str());
       }
     }
   }
@@ -196,30 +196,25 @@ void IceModel::write_timeseries() {
 
 //! Initialize the code saving spatially-variable diagnostic quantities.
 void IceModel::init_extras() {
-  bool extra_times_set, extra_file_set, extra_vars_set;
-  std::string times, vars;
 
   last_extra = 0;               // will be set in write_extras()
   next_extra = 0;
 
+  options::String extra_file("-extra_file", "Specifies the output file");
 
-  OptionsString("-extra_file", "Specifies the output file",
-                extra_filename, extra_file_set);
+  options::String times("-extra_times", "Specifies times to save at");
 
-  OptionsString("-extra_times", "Specifies times to save at",
-                times, extra_times_set);
-
-  OptionsString("-extra_vars", "Specifies a comma-separated list of variables to save",
-                vars, extra_vars_set);
+  options::StringSet vars("-extra_vars",
+                          "Specifies a comma-separated list of variables to save", "");
 
   bool split  = options::Bool("-extra_split", "Specifies whether to save to separate files");
   bool append = options::Bool("-extra_append", "append spatial diagnostics");
 
-  if (extra_file_set ^ extra_times_set) {
+  if (extra_file.is_set() ^ times.is_set()) {
     throw RuntimeError("you need to specify both -extra_file and -extra_times to save spatial time-series.");
   }
 
-  if (!extra_file_set && !extra_times_set) {
+  if (!extra_file.is_set() && !times.is_set()) {
     save_extra = false;
     return;
   }
@@ -289,22 +284,16 @@ void IceModel::init_extras() {
                extra_filename.c_str());
   }
 
-  verbPrintf(2, grid.com, "times requested: %s\n", times.c_str());
+  verbPrintf(2, grid.com, "times requested: %s\n", times->c_str());
 
   if (extra_times.size() > 500) {
     verbPrintf(2, grid.com,
                "PISM WARNING: more than 500 times requested. This might fill your hard-drive!\n");
   }
 
-  std::string var_name;
-  if (extra_vars_set) {
-    verbPrintf(2, grid.com, "variables requested: %s\n", vars.c_str());
-    std::istringstream arg(vars);
-
-    while (getline(arg, var_name, ',')) {
-      extra_vars.insert(var_name);
-    }
-
+  if (vars.is_set()) {
+    verbPrintf(2, grid.com, "variables requested: %s\n", vars.to_string().c_str());
+    extra_vars = vars;
   } else {
     verbPrintf(2, grid.com, "PISM WARNING: -extra_vars was not set."
                " Writing model_state, mapping and climate_steady variables...\n");

src/base/basalstrength/PISMMohrCoulombYieldStress.cc
@@ -244,7 +244,7 @@ void MohrCoulombYieldStress::init()
 
   if (tauc_to_phi.is_set()) {
 
-    if (tauc_to_phi.value().empty() == false) {
+    if (not tauc_to_phi->empty()) {
       // "-tauc_to_phi filename.nc" is given
       m_tauc.regrid(tauc_to_phi, CRITICAL);
     } else {

src/base/calving/PISMOceanKill.cc
@@ -54,7 +54,7 @@ void OceanKill::init() {
   if (ocean_kill_file.is_set()) {
     verbPrintf(2, m_grid.com,
                "  setting fixed calving front location using\n"
-               "  ice thickness from '%s'.\n", ocean_kill_file.c_str());
+               "  ice thickness from '%s'.\n", ocean_kill_file->c_str());
 
     thickness.create(m_grid, "thk", WITHOUT_GHOSTS);
     thickness.set_attrs("temporary", "land ice thickness",

src/base/hydrology/PISMDistributedHydrology.cc
@@ -99,7 +99,7 @@ void DistributedHydrology::init() {
   if (hydrology_velbase_mag_file.is_set()) {
     verbPrintf(2, m_grid.com,
                "  reading velbase_mag for 'distributed' hydrology from '%s'.\n",
-               hydrology_velbase_mag_file.c_str());
+               hydrology_velbase_mag_file->c_str());
     velbase_mag.regrid(hydrology_velbase_mag_file, CRITICAL_FILL_MISSING, 0.0);
     hold_velbase_mag = true;
   }

src/base/hydrology/PISMHydrology.cc
@@ -61,29 +61,30 @@ Hydrology::~Hydrology() {
 
 
 void Hydrology::init() {
-  std::string itbfilename,  // itb = input_to_bed
-              bmeltfilename;
-  bool bmeltfile_set, itbfile_set, itbperiod_set, itbreference_set;
-  double itbperiod_years = 0.0, itbreference_year = 0.0;
 
   verbPrintf(4, m_grid.com,
              "entering Hydrology::init() ...\n");
 
-  {
-    OptionsString("-hydrology_bmelt_file",
-                  "Read time-independent values for bmelt from a file; replaces bmelt computed through conservation of energy",
-                  bmeltfilename, bmeltfile_set);
-    OptionsString("-hydrology_input_to_bed_file",
-                  "A time- and space-dependent file with amount of water (depth per time) which should be added to the amount of water at the ice sheet bed at the given location at the given time; adds to bmelt",
-                  itbfilename, itbfile_set);
-    OptionsReal("-hydrology_input_to_bed_period",
-                "The period (i.e. duration before repeat), in years, of -hydrology_input_to_bed_file data",
-                itbperiod_years, itbperiod_set);
-    OptionsReal("-hydrology_input_to_bed_reference_year",
-                "The reference year for periodizing the -hydrology_input_to_bed_file data",
-                itbreference_year, itbreference_set);
-  }
+  options::String bmelt_file("-hydrology_bmelt_file",
+                             "Read time-independent values for bmelt from a file;"
+                             " replaces bmelt computed through conservation of energy");
+  // itb = input_to_bed
+  options::String itb_file("-hydrology_input_to_bed_file",
+                           "A time- and space-dependent file with amount of water"
+                           " (depth per time) which should be added to the amount of water"
+                           " at the ice sheet bed at the given location at the given time;"
+                           " adds to bmelt");
+
+  options::Real itb_period_years("-hydrology_input_to_bed_period",
+                                 "The period (i.e. duration before repeat), in years,"
+                                 " of -hydrology_input_to_bed_file data", 0.0);
+
+  options::Real itb_reference_year("-hydrology_input_to_bed_reference_year",
+                                   "The reference year for periodizing the"
+                                   " -hydrology_input_to_bed_file data", 0.0);
+
   bool i = options::Bool("-i", "PISM input file");
+
   bool bootstrap = options::Bool("-boot_file", "PISM bootstrapping file");
 
   // the following are IceModelVec pointers into IceModel generally and are read by code in the
@@ -95,36 +96,36 @@ void Hydrology::init() {
   cellarea = m_grid.variables().get_2d_scalar("cell_area");
   mask     = m_grid.variables().get_2d_mask("mask");
 
-
-  if (bmeltfile_set) {
+  if (bmelt_file.is_set()) {
     verbPrintf(2, m_grid.com,
-               "  option -hydrology_bmelt_file seen; reading bmelt from '%s'.\n", bmeltfilename.c_str());
-    bmelt_local.regrid(bmeltfilename, CRITICAL);
+               "  option -hydrology_bmelt_file seen; reading bmelt from '%s'.\n", bmelt_file->c_str());
+    bmelt_local.regrid(bmelt_file, CRITICAL);
     hold_bmelt = true;
   }
 
 
-  if (itbfile_set) {
-    inputtobed_period = (itbperiod_set) ? itbperiod_years : 0.0;
-    inputtobed_reference_time = (itbreference_set) ? m_grid.convert(itbreference_year, "years", "seconds") : 0.0;
+  if (itb_file.is_set()) {
+    inputtobed_period = itb_period_years;
+    inputtobed_reference_time = m_grid.convert(itb_reference_year, "years", "seconds");
 
     unsigned int buffer_size = (unsigned int) m_config.get("climate_forcing_buffer_size");
 
     PIO nc(m_grid.com, "netcdf3", m_grid.config.get_unit_system());
-    nc.open(itbfilename, PISM_READONLY);
+    nc.open(itb_file, PISM_READONLY);
     unsigned int n_records = nc.inq_nrecords("inputtobed", "");
     nc.close();
 
     // if -..._period is not set, make n_records the minimum of the
     // buffer size and the number of available records. Otherwise try
     // to keep all available records in memory.
-    if (itbperiod_set == false) {
+    if (not itb_period_years.is_set()) {
       n_records = std::min(n_records, buffer_size);
     }
 
     if (n_records == 0) {
-      throw RuntimeError::formatted("can't find 'inputtobed' in -hydrology_input_to_bed file with name '%s'",
-                                    itbfilename.c_str());
+      throw RuntimeError::formatted("can't find 'inputtobed' in -hydrology_input_to_bed"
+                                    " file with name '%s'",
+                                    itb_file->c_str());
     }
 
     verbPrintf(2,m_grid.com,
@@ -135,11 +136,13 @@ void Hydrology::init() {
     inputtobed->set_n_records(n_records);
     inputtobed->create(m_grid, "inputtobed", WITHOUT_GHOSTS);
     inputtobed->set_attrs("climate_forcing",
-                          "amount of water (depth per time like bmelt) which should be put at the ice sheet bed",
+                          "amount of water (depth per time like bmelt)"
+                          " which should be put at the ice sheet bed",
                           "m s-1", "");
     verbPrintf(2,m_grid.com,
-               "    reading 'inputtobed' variable from file '%s' ...\n",itbfilename.c_str());
-    inputtobed->init(itbfilename, inputtobed_period, inputtobed_reference_time);
+               "    reading 'inputtobed' variable from file '%s' ...\n",
+               itb_file->c_str());
+    inputtobed->init(itb_file, inputtobed_period, inputtobed_reference_time);
   }
 
   // initialize till water layer thickness from the context if present,

src/base/iMIO.cc
@@ -52,15 +52,11 @@ Optionally allows saving of full velocity field.
 Calls dumpToFile() to do the actual work.
  */
 void  IceModel::writeFiles(const std::string &default_filename) {
-  std::string filename = default_filename,
-    config_out;
-  bool o_set;
+  std::string config_out;
 
   stampHistoryEnd();
 
-  {
-    OptionsString("-o", "Output file name", filename, o_set);
-  }
+  options::String filename("-o", "Output file name", default_filename);
 
   if (!ends_with(filename, ".nc")) {
     verbPrintf(2, grid.com,
@@ -69,7 +65,7 @@ void  IceModel::writeFiles(const std::string &default_filename) {
 
   if (get_output_size("-o_size") != "none") {
     verbPrintf(2, grid.com,
-               "Writing model state to file `%s'\n", filename.c_str());
+               "Writing model state to file `%s'\n", filename->c_str());
     dumpToFile(filename);
   }
 }
@@ -443,56 +439,52 @@ void IceModel::initFromFile(const std::string &filename) {
   only) and 0 (everything).
 */
 void IceModel::regrid(int dimensions) {
-  std::string filename;
-  bool regrid_vars_set, regrid_file_set;
-  std::set<std::string> regrid_vars;
 
-  if (! (dimensions == 0 ||
-         dimensions == 2 ||
-         dimensions == 3)) {
+  if (not (dimensions == 0 ||
+           dimensions == 2 ||
+           dimensions == 3)) {
     throw RuntimeError("dimensions can only be 0, 2 or 3");
   }
+  
+  options::String regrid_file("-regrid_file", "Specifies the file to regrid from");
 
-  {
-    OptionsString("-regrid_file", "Specifies the file to regrid from",
-                  filename, regrid_file_set);
+  options::StringSet regrid_vars("-regrid_vars",
+                                 "Specifies the list of variables to regrid",
+                                 "");
 
-    OptionsStringSet("-regrid_vars", "Specifies the list of variables to regrid",
-                     "", regrid_vars, regrid_vars_set);
-  }
 
   // Return if no regridding is requested:
-  if (!regrid_file_set) {
+  if (not regrid_file.is_set()) {
      return;
   }
 
   if (dimensions != 0) {
     verbPrintf(2, grid.com, "regridding %dD variables from file %s ...\n",
-               dimensions, filename.c_str());
+               dimensions, regrid_file->c_str());
   } else {
-    verbPrintf(2, grid.com, "regridding from file %s ...\n",filename.c_str());
+    verbPrintf(2, grid.com, "regridding from file %s ...\n",regrid_file->c_str());
   }
 
-  if (regrid_vars.empty()) {
+  if (regrid_vars->empty()) {
     // defaults if user gives no regrid_vars list
-    regrid_vars.insert("litho_temp");
+    regrid_vars->insert("litho_temp");
 
     if (config.get_flag("do_age")) {
-      regrid_vars.insert("age");
+      regrid_vars->insert("age");
     }
 
     if (config.get_flag("do_cold_ice_methods")) {
-      regrid_vars.insert("temp");
+      regrid_vars->insert("temp");
     } else {
-      regrid_vars.insert("enthalpy");
+      regrid_vars->insert("enthalpy");
     }
   }
 
   if (dimensions == 0) {
-    regrid_variables(filename, regrid_vars, 2);
-    regrid_variables(filename, regrid_vars, 3);
+    regrid_variables(regrid_file, regrid_vars, 2);
+    regrid_variables(regrid_file, regrid_vars, 3);
   } else {
-    regrid_variables(filename, regrid_vars, dimensions);
+    regrid_variables(regrid_file, regrid_vars, dimensions);
   }
 }
 
@@ -672,7 +664,7 @@ void IceModel::init_snapshots() {
                snapshots_filename.c_str());
   }
 
-  verbPrintf(2, grid.com, "times requested: %s\n", save_times.c_str());
+  verbPrintf(2, grid.com, "times requested: %s\n", save_times->c_str());
 }
 
   //! Writes a snapshot of the model state (if necessary)
@@ -762,24 +754,21 @@ void IceModel::write_snapshot() {
 
 //! Initialize the backup (snapshot-on-wallclock-time) mechanism.
 void IceModel::init_backups() {
-  bool o_set;
 
   backup_interval = config.get("backup_interval");
 
-  {
-    OptionsString("-o", "Output file name", backup_filename, o_set);
-    if (!o_set) {
-      backup_filename = executable_short_name + "_backup.nc";
-    } else {
-      backup_filename = pism_filename_add_suffix(backup_filename, "_backup", "");
-    }
-
-    OptionsReal("-backup_interval", "Automatic backup interval, hours",
-                backup_interval, o_set);
-
-    output_size_from_option("-backup_size", "Sets the 'size' of a backup file.",
-                            "small", backup_vars);
+  options::String backup_file("-o", "Output file name");
+  if (backup_file.is_set()) {
+    backup_filename = pism_filename_add_suffix(backup_file, "_backup", "");
+  } else {
+    backup_filename = executable_short_name + "_backup.nc";
   }
+
+  backup_interval = options::Real("-backup_interval",
+                                  "Automatic backup interval, hours", backup_interval);
+
+  output_size_from_option("-backup_size", "Sets the 'size' of a backup file.",
+                          "small", backup_vars);
 
   last_backup_time = 0.0;
 }

src/base/iMinit.cc
@@ -304,14 +304,14 @@ void IceModel::grid_setup() {
                  "PISM WARNING: file '%s' does not have the 'source' global attribute.\n"
                  "     If '%s' is a PISM output file, please run the following to get rid of this warning:\n"
                  "     ncatted -a source,global,c,c,PISM %s\n",
-                 input_file.c_str(), input_file.c_str(), input_file.c_str());
+                 input_file->c_str(), input_file->c_str(), input_file->c_str());
     } else if (source.find("PISM") == std::string::npos) {
       // If the 'source' attribute does not contain the string "PISM", then print
       // a message and stop:
       verbPrintf(1, grid.com,
                  "PISM WARNING: '%s' does not seem to be a PISM output file.\n"
                  "     If it is, please make sure that the 'source' global attribute contains the string \"PISM\".\n",
-                 input_file.c_str());
+                 input_file->c_str());
     }
 
     std::vector<std::string> names;
@@ -333,7 +333,7 @@ void IceModel::grid_setup() {
     if (var_exists == false) {
       nc.close();
       throw RuntimeError::formatted("file %s has neither enthalpy nor temperature in it",
-                                    input_file.c_str());
+                                    input_file->c_str());
     }
 
     nc.close();
@@ -378,8 +378,6 @@ void IceModel::grid_setup() {
   than once (memory allocation is one example).
  */
 void IceModel::model_state_setup() {
-  bool i_set;
-  std::string filename;
 
   reset_counters();
 
@@ -387,11 +385,10 @@ void IceModel::model_state_setup() {
   init_couplers();
 
   // Check if we are initializing from a PISM output file:
-  OptionsString("-i", "Specifies the PISM input file",
-                filename, i_set);
+  options::String input_file("-i", "Specifies the PISM input file");
 
-  if (i_set) {
-    initFromFile(filename);
+  if (input_file.is_set()) {
+    initFromFile(input_file);
 
     regrid(0);
     // Check consistency of geometry after initialization:
@@ -442,53 +439,53 @@ void IceModel::model_state_setup() {
   }
 
   if (climatic_mass_balance_cumulative.was_created()) {
-    if (i_set) {
+    if (input_file.is_set()) {
       verbPrintf(2, grid.com,
                  "* Trying to read cumulative climatic mass balance from '%s'...\n",
-                 filename.c_str());
-      climatic_mass_balance_cumulative.regrid(filename, OPTIONAL, 0.0);
+                 input_file->c_str());
+      climatic_mass_balance_cumulative.regrid(input_file, OPTIONAL, 0.0);
     } else {
       climatic_mass_balance_cumulative.set(0.0);
     }
   }
 
   if (grounded_basal_flux_2D_cumulative.was_created()) {
-    if (i_set) {
+    if (input_file.is_set()) {
       verbPrintf(2, grid.com,
                  "* Trying to read cumulative grounded basal flux from '%s'...\n",
-                 filename.c_str());
-      grounded_basal_flux_2D_cumulative.regrid(filename, OPTIONAL, 0.0);
+                 input_file->c_str());
+      grounded_basal_flux_2D_cumulative.regrid(input_file, OPTIONAL, 0.0);
     } else {
       grounded_basal_flux_2D_cumulative.set(0.0);
     }
   }
 
   if (floating_basal_flux_2D_cumulative.was_created()) {
-    if (i_set) {
+    if (input_file.is_set()) {
       verbPrintf(2, grid.com,
                  "* Trying to read cumulative floating basal flux from '%s'...\n",
-                 filename.c_str());
-      floating_basal_flux_2D_cumulative.regrid(filename, OPTIONAL, 0.0);
+                 input_file->c_str());
+      floating_basal_flux_2D_cumulative.regrid(input_file, OPTIONAL, 0.0);
     } else {
       floating_basal_flux_2D_cumulative.set(0.0);
     }
   }
 
   if (nonneg_flux_2D_cumulative.was_created()) {
-    if (i_set) {
+    if (input_file.is_set()) {
       verbPrintf(2, grid.com,
                  "* Trying to read cumulative nonneg flux from '%s'...\n",
-                 filename.c_str());
-      nonneg_flux_2D_cumulative.regrid(filename, OPTIONAL, 0.0);
+                 input_file->c_str());
+      nonneg_flux_2D_cumulative.regrid(input_file, OPTIONAL, 0.0);
     } else {
       nonneg_flux_2D_cumulative.set(0.0);
     }
   }
 
-  if (i_set) {
+  if (input_file.is_set()) {
     PIO nc(grid.com, "netcdf3", grid.config.get_unit_system());
 
-    nc.open(filename, PISM_READONLY);
+    nc.open(input_file, PISM_READONLY);
     bool run_stats_exists = nc.inq_var("run_stats");
     if (run_stats_exists) {
       nc.read_attributes(run_stats.get_name(), run_stats);
@@ -566,16 +563,13 @@ void IceModel::model_state_setup() {
   In the base class there is only one case: bootstrapping.
  */
 void IceModel::set_vars_from_options() {
-  bool boot_file_set;
-  std::string filename;
 
   verbPrintf(3, grid.com,
              "Setting initial values of model state variables...\n");
 
-  OptionsString("-boot_file", "Specifies the file to bootstrap from",
-                filename, boot_file_set);
+  options::String filename("-boot_file", "Specifies the file to bootstrap from");
 
-  if (boot_file_set) {
+  if (filename.is_set()) {
     bootstrapFromFile(filename);
   } else {
     throw RuntimeError("No input file specified.");

src/base/iMviewers.cc
@@ -26,6 +26,7 @@
 #include "PISMDiagnostic.hh"
 
 #include "error_handling.hh"
+#include "pism_options.hh"
 
 namespace pism {
 
@@ -118,31 +119,15 @@ void IceModel::update_viewers() {
 
 //! Initialize run-time diagnostic viewers.
 void IceModel::init_viewers() {
-  PetscErrorCode ierr;
-  PetscBool flag;
-  char tmp[TEMPORARY_STRING_LENGTH];
 
-  PetscInt viewer_size = (int)config.get("viewer_size");
-  ierr = PetscOptionsInt("-view_size", "specifies desired viewer size",
-                         "", viewer_size, &viewer_size, &flag);
-  PISM_PETSC_CHK(ierr, "PetscOptionsInt");
-
-  if (flag) {
-    config.set_double("viewer_size", viewer_size);
-  }
+  int viewer_size = (int)config.get("viewer_size");
+  options::Integer("-view_size", "specifies desired viewer size", viewer_size);
+  config.set_double("viewer_size", viewer_size);
 
   // map-plane (and surface) viewers:
-  ierr = PetscOptionsString("-view_map", "specifies the comma-separated list of map-plane viewers", "", "empty",
-                            tmp, TEMPORARY_STRING_LENGTH, &flag);
-  PISM_PETSC_CHK(ierr, "PetscOptionsString");
-  std::string var_name;
-  if (flag) {
-    std::istringstream arg(tmp);
-
-    while (getline(arg, var_name, ',')) {
-      map_viewers.insert(var_name);
-    }
-  }
+  map_viewers = options::StringSet("-view_map", 
+                                   "specifies the comma-separated list of map-plane viewers",
+                                   "");
 }
 
 

commit 38485520bb20924e648b0c32fac9336f33a4acad

42a81516 broke -extra_file. This should fix it.

src/base/iMtimeseries.cc
@@ -201,6 +201,7 @@ void IceModel::init_extras() {
   next_extra = 0;
 
   options::String extra_file("-extra_file", "Specifies the output file");
+  extra_filename = extra_file;
 
   options::String times("-extra_times", "Specifies times to save at");
 
@@ -220,7 +221,7 @@ void IceModel::init_extras() {
   }
 
   try {
-    grid.time->parse_times(times, extra_times);    
+    grid.time->parse_times(times, extra_times);
   } catch (RuntimeError &e) {
     e.add_context("parsing the -extra_times argument");
     throw;
@@ -268,9 +269,9 @@ void IceModel::init_extras() {
     nc.close();
   }
 
-  save_extra = true;
+  save_extra          = true;
   extra_file_is_ready = false;
-  split_extra = false;
+  split_extra         = false;
 
   if (split) {
     split_extra = true;