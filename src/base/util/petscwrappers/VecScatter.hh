6163eefbf0f2e18566f38c1479e2c969e6423d73
Constantine Khroulev <ckhroulev@alaska.edu>
2015-01-26 15:33:37 -0900

    Add a wrapper for PETSc's VecScatter.

---
+++ src/base/util/petscwrappers/VecScatter.hh
@@ -0,0 +1,38 @@
+/* Copyright (C) 2015 PISM Authors
+ *
+ * This file is part of PISM.
+ *
+ * PISM is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * PISM is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PISM; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _VECSCATTER_H_
+#define _VECSCATTER_H_
+
+#include <petscvec.h>
+
+#include "Wrapper.hh"
+
+namespace pism {
+namespace petsc {
+class VecScatter : public petsc::Wrapper< ::VecScatter > {
+public:
+  VecScatter();
+  VecScatter(::VecScatter m);
+  ~VecScatter();
+};
+} // end of namespace petsc
+} // end of namespace pism
+
+#endif /* _VECSCATTER_H_ */
