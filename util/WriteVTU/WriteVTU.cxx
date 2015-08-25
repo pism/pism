c072cc05a08cc8c47a6e4b4073b13bf4dbb614c8
Florian Ziemen <florian.ziemen@awi.de>
2015-08-25 18:06:07 +0200

    WriteVTU and trace_tracer options for coord and tracer file

--- util/WriteVTU/WriteVTU.cxx
+++ util/WriteVTU/WriteVTU.cxx
@@ -1,3 +1,22 @@
+/* Copyright (C) 2015 Florian Ziemen and the PISM Authors
+ *
+ * This file is part of PISM.
+ *
+ * PISM is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * PISM is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PISM; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <vtkVersion.h>
 #include <vtkSmartPointer.h>
 #include <vtkXMLUnstructuredGridWriter.h>
@@ -5,13 +24,14 @@
 #include <vtkPointData.h>
 #include <vtkFloatArray.h>
 
-
-
 #include <netcdf>
 #include <vector>
 #include <iostream>
 #include <cmath>
 #include <string>
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
 using namespace std;
 using namespace netCDF;
@@ -20,82 +40,80 @@ void write_xml_header();
 void write_xml_reference(const std::string filename, double step = -9e9);
 void write_xml_footer();
 vector<double> interp_field(vector<double> & tx , vector<double> & ty , const string name , NcFile & nc);
-
 vector<double> read_var(string variable, NcFile & file);
+int parse_options(int ac, char * argv[], string & tracer_file, string & coordinates_file, vector <string> & input_files );
 
-
-  
 int main(int argc, char *argv[])
 {
-  NcFile tracer_file ("tracers_created.nc", NcFile::read);
-  NcFile coords_file ("coordinates.nc", NcFile::read);
+	string tracer_file_name, coordinates_file_name;
+	vector <string> input_files;
+	int retval = parse_options (argc, argv, tracer_file_name, coordinates_file_name, input_files);
+	if (retval != 0 ) return retval;
+  NcFile tracer_file (tracer_file_name, NcFile::read);
+  NcFile coords_file (coordinates_file_name, NcFile::read);
   vector<string> variables;
-  variables.push_back("x");
-  variables.push_back("y");
-  variables.push_back("z");
+  variables.push_back("tracer_x");
+  variables.push_back("tracer_y");
+  variables.push_back("tracer_z");
   variables.push_back("time");
+	std::cerr<<"Reading tracers from " << tracer_file_name << std::endl;
   vector<vector<double> > creations;
   for (vector<string>::iterator v = variables.begin() ; v != variables.end() ; v++){
-    
+
     creations.push_back(read_var(*v, tracer_file));}
-  
- 
-  // Parse command line arguments
-  if(argc < 2)
-    {
-      std::cerr << "Required arguments: OutputFilename" << std::endl;
-      return EXIT_FAILURE;
-    }
+
   write_xml_header();
-  
-  
-  for (int file_i = 1 ; file_i < argc ; file_i++)
+
+  for (vector<string>::iterator filename = input_files.begin() ; filename != input_files.end() ; filename++)
     {
-      string filename = argv[file_i];
-      string outfile = filename;
+      string outfile = *filename;
       outfile.replace(outfile.end()-3,outfile.end(), ".vtu");
-      NcFile extra_file (filename, NcFile::read);
-      
+			std::cerr<<"Reading from " << *filename << std::endl;
+      NcFile extra_file (*filename, NcFile::read);
+
       vector<double>
-	tx = read_var("tracer_x", extra_file),
-	ty = read_var("tracer_y", extra_file),
-	tz = read_var("tracer_z", extra_file),
-	t_id = read_var("tracer_id", extra_file),
-	lon = interp_field(tx, ty, "lon", coords_file),
-	lat = interp_field(tx, ty, "lat", coords_file);
+				tx = read_var("tracer_x", extra_file),
+				ty = read_var("tracer_y", extra_file),
+				tz = read_var("tracer_z", extra_file),
+				t_id = read_var("tracer_id", extra_file);
+
+	std::cerr<<"Reading coordinates from " << "coordinates.nc" << std::endl;
+	vector<double>
+		lon = interp_field(tx, ty, "lon", coords_file),
+		lat = interp_field(tx, ty, "lat", coords_file);
 
       vector<double> topgz (tx.size(), 0.);
 
       if(not extra_file.getVar("topg").isNull())
-	topgz = interp_field(tx, ty, "topg", extra_file);
-	 
+				topgz = interp_field(tx, ty, "topg", extra_file);
+
 
 
       vtkSmartPointer<vtkPoints> points =
-	vtkSmartPointer<vtkPoints>::New();
+				vtkSmartPointer<vtkPoints>::New();
 
       vector<double>::iterator ix  = tx.begin(),
-	iy = ty.begin(),
-	iz = tz.begin(),
-	itg = topgz.begin(),
-	ilon = lon.begin(),
-	ilat = lat.begin();
-      
+				iy = ty.begin(),
+				iz = tz.begin(),
+				itg = topgz.begin(),
+				ilon = lon.begin(),
+				ilat = lat.begin();
+
       for ( ;ilon != lon.end(); )	{
-	const double r = 6.371e6 + (*iz + *itg)*100;
-	*ilon *= M_PI/180.;
-	*ilat *= M_PI/180.;
-	points->InsertNextPoint(cos(*ilat)*cos(*ilon)*r, cos(*ilat)*sin(*ilon)*r, sin(*ilat)*r);
-	ilon++; ilat++; iz++; itg++;
+				const double r = 6.371e6 + (*iz + *itg)*100;
+				*ilon *= M_PI/180.;
+				*ilat *= M_PI/180.;
+				points->InsertNextPoint(cos(*ilat)*cos(*ilon)*r, cos(*ilat)*sin(*ilon)*r, sin(*ilat)*r);
+				ilon++; ilat++; iz++; itg++;
       }
-      
-      
+
+
       vtkSmartPointer<vtkUnstructuredGrid> unstructuredGrid =
-	vtkSmartPointer<vtkUnstructuredGrid>::New();
+				vtkSmartPointer<vtkUnstructuredGrid>::New();
       unstructuredGrid->SetPoints(points);
-      
+
       size_t npoints = t_id.size();
-      
+
       vtkSmartPointer<vtkFloatArray> floatArray =
 	vtkSmartPointer<vtkFloatArray>::New();
       floatArray->SetNumberOfValues(npoints);
@@ -120,12 +138,13 @@ int main(int argc, char *argv[])
 	  unstructuredGrid->GetPointData()->AddArray(fArray);
 	  ic++;
 	}
-	
-      
-      
-  
+
+
+
+
   // Write file
-  write_xml_reference(outfile, file_i);
+			vector<double> time = read_var("time", extra_file);
+			write_xml_reference(outfile, time.back());
   vtkSmartPointer<vtkXMLUnstructuredGridWriter> writer =
     vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();
   writer->SetFileName(outfile.c_str());
@@ -138,20 +157,21 @@ int main(int argc, char *argv[])
   writer->Write();
     }
   write_xml_footer();
+	std::cerr<< "\n\nPIPE STDOUT INTO A .pvd FILE TO FEED INTO PARAVIEW. ... > test.pvd\n";
   return EXIT_SUCCESS;
 	    }
 
 void write_xml_header(){
-  std::cout<< "<?xml version=\"1.0\" ?>\n" 
+  std::cout<< "<?xml version=\"1.0\" ?>\n"
 	   <<"    <VTKFile byte_order=\"LittleEndian\" type=\"Collection\" version=\"0.1\"> \n"
 	   << " <Collection>\n" ;
 }
-	    
+
 void write_xml_reference(const std::string filename, double step){
-  static double default_step = 0 ; 
+  static double default_step = 0 ;
   if (step == -9e9) {
     step = default_step;
-    default_step += 1 ; 
+    default_step += 1 ;
   }
   std::cout<< "<DataSet file=\""<< filename << "\" group=\"\" part=\"0\" timestep=\"" << step << "\"/> \n";
 }
@@ -162,14 +182,14 @@ void write_xml_footer(){
 
 vector<double> read_var(string variable, NcFile & file){
     NcVar var = file.getVar(variable);
-    
+
     size_t npoints =  var.getDim(0).getSize();
     vector <double> data (npoints);
     var.getVar(&data[0] );
     return data;
 }
 
-  
+
   vector<double> interp_field(vector<double> & tx , vector<double> & ty , const string name , NcFile & nc){
   NcVar var = nc.getVar(name);
   vector<double> interpolated(tx.size());
@@ -199,7 +219,7 @@ vector<double> read_var(string variable, NcFile & file){
       fj = floor(j),
       cj = ceil(j);
     *d =
-      (1-ir)*(1-jr) * data[fi][fj] +		
+      (1-ir)*(1-jr) * data[fi][fj] +
       (  ir)*(1-jr) * data[ci][fj] +
       (  ir)*(  jr) * data[ci][cj] +
       (1-ir)*(  jr) * data[fi][cj];
@@ -209,32 +229,54 @@ vector<double> read_var(string variable, NcFile & file){
   return interpolated;
 }
 
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+	copy(v.begin(), v.end(), ostream_iterator<T>(os, "\n"));
+	return os;
+}
 
-  // #include <boost/program_options.hpp>
-	    
- // namespace {
- //   const size_t ERROR_IN_COMMAND_LINE = 1;
- //     const size_t SUCCESS = 0;
- //     const size_t ERROR_UNHANDLED_EXCEPTION = 2;
-     
- //     } // namespace
-	      
-	 
- // vector<string> parse_options(int argc, char* argv){
- //   vector<string> files;
- //     namespace po = boost::program_options;
- //       po::options_description desc("Options");
- // 	 desc.add_options()
- // 	   ("help,h", "Print help messages")
- // 	   ("tracer_file,t", po::value<string>(&tracer_file)->required(),  "Tracer file")
- // 	   ("coordinates,c", po::value<string>(&coord_file)->required(),
- // 	    "Coordinate file (if different from files parsed)");
- // 	   po::variables_map vm;
- // 	     po::store(po::parse_command_line(ac, av, desc), vm);
- // 	       po::notify(vm);
- // 		 if (vm.count("help")) {
- // 		   cout << desc << "\n";
- // 		   return files;
- // 		   }
-		   
- // };
+int  parse_options(int ac, char * av[], string & creation_file, string & coordinates_file, vector <string> & input_files)
+	{
+
+		try {
+			po::options_description desc("Allowed options");
+			desc.add_options()
+				("help", "produce help message")
+				("creation_file,c", po::value<string>(&creation_file),
+				 "Tracer creation log")
+				("coordinates_file,l", po::value<string>(&coordinates_file),
+				 "file with coordinates")
+				("input-file", po::value< vector<string> >(&input_files), "input file, can just come as normal argument without --input_file")
+				;
+
+			po::positional_options_description p;
+			p.add("input-file", -1);
+
+			po::variables_map vm;
+			po::store(po::command_line_parser(ac, av).
+								options(desc).positional(p).run(), vm);
+			po::notify(vm);
+
+			if (vm.count("help") || (coordinates_file == "") || (creation_file == "") || (!vm.count("input-file"))) {
+				cerr << "Usage: options_description [options] input_files\n";
+				cerr << desc;
+				return -7;
+			}
+
+			cerr << "Input files are:\n"
+					 << vm["input-file"].as< vector<string> >() << "\n";
+
+
+			cerr << "Creation file is " << creation_file << "\n";
+
+			cerr << "Coordinates file is " << coordinates_file << "\n";
+		}
+		catch(std::exception& e)
+			{
+				cout << e.what() << "\n";
+				return 1;
+			}
+		return 0;
+	}
--- util/trace_tracer.cc
+++ util/trace_tracer.cc
@@ -1,3 +1,25 @@
+/* Copyright (C) 2015 Florian Ziemen and the PISM Authors
+ *
+ * This file is part of PISM.
+ *
+ * PISM is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * PISM is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PISM; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+// g++ -lnetcdf_c++4 -lboost_program_options trace_tracer.cc -o trace_tracer
+
 #include <netcdf>
 #include <vector>
 #include <iostream>
@@ -5,7 +27,8 @@
 #include <string>
 #include <algorithm>
 
-
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
 using namespace std;
 using namespace netCDF;
@@ -31,17 +54,22 @@ bool  lt (const Dataset a , const Dataset b);
 std::vector<Dataset> read_file( string filename) ;
 void write_data(const std::vector<Dataset> & data, double time, NcFile & file);
 
-  
+int parse_options(int ac, char * argv[], string & tracer_file, string & coordinates_file, vector <string> & input_files );
+
 int main(int argc, char *argv[])
 {
+	string tracer_file_name, coordinates_file_name;
+	vector <string> input_files;
+	int retval = parse_options (argc, argv, tracer_file_name, coordinates_file_name, input_files);
+	if (retval != 0 ) return retval;
+
   std::vector<double> ids ;
   double id;
   while (cin >> id )
     ids.push_back(id);
-  
   std::sort(ids.begin(), ids.end());
 
-
+	std::cerr<< "Creating tracers_traced.nc\n";
   NcFile tracer_file ("tracers_traced.nc", NcFile::replace);
   tracer_file.addDim("time");
   tracer_file.addDim("tracer_id", ids.size());
@@ -61,56 +89,42 @@ int main(int argc, char *argv[])
     NcVar cv = tracer_file.addVar(*v, "float", dimensions);
     cv.putAtt("_FillValue", ncFloat, -9e9);
   }
-  
+
   // Parse command line arguments
   if(argc < 2)
     {
       std::cerr << "Required arguments: OutputFilename" << std::endl;
       return EXIT_FAILURE;
     }
-  
-  for (int file_i = 1 ; file_i < argc ; file_i++)
+
+
+	bool first = true;
+  for (vector<string>::iterator filename = input_files.begin() ; filename != input_files.end() ; filename++)
     {
-      string filename = argv[file_i];
-      vector<double> time ; 
+      vector<double> time ;
       {
-	NcFile extra_file (filename, NcFile::read);
-	time = read_var("time", extra_file );
-	if (file_i == 1){
-	  std::string timeunit, calendar;
-	  extra_file.getVar("time").getAtt("units").getValues(timeunit);
-	  extra_file.getVar("time").getAtt("calendar").getValues(calendar);
-	  tracer_file.getVar("time").putAtt("units", timeunit);
-	  tracer_file.getVar("time").putAtt("calendar", calendar);
-	}
+				std::cerr<< "Reading from " << *filename<< "\n";
+				NcFile extra_file (*filename, NcFile::read);
+				time = read_var("time", extra_file );
+				if (first){
+					std::string timeunit, calendar;
+					extra_file.getVar("time").getAtt("units").getValues(timeunit);
+					extra_file.getVar("time").getAtt("calendar").getValues(calendar);
+					tracer_file.getVar("time").putAtt("units", timeunit);
+					tracer_file.getVar("time").putAtt("calendar", calendar);
+				}
       }
-      
-      vector<Dataset> particles = read_file(filename);
+
+      vector<Dataset> particles = read_file(*filename);
       std::sort(particles.begin(), particles.end(), lt);
       vector<Dataset> particles_found = find_tracers(particles, ids);
-      
+
+			std::cerr<< "Writing the data from " << *filename<< "\n";
       write_data(particles_found, *time.begin(), tracer_file);
+			first = false;
     }
 }
 
-void write_xml_header(){
-  std::cout<< "<?xml version=\"1.0\" ?>\n" 
-	   <<"    <VTKFile byte_order=\"LittleEndian\" type=\"Collection\" version=\"0.1\"> \n"
-	   << " <Collection>\n" ;
-}
-	    
-void write_xml_reference(const std::string filename, double step){
-  static double default_step = 0 ; 
-  if (step == -9e9) {
-    step = default_step;
-    default_step += 1 ; 
-  }
-  std::cout<< "<DataSet file=\""<< filename << "\" group=\"\" part=\"0\" timestep=\"" << step << "\"/> \n";
-}
-
-void write_xml_footer(){
-  std::cout<<"</Collection> \n</VTKFile>\n";
-}
 
 vector<double> read_var(string variable, NcFile & file){
   if (debug)    std::cerr<< "reading " << variable << " ... ";
@@ -127,7 +141,7 @@ vector<double> read_var(string variable, NcFile & file){
     return data;
 }
 
-  
+
   vector<double> interp_field(vector<double> & tx , vector<double> & ty , const string name , NcFile & nc){
     if (debug)    std::cerr<< "interpolating " << name << "\n";
     if (tx.size() == 0 )
@@ -163,7 +177,7 @@ vector<double> read_var(string variable, NcFile & file){
       fj = floor(j),
       cj = ceil(j);
     *d =
-      (1-ir)*(1-jr) * data[fi][fj] +		
+      (1-ir)*(1-jr) * data[fi][fj] +
       (  ir)*(1-jr) * data[ci][fj] +
       (  ir)*(  jr) * data[ci][cj] +
       (1-ir)*(  jr) * data[fi][cj];
@@ -181,7 +195,7 @@ bool  lt (const Dataset a , const Dataset b){
 void print_tracer(const Dataset & p){
   std::cout <<" Dataset " << p.id << "\t x="<< p.x << "\t y="<< p.y << "\t z="<< p.z<<"\t topg" << p.topg <<  "\n";
 }
- 
+
  std::vector<Dataset> read_file( string filename) {
    NcFile extra_file (filename, NcFile::read);
    if (debug) std::cerr<< "reading tracer information from " << filename << "\n";
@@ -195,9 +209,9 @@ void print_tracer(const Dataset & p){
      topgz = interp_field(tx, ty, "topg", extra_file);
    else
      topgz.resize(tx.size(), -9e9);
-   
+
    vector<Dataset> result(tx.size());
-   
+
    vector<double>::iterator
      ix  = tx.begin(),
      iy = ty.begin(),
@@ -244,7 +258,7 @@ std::vector<Dataset> find_tracers(std::vector<Dataset> tracers, std::vector<doub
   }
   return c;
 }
- 
+
 void write_data(const std::vector<Dataset> & data, double time, NcFile & file) {
      if (debug) std::cerr<< "Writing tracer information\n";
 
@@ -254,7 +268,7 @@ void write_data(const std::vector<Dataset> & data, double time, NcFile & file) {
      tz    (data.size()),
      t_id  (data.size()),
      topgz (data.size());
-   
+
    vector<float>::iterator
      ix  = tx.begin(),
      iy  = ty.begin(),
@@ -264,11 +278,11 @@ void write_data(const std::vector<Dataset> & data, double time, NcFile & file) {
    for (   std::vector<Dataset>::const_iterator ip = data.begin(); ip != data.end(); ip++){
      *iid++ = ip->id;
      *ix++ = ip->x ;
-     *iy++ = ip->y; 
+     *iy++ = ip->y;
      *iz++ = ip->z;
      *itg++ = ip->topg;
    }
-   
+
    std::vector<double*> pointers ;
    std::vector<std::string> names ;
    vector<size_t> offsets, counts;
@@ -283,5 +297,58 @@ void write_data(const std::vector<Dataset> & data, double time, NcFile & file) {
    file.getVar("tracer_y").putVar(offsets, counts, &ty[0]);
    file.getVar("tracer_z").putVar(offsets, counts, &tz[0]);
    file.getVar("tracer_topg").putVar(offsets, counts, &topgz[0]);
-   
+
 }
+
+
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+	copy(v.begin(), v.end(), ostream_iterator<T>(os, "\n"));
+	return os;
+}
+
+int  parse_options(int ac, char * av[], string & creation_file, string & coordinates_file, vector <string> & input_files)
+	{
+
+		try {
+			po::options_description desc("Allowed options");
+			desc.add_options()
+				("help", "produce help message")
+				("creation_file,c", po::value<string>(&creation_file),
+				 "Tracer creation log")
+				("coordinates_file,l", po::value<string>(&coordinates_file),
+				 "file with coordinates")
+				("input-file", po::value< vector<string> >(&input_files), "input file, can just come as normal argument without --input_file")
+				;
+
+			po::positional_options_description p;
+			p.add("input-file", -1);
+
+			po::variables_map vm;
+			po::store(po::command_line_parser(ac, av).
+								options(desc).positional(p).run(), vm);
+			po::notify(vm);
+
+			if (vm.count("help") || (coordinates_file == "") || (creation_file == "") || (!vm.count("input-file"))) {
+				cerr << "Usage: options_description [options] input_files\n";
+				cerr << desc;
+				return -7;
+			}
+
+			cerr << "Input files are:\n"
+					 << vm["input-file"].as< vector<string> >() << "\n";
+
+
+			cerr << "Creation file is " << creation_file << "\n";
+
+			cerr << "Coordinates file is " << coordinates_file << "\n";
+		}
+		catch(std::exception& e)
+			{
+				cout << e.what() << "\n";
+				return 1;
+			}
+		return 0;
+	}
