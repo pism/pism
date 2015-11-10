e91dd56dbb0e99038312ca72c2b76563fc7b4f7f
Constantine Khroulev <ckhroulev@alaska.edu>
2015-11-09 15:54:01 -0900

    Resurrect half-bakes tests of the enthalpy solver.
    Also: add trivial exact steady state solutions and a test using Neumann
    B.C. at the top.

--- test/enthalpy.py
+++ test/enthalpy.py
@@ -12,25 +12,28 @@ class EnthalpyTest(object):
     def __init__(self, Mz=101, dt=0.1):
         ctx = PISM.Context()
         self.EC = PISM.EnthalpyConverter(ctx.config)
-        self.grid = ctx.newgrid()
+        sys = ctx.unit_system
+        config = ctx.config
 
-        grid = self.grid
+        config.set_string("grid_ice_vertical_spacing_type", "equal")
 
-        sys = grid.ctx().unit_system()
-
-        self.Lx = 1e5
-        self.Ly = 1e5
         self.Lz = 1000.0
-        self.Mx = 3
-        self.My = 3
-        self.Mz = Mz
+        self.z = np.linspace(0, self.Lz, Mz)
+
+        param = PISM.GridParameters()
+        param.Lx = 1e5
+        param.Ly = 1e5
+        param.z = PISM.DoubleVector(self.z)
+        param.Mx = 3
+        param.My = 3
+        param.Mz = Mz
+
+        param.ownership_ranges_from_options(1)
+
         self.dt = PISM.convert(sys, dt, "years", "seconds")
 
-        PISM.model.initGrid(grid, self.Lx, self.Ly, self.Lz,
-                            self.Mx, self.My, self.Mz,
-                            PISM.NOT_PERIODIC)
-
-        self.z = np.array(grid.zlevels_fine)
+        self.grid = PISM.IceGrid(ctx.ctx, param)
+        grid = self.grid
 
         self.enthalpy = PISM.model.createEnthalpyVec(grid)
 
@@ -38,9 +41,9 @@ class EnthalpyTest(object):
 
         self.u, self.v, self.w = PISM.model.create3DVelocityVecs(grid)
 
-        self.esys = PISM.enthSystemCtx(grid.Mz_fine, "enth",
-                                       grid.dx(), grid.dy(), grid.dz_fine, self.dt,
-                                       ctx.config,
+        self.esys = PISM.enthSystemCtx(grid.z(), "enth",
+                                       grid.dx(), grid.dy(), self.dt,
+                                       config,
                                        self.enthalpy,
                                        self.u, self.v, self.w,
                                        self.strain_heating,
@@ -59,7 +62,7 @@ class EnthalpyTest(object):
         self.strain_heating.set(0.0)
 
     def init_column(self):
-        self.esys.initThisColumn(1, 1, False, self.Lz)  # NOT marginal (but it does not matter)
+        self.esys.init(1, 1, False, self.Lz)  # NOT marginal (but it does not matter)
 
 
 def dirichlet_test(dt):
@@ -69,7 +72,7 @@ def dirichlet_test(dt):
     T = EnthalpyTest(dt=dt)
 
     E_surface = T.EC.enthalpy(270.0, 0.0, 0.0)
-    E_base = T.EC.enthalpy(230.0, 0.0, T.EC.getPressureFromDepth(T.Lz))
+    E_base = T.EC.enthalpy(230.0, 0.0, T.EC.pressure(T.Lz))
 
     T.enthalpy.set(E_base)
 
@@ -81,13 +84,14 @@ def dirichlet_test(dt):
     with PISM.vec.Access(nocomm=[T.enthalpy, T.u, T.v, T.w, T.strain_heating]):
         T.init_column()
 
-        T.esys.setDirichletSurface(E_surface)
-        T.esys.setDirichletBasal(E_base)
+        T.esys.set_surface_dirichlet(E_surface)
+        T.esys.set_basal_dirichlet(E_base)
 
-        x = T.esys.solveThisColumn()
+        x = T.esys.solve()
 
-        plt.plot(T.z, x, label="dt={}".format(dt))
-        plt.plot(T.z, E_exact(T.z))
+        plt.step(T.z, x, label="dt={}".format(dt))
+
+    return T.z, E_exact(T.z)
 
 
 def neumann_bc_base_test(dt):
@@ -105,31 +109,77 @@ def neumann_bc_base_test(dt):
         """In the cold case, the exact solution is a straight line with the
         slope -G/K (G is the geothermal flux) passing through the surface B.C.
         """
-        pass
+        return np.zeros_like(z) + E_surface
 
     with PISM.vec.Access(nocomm=[T.enthalpy, T.u, T.v, T.w, T.strain_heating]):
         T.init_column()
 
-        T.esys.setDirichletSurface(E_surface)
-        T.esys.setBasalHeatFlux(0.0)
+        T.esys.set_surface_dirichlet(E_surface)
+        T.esys.set_basal_heat_flux(0.0)
 
-        x = T.esys.solveThisColumn()
+        x = T.esys.solve()
 
-        plt.plot(T.z, x, label="dt={}".format(dt))
+        plt.step(T.z, x, label="dt={}".format(dt))
+
+    return T.z, E_exact(T.z)
+
+def neumann_bc_surface_test(dt):
+    """Test the enthalpy solver with Neumann B.C. at the surface and
+    Dirichlet B.C. at the base.
+    """
+
+    T = EnthalpyTest(dt=dt)
+
+    E_base = T.EC.enthalpy(250.0, 0.0, T.EC.pressure(T.Lz))
+
+    T.enthalpy.set(0.0)
+
+    def E_exact(z):
+        """In the cold case, the exact solution is a straight line with the
+        slope -G/K (G is the geothermal flux) passing through the base B.C.
+        """
+        return np.zeros_like(z) + E_base
+
+    with PISM.vec.Access(nocomm=[T.enthalpy, T.u, T.v, T.w, T.strain_heating]):
+        T.init_column()
+
+        T.esys.set_basal_dirichlet(E_base)
+        T.esys.set_surface_heat_flux(0.0)
+
+        x = T.esys.solve()
+
+        plt.step(T.z, x, label="dt={}".format(dt))
+
+    return T.z, E_exact(T.z)
+
 
 if __name__ == "__main__":
     plt.figure(1)
+    plt.title("Testing Dirichlet B.C. (base and surface)")
     plt.hold(True)
+    z, exact = None, None
     for dt in [10 ** x for x in range(1, 6)]:
-        dirichlet_test(dt)
+        z, exact = dirichlet_test(dt)
+    plt.step(z, exact, label="exact (steady state)")
     plt.grid(True)
-    plt.legend()
+    plt.legend(loc="best")
 
     plt.figure(2)
+    plt.title("Testing Neumann B.C. (base)")
     plt.hold(True)
     for dt in [10 ** x for x in range(1, 6)]:
-        neumann_bc_base_test(dt)
+        z, exact = neumann_bc_base_test(dt)
+    plt.step(z, exact, label="exact (steady state)")
     plt.grid(True)
-    plt.legend()
+    plt.legend(loc="best")
+
+    plt.figure(3)
+    plt.title("Testing Neumann B.C. (surface)")
+    plt.hold(True)
+    for dt in [10 ** x for x in range(1, 6)]:
+        z, exact = neumann_bc_surface_test(dt)
+    plt.step(z, exact, label="exact (steady state)")
+    plt.grid(True)
+    plt.legend(loc="best")
 
     plt.show()
