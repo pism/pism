pism/pism

commit 97d231d2f78d5ae5a166b5735a3012fd4d837244

Fix a sign error in the code and docs.

src/base/energy/enthSystem.cc
@@ -207,7 +207,7 @@ void enthSystemCtx::set_surface_heat_flux(double heat_flux) {
 
   const double A_l = m_w[m_ks] < 0.0 ? 1.0 - m_lambda : m_lambda - 1.0;
   const double A_d = m_w[m_ks] < 0.0 ? m_lambda - 1.0 : 1.0 - m_lambda;
-  const double A_b = m_w[m_ks] < 0.0 ? 2.0 - m_lambda : m_lambda;
+  const double A_b = m_w[m_ks] < 0.0 ? m_lambda - 2.0 : -m_lambda;
 
   // modified lower-diagonal entry:
   m_L_ks = - Rminus - Rplus + 2.0 * mu_w * A_l;
@@ -218,7 +218,7 @@ void enthSystemCtx::set_surface_heat_flux(double heat_flux) {
   // m_Enth[0] (below) is there due to the fully-implicit discretization in time, the second term is
   // the modification of the right-hand side implementing the Neumann B.C. (similar to
   // set_basal_heat_flux(); see that method for details)
-  m_B_ks = m_Enth[m_ks] + 2.0 * G * m_dz * (-Rplus + mu_w * A_b);
+  m_B_ks = m_Enth[m_ks] + 2.0 * G * m_dz * (Rplus + mu_w * A_b);
   // treat horizontal velocity using first-order upwinding:
   if (not m_ismarginal) {
     const double UpEnthu = upwind(m_u[m_ks], m_E_w[m_ks], m_Enth[m_ks], m_E_e[m_ks], m_dx);
@@ -308,14 +308,14 @@ void enthSystemCtx::set_basal_heat_flux(double heat_flux) {
 
   const double A_d = m_w[0] < 0.0 ? m_lambda - 1.0 : 1.0 - m_lambda;
   const double A_u = m_w[0] < 0.0 ? 1.0 - m_lambda : m_lambda - 1.0;
-  const double A_b = m_w[0] < 0.0 ? m_lambda : 2.0 - m_lambda;
+  const double A_b = m_w[0] < 0.0 ? -m_lambda : m_lambda - 2.0;
 
   // diagonal entry
   m_D0 = 1.0 + Rminus + Rplus + 2.0 * mu_w * A_d;
   // upper-diagonal entry
   m_U0 = - Rminus - Rplus + 2.0 * mu_w * A_u;
   // right-hand side, excluding the strain heating term and the horizontal advection
-  m_B0 = m_Enth[0] + 2.0 * G * m_dz * (Rminus + mu_w * A_b);
+  m_B0 = m_Enth[0] + 2.0 * G * m_dz * (-Rminus + mu_w * A_b);
 
   // treat horizontal velocity using first-order upwinding:
   if (not m_ismarginal) {

test/enthalpy.py
@@ -161,12 +161,12 @@ def convergence_rate_space(title, error_func):
 
     return p_max[0], p_avg[0]
 
-def exact_DN(Lz, U0, QL):
+def exact_DN(L, U0, QL):
     """Exact solution (and an initial state) for the 'Dirichlet at the base,
     Neumann at the top' setup."""
     n = 1
-    lambda_n = 1.0 / Lz * (np.pi / 2.0 + n * np.pi)
-    a = Lz * 25.0
+    lambda_n = 1.0 / L * (np.pi / 2.0 + n * np.pi)
+    a = L * 25.0
 
     def f(z, t):
         v = a * np.exp(-lambda_n**2 * alpha2 * t) * np.sin(lambda_n * z)
@@ -202,7 +202,7 @@ def errors_DN(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
         while t < T_final:
             column.init_column()
 
-            column.sys.set_surface_heat_flux(-K * Q_surface)
+            column.sys.set_surface_heat_flux(K * Q_surface)
             column.sys.set_basal_dirichlet(E_base)
 
             x = column.sys.solve()
@@ -238,17 +238,16 @@ def errors_DN(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
 
     return max_error, avg_error
 
-
-def exact_ND(Lz, Q0, UL):
+def exact_ND(L, Q0, UL):
     """Exact solution (and an initial state) for the 'Dirichlet at the base,
     Neumann at the top' setup."""
     n = 1
-    lambda_n = 1.0 / Lz * (np.pi / 2.0 + n * np.pi)
-    a = Lz * 25.0
+    lambda_n = 1.0 / L * (-np.pi / 2.0 + n * np.pi)
+    a = L * 25.0
 
     def f(z, t):
-        v = a * np.exp(-lambda_n**2 * alpha2 * t) * np.sin(lambda_n * (Lz - z))
-        return v + (UL + Q0 * (Lz - z))
+        v = a * np.exp(-lambda_n**2 * alpha2 * t) * np.sin(lambda_n * (L - z))
+        return v + (UL + Q0 * (z - L))
     return f
 
 def errors_ND(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
@@ -265,10 +264,10 @@ def errors_ND(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
 
     E_surface = EC.enthalpy(260.0, 0.0, 0.0)
     E_base = EC.enthalpy(240.0, 0.0, EC.pressure(Lz))
-    Q_base = -(E_surface  - E_base) / Lz
+    Q_base = (E_surface  - E_base) / Lz
     E_exact = exact_ND(Lz, Q_base, E_surface)
 
-    E_steady = E_surface + Q_base * (Lz - z)
+    E_steady = E_surface + Q_base * (z - Lz)
 
     with PISM.vec.Access(nocomm=[column.enthalpy,
                                  column.u, column.v, column.w,

src/base/energy/doc/discretization.tex
@@ -184,7 +184,7 @@ Alternatively,
 \begin{align}
   D_{0} &= 1 + \Rm + \Rp + 2\mu\W \wstack{\lambda - 1}{1 - \lambda}\\
   U_{0} &= -\Rp -\Rm + 2\mu\W \wstack{1 - \lambda}{\lambda - 1}\\
-  b_{0} &= \rhs{0} + 2G_{0}\Delta z \left( \Rm + \mu\W\wstack{\lambda}{2 - \lambda} \right).
+  b_{0} &= \rhs{0} + 2G_{0}\Delta z \left( -\Rm + \mu\W\wstack{-\lambda}{\lambda - 2} \right).
 \end{align}
 
 When assembling the basal equation we approximate $R_{-\frac{1}{2}}$ by $R_{0}$.
@@ -214,7 +214,7 @@ Alternatively,
 \begin{align}
   L_{\ks} &= -\Rp -\Rm + 2\mu\W \wstack{1 - \lambda}{\lambda - 1}\\
   D_{\ks} &= 1 + \Rm + \Rp + 2\mu\W \wstack{\lambda - 1}{1 - \lambda}\\
-  b_{\ks} &= \rhs{\ks} + 2G_{\ks}\Delta z\left( -\Rp + \mu\W\wstack{2-\lambda}{\lambda} \right)
+  b_{\ks} &= \rhs{\ks} + 2G_{\ks}\Delta z\left( \Rp + \mu\W\wstack{\lambda-2}{-\lambda} \right)
 \end{align}
 
 When assembling the surface equation we approximate $R_{\ks + \frac{1}{2}}$ by $R_{\ks}$.

src/base/energy/doc/neumann_bc.mac
@@ -4,7 +4,7 @@ load("generic_equation.mac")$
 eq[pm] := E[k-1]*l[pm] + E[k]*d[pm] + E[k+1]*u[pm] = b[pm];
 
 /* Additional equation implementing the Neumann B.C. */
-neumann[k] := (E[k+1] - E[k-1]) / (2 * dz) = -G[k];
+neumann[k] := (E[k+1] - E[k-1]) / (2 * dz) = G[k];
 
 /* Basal equation */
 E_base : solve(neumann[0], E[-1])[1];

commit 86c176038671190bae11e415219072973586577f

Fix a sign error bug introduced by 97d231d2.
I need to be more careful with directions of fluxes.

This commit also makes it easier to see the difference between Neumann
boundary conditions and and prescribed heat fluxes in the enthSystemCtx
API.

src/base/energy/enthSystem.cc
@@ -172,7 +172,7 @@ double enthSystemCtx::compute_lambda() {
 }
 
 
-void enthSystemCtx::set_surface_dirichlet(double E_surface) {
+void enthSystemCtx::set_surface_dirichlet_bc(double E_surface) {
 #if (PISM_DEBUG==1)
   if ((m_nu < 0.0) || (m_R_cold < 0.0) || (m_R_temp < 0.0)) {
     throw RuntimeError("setDirichletSurface() should only be called after\n"
@@ -189,6 +189,10 @@ static inline double upwind(double u, double E_m, double E, double E_p, double d
   return u * delta_inverse * (u < 0 ? (E_p -  E) : (E  - E_m));
 }
 
+
+//! Set the top surface heat flux *into* the ice.
+/** @param[in] heat_flux prescribed heat flux (positive means flux into the ice)
+ */
 void enthSystemCtx::set_surface_heat_flux(double heat_flux) {
   // extract K from R[ks], so this code works even if K=K(T)
   // recall:   R = (ice_K / ice_density) * dt / PetscSqr(dz)
@@ -196,14 +200,14 @@ void enthSystemCtx::set_surface_heat_flux(double heat_flux) {
     K = (m_ice_density * PetscSqr(m_dz) * m_R[m_ks]) / m_dt,
     G = heat_flux / K;
 
-  this->set_surface_enthalpy_flux(G);
+  this->set_surface_neumann_bc(G);
 }
 
 //! Set enthalpy flux at the surface.
 /*! This method should probably be used for debugging only. Its purpose is to allow setting the
     enthalpy flux even if K == 0, i.e. in a "pure advection" setup.
  */
-void enthSystemCtx::set_surface_enthalpy_flux(double G) {
+void enthSystemCtx::set_surface_neumann_bc(double G) {
   const double
     Rminus = 0.5 * (m_R[m_ks - 1] + m_R[m_ks]), // R_{ks-1/2}
     Rplus  = m_R[m_ks],                         // R_{ks+1/2}
@@ -247,7 +251,7 @@ void enthSystemCtx::checkReadyToSolve() {
 This method should only be called if everything but the basal boundary condition
 is already set.
  */
-void enthSystemCtx::set_basal_dirichlet(double Y) {
+void enthSystemCtx::set_basal_dirichlet_bc(double Y) {
 #if (PISM_DEBUG==1)
   checkReadyToSolve();
   if (gsl_isnan(m_D0) == 0 || gsl_isnan(m_U0) == 0 || gsl_isnan(m_B0) == 0) {
@@ -293,22 +297,24 @@ The error in the pure conductive and smooth conductivity case is @f$ O(\dz^2) @f
 This method should only be called if everything but the basal boundary condition
 is already set.
 
+@param[in] heat_flux prescribed heat flux (positive means flux into the ice)
+
  */
 void enthSystemCtx::set_basal_heat_flux(double heat_flux) {
   // extract K from R[0], so this code works even if K=K(T)
   // recall:   R = (ice_K / ice_density) * dt / PetscSqr(dz)
   const double
     K = (m_ice_density * PetscSqr(m_dz) * m_R[0]) / m_dt,
-    G = heat_flux / K;
+    G = - heat_flux / K;
 
-  this->set_basal_enthalpy_flux(G);
+  this->set_basal_neumann_bc(G);
 }
 
 //! Set enthalpy flux at the base.
 /*! This method should probably be used for debugging only. Its purpose is to allow setting the
     enthalpy flux even if K == 0, i.e. in a "pure advection" setup.
  */
-void enthSystemCtx::set_basal_enthalpy_flux(double G) {
+void enthSystemCtx::set_basal_neumann_bc(double G) {
   const double
     Rminus = m_R[0],                  // R_{-1/2}
     Rplus  = 0.5 * (m_R[0] + m_R[1]), // R_{+1/2}

src/base/energy/enthSystem.hh
@@ -56,12 +56,12 @@ public:
   double k_from_T(double T) const;
 
   void set_surface_heat_flux(double hf);
-  void set_surface_enthalpy_flux(double dE);
-  void set_surface_dirichlet(double E_surface);
+  void set_surface_neumann_bc(double dE);
+  void set_surface_dirichlet_bc(double E_surface);
 
-  void set_basal_dirichlet(double E_basal);
+  void set_basal_dirichlet_bc(double E_basal);
   void set_basal_heat_flux(double hf);
-  void set_basal_enthalpy_flux(double dE);
+  void set_basal_neumann_bc(double dE);
 
   virtual void save_system(std::ostream &output, unsigned int M) const;
 

src/base/iMenthalpy.cc
@@ -320,7 +320,7 @@ void IceModel::enthalpyAndDrainageStep(unsigned int *vertSacrCount,
 
       // set boundary conditions and update enthalpy
       {
-        system.set_surface_dirichlet(Enth_ks);
+        system.set_surface_dirichlet_bc(Enth_ks);
 
         // determine lowest-level equation at bottom of ice; see
         // decision chart in the source code browser and page
@@ -331,7 +331,7 @@ void IceModel::enthalpyAndDrainageStep(unsigned int *vertSacrCount,
           double Enth0 = EC->enthalpy_permissive(shelfbtemp(i, j), 0.0,
                                                EC->pressure(ice_thickness(i, j)));
 
-          system.set_basal_dirichlet(Enth0);
+          system.set_basal_dirichlet_bc(Enth0);
         } else {
           // grounded ice warm and wet 
           if (base_is_warm && (till_water_thickness(i, j) > 0.0)) {
@@ -341,7 +341,7 @@ void IceModel::enthalpyAndDrainageStep(unsigned int *vertSacrCount,
             } else {
               // only the base is warm: E = E_s(p) (Dirichlet)
               // ( Assumes ice has zero liquid fraction. Is this a valid assumption here?
-              system.set_basal_dirichlet(system.Enth_s(0));
+              system.set_basal_dirichlet_bc(system.Enth_s(0));
             }
           } else {
             // (Neumann) case:  q . n = q_lith . n + F_b

test/enthalpy.py
@@ -1,5 +1,5 @@
 """Simple verification/regression tests for the PISM's enthalpy
-solver. Work in progress.
+solver.
 """
 
 import PISM
@@ -109,7 +109,6 @@ def diffusion_convergence_rate_time(title, error_func):
         plt.axis('tight')
         plt.grid(True)
         plt.legend(loc="best")
-        plt.show()
 
     return p_max[0], p_avg[0]
 
@@ -141,7 +140,6 @@ def diffusion_convergence_rate_space(title, error_func):
         plt.axis('tight')
         plt.grid(True)
         plt.legend(loc="best")
-        plt.show()
 
     return p_max[0], p_avg[0]
 
@@ -187,7 +185,7 @@ def errors_DN(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
             column.init_column()
 
             column.sys.set_surface_heat_flux(K * Q_surface)
-            column.sys.set_basal_dirichlet(E_base)
+            column.sys.set_basal_dirichlet_bc(E_base)
 
             x = column.sys.solve()
 
@@ -213,7 +211,6 @@ def errors_DN(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
         plt.step(z, x, label="T={} years".format(t_years), color="red")
 
         plt.legend(loc="best")
-        plt.show()
 
     errors = E_exact(z, t) - x
 
@@ -265,7 +262,7 @@ def errors_ND(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
             column.init_column()
 
             column.sys.set_basal_heat_flux(K * Q_base)
-            column.sys.set_surface_dirichlet(E_surface)
+            column.sys.set_surface_dirichlet_bc(E_surface)
 
             x = column.sys.solve()
 
@@ -291,7 +288,6 @@ def errors_ND(plot_results=True, T_final_years=1000.0, dt_years=100, Mz=101):
         plt.step(z, x, label="T={} years".format(t_years), color="red")
 
         plt.legend(loc="best")
-        plt.show()
 
     errors = E_exact(z, t) - x
 
@@ -339,8 +335,8 @@ def errors_advection_up(plot_results=True, T_final=1000.0, dt=100, Mz=101):
         while t < T_final:
             column.init_column()
 
-            column.sys.set_basal_enthalpy_flux(dE_exact(0, t+dt))
-            column.sys.set_surface_dirichlet(E_exact(Lz, t+dt))
+            column.sys.set_basal_neumann_bc(dE_exact(0, t+dt))
+            column.sys.set_surface_dirichlet_bc(E_exact(Lz, t+dt))
 
             x = column.sys.solve()
 
@@ -360,7 +356,6 @@ def errors_advection_up(plot_results=True, T_final=1000.0, dt=100, Mz=101):
         plt.step(z, x, label="T={} seconds".format(t), color="red")
 
         plt.legend(loc="best")
-        plt.show()
 
     errors = E_exact(z, t) - x
 
@@ -399,8 +394,8 @@ def errors_advection_down(plot_results=True, T_final=1000.0, dt=100, Mz=101):
         while t < T_final:
             column.init_column()
 
-            column.sys.set_basal_dirichlet(E_exact(0, t+dt))
-            column.sys.set_surface_enthalpy_flux(dE_exact(Lz, t+dt))
+            column.sys.set_basal_dirichlet_bc(E_exact(0, t+dt))
+            column.sys.set_surface_neumann_bc(dE_exact(Lz, t+dt))
 
             x = column.sys.solve()
 
@@ -420,7 +415,6 @@ def errors_advection_down(plot_results=True, T_final=1000.0, dt=100, Mz=101):
         plt.step(z, x, label="T={} seconds".format(t), color="red")
 
         plt.legend(loc="best")
-        plt.show()
 
     errors = E_exact(z, t) - x
 
@@ -457,7 +451,6 @@ def advection_convergence_rate_time(title, error_func):
         plt.axis('tight')
         plt.grid(True)
         plt.legend(loc="best")
-        plt.show()
 
     return p_max[0], p_avg[0]
 
@@ -491,7 +484,6 @@ def advection_convergence_rate_space(title, error_func):
         plt.axis('tight')
         plt.grid(True)
         plt.legend(loc="best")
-        plt.show()
 
     return p_max[0], p_avg[0]
 
@@ -506,3 +498,5 @@ advection_convergence_rate_space("Advection: Upward flow", errors_advection_up)
 
 advection_convergence_rate_time("Advection: Downward flow", errors_advection_down)
 advection_convergence_rate_space("Advection: Downward flow", errors_advection_down)
+
+plt.show()