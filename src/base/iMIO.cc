pism/pism

commit 74c109c49afbfd7d26e01067d7c70d5b335b7966

Use shared_ptr to manage diagnostics.

src/base/iMIO.cc
@@ -155,9 +155,9 @@ void IceModel::write_variables(const PIO &nc, const std::set<std::string> &vars_
         }
       } else {
         // It might be a diagnostic quantity
-        Diagnostic *diag = m_diagnostics[*i];
+        Diagnostic::Ptr diag = m_diagnostics[*i];
 
-        if (diag != NULL) {
+        if (diag) {
           diag->define(nc);
         }
       }
@@ -285,7 +285,7 @@ void IceModel::write_variables(const PIO &nc, const std::set<std::string> &vars_
 
   // All the remaining names in vars must be of diagnostic quantities.
   for (i = vars.begin(); i != vars.end();) {
-    Diagnostic *diag = m_diagnostics[*i];
+    Diagnostic::Ptr diag = m_diagnostics[*i];
 
     if (diag == NULL) {
       ++i;

src/base/energy/bedrockThermalUnit.cc
@@ -225,9 +225,9 @@ void BedThermalUnit::write_variables_impl(const std::set<std::string> &vars, con
   }
 }
 
-void BedThermalUnit::get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                          std::map<std::string, TSDiagnostic*> &ts_dict) {
-  dict["hfgeoubed"] = new BTU_geothermal_flux_at_ground_level(this);
+void BedThermalUnit::get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                          std::map<std::string, TSDiagnostic::Ptr> &ts_dict) {
+  dict["hfgeoubed"] = Diagnostic::Ptr(new BTU_geothermal_flux_at_ground_level(this));
   (void)ts_dict;
 }
 

src/base/energy/bedrockThermalUnit.hh
@@ -111,8 +111,8 @@ protected:
   virtual void add_vars_to_output_impl(const std::string &keyword, std::set<std::string> &result);
   virtual void define_variables_impl(const std::set<std::string> &vars, const PIO &nc,
                                      IO_Type nctype);  
-  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                    std::map<std::string, TSDiagnostic*> &ts_dict);
+  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                    std::map<std::string, TSDiagnostic::Ptr> &ts_dict);
 protected:
   IceModelVec3Custom m_temp;
   IceModelVec2S m_upward_flux;

src/base/hydrology/PISMDistributedHydrology.cc
@@ -160,27 +160,27 @@ void Distributed::write_variables_impl(const std::set<std::string> &vars,
 }
 
 
-void Distributed::get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                                std::map<std::string, TSDiagnostic*> &ts_dict) {
+void Distributed::get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                                std::map<std::string, TSDiagnostic::Ptr> &ts_dict) {
   // bwat is state
   // bwp is state
-  dict["bwprel"]           = new Hydrology_bwprel(this);
-  dict["effbwp"]           = new Hydrology_effbwp(this);
-  dict["hydrobmelt"]       = new Hydrology_hydrobmelt(this);
-  dict["hydroinput"]       = new Hydrology_hydroinput(this);
-  dict["wallmelt"]         = new Hydrology_wallmelt(this);
-  dict["bwatvel"]          = new Routing_bwatvel(this);
-  dict["hydrovelbase_mag"] = new Distributed_hydrovelbase_mag(this);
+  dict["bwprel"]           = Diagnostic::Ptr(new Hydrology_bwprel(this));
+  dict["effbwp"]           = Diagnostic::Ptr(new Hydrology_effbwp(this));
+  dict["hydrobmelt"]       = Diagnostic::Ptr(new Hydrology_hydrobmelt(this));
+  dict["hydroinput"]       = Diagnostic::Ptr(new Hydrology_hydroinput(this));
+  dict["wallmelt"]         = Diagnostic::Ptr(new Hydrology_wallmelt(this));
+  dict["bwatvel"]          = Diagnostic::Ptr(new Routing_bwatvel(this));
+  dict["hydrovelbase_mag"] = Diagnostic::Ptr(new Distributed_hydrovelbase_mag(this));
 
   // add mass-conservation time-series diagnostics
-  ts_dict["hydro_ice_free_land_loss_cumulative"]      = new MCHydrology_ice_free_land_loss_cumulative(this);
-  ts_dict["hydro_ice_free_land_loss"]                 = new MCHydrology_ice_free_land_loss(this);
-  ts_dict["hydro_ocean_loss_cumulative"]              = new MCHydrology_ocean_loss_cumulative(this);
-  ts_dict["hydro_ocean_loss"]                         = new MCHydrology_ocean_loss(this);
-  ts_dict["hydro_negative_thickness_gain_cumulative"] = new MCHydrology_negative_thickness_gain_cumulative(this);
-  ts_dict["hydro_negative_thickness_gain"]            = new MCHydrology_negative_thickness_gain(this);
-  ts_dict["hydro_null_strip_loss_cumulative"]         = new MCHydrology_null_strip_loss_cumulative(this);
-  ts_dict["hydro_null_strip_loss"]                    = new MCHydrology_null_strip_loss(this);
+  ts_dict["hydro_ice_free_land_loss_cumulative"]      = TSDiagnostic::Ptr(new MCHydrology_ice_free_land_loss_cumulative(this));
+  ts_dict["hydro_ice_free_land_loss"]                 = TSDiagnostic::Ptr(new MCHydrology_ice_free_land_loss(this));
+  ts_dict["hydro_ocean_loss_cumulative"]              = TSDiagnostic::Ptr(new MCHydrology_ocean_loss_cumulative(this));
+  ts_dict["hydro_ocean_loss"]                         = TSDiagnostic::Ptr(new MCHydrology_ocean_loss(this));
+  ts_dict["hydro_negative_thickness_gain_cumulative"] = TSDiagnostic::Ptr(new MCHydrology_negative_thickness_gain_cumulative(this));
+  ts_dict["hydro_negative_thickness_gain"]            = TSDiagnostic::Ptr(new MCHydrology_negative_thickness_gain(this));
+  ts_dict["hydro_null_strip_loss_cumulative"]         = TSDiagnostic::Ptr(new MCHydrology_null_strip_loss_cumulative(this));
+  ts_dict["hydro_null_strip_loss"]                    = TSDiagnostic::Ptr(new MCHydrology_null_strip_loss(this));
 }
 
 

src/base/hydrology/PISMHydrology.cc
@@ -155,15 +155,15 @@ void Hydrology::init() {
 }
 
 
-void Hydrology::get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                     std::map<std::string, TSDiagnostic*> &/*ts_dict*/) {
-  dict["bwat"]       = new Hydrology_bwat(this);
-  dict["bwp"]        = new Hydrology_bwp(this);
-  dict["bwprel"]     = new Hydrology_bwprel(this);
-  dict["effbwp"]     = new Hydrology_effbwp(this);
-  dict["hydrobmelt"] = new Hydrology_hydrobmelt(this);
-  dict["hydroinput"] = new Hydrology_hydroinput(this);
-  dict["wallmelt"]   = new Hydrology_wallmelt(this);
+void Hydrology::get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                     std::map<std::string, TSDiagnostic::Ptr> &/*ts_dict*/) {
+  dict["bwat"]       = Diagnostic::Ptr(new Hydrology_bwat(this));
+  dict["bwp"]        = Diagnostic::Ptr(new Hydrology_bwp(this));
+  dict["bwprel"]     = Diagnostic::Ptr(new Hydrology_bwprel(this));
+  dict["effbwp"]     = Diagnostic::Ptr(new Hydrology_effbwp(this));
+  dict["hydrobmelt"] = Diagnostic::Ptr(new Hydrology_hydrobmelt(this));
+  dict["hydroinput"] = Diagnostic::Ptr(new Hydrology_hydroinput(this));
+  dict["wallmelt"]   = Diagnostic::Ptr(new Hydrology_wallmelt(this));
 }
 
 

src/base/hydrology/PISMHydrology.hh
@@ -123,8 +123,8 @@ public:
 
 protected:
   virtual void update_impl(double icet, double icedt) = 0;
-  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                    std::map<std::string, TSDiagnostic*> &ts_dict);
+  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                    std::map<std::string, TSDiagnostic::Ptr> &ts_dict);
   // in the base class these only add/define/write tillwat
   virtual void write_variables_impl(const std::set<std::string> &vars, const PIO &nc);
   virtual void add_vars_to_output_impl(const std::string &keyword, std::set<std::string> &result);
@@ -270,8 +270,8 @@ public:
 protected:
   virtual MaxTimestep max_timestep_impl(double t);
   virtual void update_impl(double icet, double icedt);
-  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                    std::map<std::string, TSDiagnostic*> &ts_dict);
+  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                    std::map<std::string, TSDiagnostic::Ptr> &ts_dict);
   virtual void write_variables_impl(const std::set<std::string> &vars, const PIO &nc);
   virtual void add_vars_to_output_impl(const std::string &keyword, std::set<std::string> &result);
   virtual void define_variables_impl(const std::set<std::string> &vars, const PIO &nc,
@@ -354,8 +354,8 @@ public:
 
 protected:
   virtual void update_impl(double icet, double icedt);
-  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                    std::map<std::string, TSDiagnostic*> &ts_dict);
+  virtual void get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                    std::map<std::string, TSDiagnostic::Ptr> &ts_dict);
   virtual void write_variables_impl(const std::set<std::string> &vars, const PIO &nc);
   virtual void add_vars_to_output_impl(const std::string &keyword, std::set<std::string> &result);
 

src/base/hydrology/PISMRoutingHydrology.cc
@@ -171,27 +171,27 @@ void Routing::write_variables_impl(const std::set<std::string> &vars, const PIO 
 }
 
 
-void Routing::get_diagnostics_impl(std::map<std::string, Diagnostic*> &dict,
-                                            std::map<std::string, TSDiagnostic*> &ts_dict) {
+void Routing::get_diagnostics_impl(std::map<std::string, Diagnostic::Ptr> &dict,
+                                            std::map<std::string, TSDiagnostic::Ptr> &ts_dict) {
   // bwat is state
-  dict["bwp"]        = new Hydrology_bwp(this);
-  dict["bwprel"]     = new Hydrology_bwprel(this);
-  dict["effbwp"]     = new Hydrology_effbwp(this);
-  dict["hydrobmelt"] = new Hydrology_hydrobmelt(this);
-  dict["hydroinput"] = new Hydrology_hydroinput(this);
-  dict["wallmelt"]   = new Hydrology_wallmelt(this);
+  dict["bwp"]        = Diagnostic::Ptr(new Hydrology_bwp(this));
+  dict["bwprel"]     = Diagnostic::Ptr(new Hydrology_bwprel(this));
+  dict["effbwp"]     = Diagnostic::Ptr(new Hydrology_effbwp(this));
+  dict["hydrobmelt"] = Diagnostic::Ptr(new Hydrology_hydrobmelt(this));
+  dict["hydroinput"] = Diagnostic::Ptr(new Hydrology_hydroinput(this));
+  dict["wallmelt"]   = Diagnostic::Ptr(new Hydrology_wallmelt(this));
   // add diagnostic that only makes sense if transport is modeled
-  dict["bwatvel"]    = new Routing_bwatvel(this);
+  dict["bwatvel"]    = Diagnostic::Ptr(new Routing_bwatvel(this));
 
   // add mass-conservation time-series diagnostics
-  ts_dict["hydro_ice_free_land_loss_cumulative"]      = new MCHydrology_ice_free_land_loss_cumulative(this);
-  ts_dict["hydro_ice_free_land_loss"]                 = new MCHydrology_ice_free_land_loss(this);
-  ts_dict["hydro_ocean_loss_cumulative"]              = new MCHydrology_ocean_loss_cumulative(this);
-  ts_dict["hydro_ocean_loss"]                         = new MCHydrology_ocean_loss(this);
-  ts_dict["hydro_negative_thickness_gain_cumulative"] = new MCHydrology_negative_thickness_gain_cumulative(this);
-  ts_dict["hydro_negative_thickness_gain"]            = new MCHydrology_negative_thickness_gain(this);
-  ts_dict["hydro_null_strip_loss_cumulative"]         = new MCHydrology_null_strip_loss_cumulative(this);
-  ts_dict["hydro_null_strip_loss"]                    = new MCHydrology_null_strip_loss(this);
+  ts_dict["hydro_ice_free_land_loss_cumulative"]      = TSDiagnostic::Ptr(new MCHydrology_ice_free_land_loss_cumulative(this));
+  ts_dict["hydro_ice_free_land_loss"]                 = TSDiagnostic::Ptr(new MCHydrology_ice_free_land_loss(this));
+  ts_dict["hydro_ocean_loss_cumulative"]              = TSDiagnostic::Ptr(new MCHydrology_ocean_loss_cumulative(this));
+  ts_dict["hydro_ocean_loss"]                         = TSDiagnostic::Ptr(new MCHydrology_ocean_loss(this));
+  ts_dict["hydro_negative_thickness_gain_cumulative"] = TSDiagnostic::Ptr(new MCHydrology_negative_thickness_gain_cumulative(this));
+  ts_dict["hydro_negative_thickness_gain"]            = TSDiagnostic::Ptr(new MCHydrology_negative_thickness_gain(this));
+  ts_dict["hydro_null_strip_loss_cumulative"]         = TSDiagnostic::Ptr(new MCHydrology_null_strip_loss_cumulative(this));
+  ts_dict["hydro_null_strip_loss"]                    = TSDiagnostic::Ptr(new MCHydrology_null_strip_loss(this));
 }
 
 

src/base/iMtimeseries.cc
@@ -91,7 +91,7 @@ void IceModel::init_timeseries() {
     m_log->message(2, "variables requested: %s\n", vars.to_string().c_str());
     m_ts_vars = vars;
   } else {
-    std::map<std::string,TSDiagnostic*>::iterator j = m_ts_diagnostics.begin();
+    std::map<std::string,TSDiagnostic::Ptr>::iterator j = m_ts_diagnostics.begin();
     while (j != m_ts_diagnostics.end()) {
       m_ts_vars.insert(j->first);
       ++j;
@@ -128,7 +128,7 @@ void IceModel::init_timeseries() {
 
 
   // set the output file:
-  std::map<std::string,TSDiagnostic*>::iterator j = m_ts_diagnostics.begin();
+  std::map<std::string,TSDiagnostic::Ptr>::iterator j = m_ts_diagnostics.begin();
   while (j != m_ts_diagnostics.end()) {
     (j->second)->init(ts_file);
     ++j;
@@ -173,9 +173,9 @@ void IceModel::write_timeseries() {
   }
 
   for (std::set<std::string>::iterator j = m_ts_vars.begin(); j != m_ts_vars.end(); ++j) {
-    TSDiagnostic *diag = m_ts_diagnostics[*j];
+    TSDiagnostic::Ptr diag = m_ts_diagnostics[*j];
 
-    if (diag != NULL) {
+    if (diag) {
       diag->update(m_time->current() - m_dt, m_time->current());
     }
   }
@@ -191,9 +191,9 @@ void IceModel::write_timeseries() {
     }
 
     for (std::set<std::string>::iterator j = m_ts_vars.begin(); j != m_ts_vars.end(); ++j) {
-      TSDiagnostic *diag = m_ts_diagnostics[*j];
+      TSDiagnostic::Ptr diag = m_ts_diagnostics[*j];
 
-      if (diag != NULL) {
+      if (diag) {
         diag->save(m_ts_times[m_current_ts - 1], m_ts_times[m_current_ts]);
       }
     }
@@ -379,9 +379,9 @@ void IceModel::write_extras() {
 
     std::set<std::string>::iterator j = m_extra_vars.begin();
     while(j != m_extra_vars.end()) {
-      Diagnostic *diag = m_diagnostics[*j];
+      Diagnostic::Ptr diag = m_diagnostics[*j];
 
-      if (diag != NULL) {
+      if (diag) {
         diag->update_cumulative();
       }
       ++j;
@@ -555,9 +555,9 @@ MaxTimestep IceModel::ts_max_timestep(double my_t) {
 void IceModel::flush_timeseries() {
   // flush all the time-series buffers:
   for (std::set<std::string>::iterator j = m_ts_vars.begin(); j != m_ts_vars.end(); ++j) {
-    TSDiagnostic *diag = m_ts_diagnostics[*j];
+    TSDiagnostic::Ptr diag = m_ts_diagnostics[*j];
 
-    if (diag != NULL) {
+    if (diag) {
       diag->flush();
     }
   }

commit 4fd674fb4f8c2436dbaa9ed511223d53153ec92e

Fix a compilation error introduced by 74c109c4. (Or I think I did.)

src/base/iMIO.cc
@@ -287,7 +287,7 @@ void IceModel::write_variables(const PIO &nc, const std::set<std::string> &vars_
   for (i = vars.begin(); i != vars.end();) {
     Diagnostic::Ptr diag = m_diagnostics[*i];
 
-    if (diag == NULL) {
+    if (not diag) {
       ++i;
     } else {
       IceModelVec::Ptr v_diagnostic = diag->compute();