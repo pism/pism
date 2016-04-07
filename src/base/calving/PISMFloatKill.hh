d4ee3fbc2cddccc4d6d789137b7823fe8a558c1a
Constantine Khroulev <ckhroulev@alaska.edu>
2016-04-07 12:40:38 -0800

    Add option -float_kill_margin_only. See #340.
    With this option "float_kill" is applied only to cells adjacent to ice
    free ocean. Needs testing, although the modification is so
    straightforward I'm pretty sure it works.

--- src/base/calving/PISMFloatKill.hh
+++ src/base/calving/PISMFloatKill.hh
@@ -44,6 +44,7 @@ protected:
   virtual void add_vars_to_output_impl(const std::string &keyword, std::set<std::string> &result);
   virtual void define_variables_impl(const std::set<std::string> &vars, const PIO &nc,
                                      IO_Type nctype);
+  bool m_margin_only;
 };
 
 
