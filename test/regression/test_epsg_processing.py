2de7303f02a29f08b547f4605078a8f3463aa74f
Constantine Khroulev <ckhroulev@alaska.edu>
2016-09-28 13:32:33 -0800

    Print commands to simplify debugging.

--- test/regression/test_epsg_processing.py
+++ test/regression/test_epsg_processing.py
@@ -7,6 +7,10 @@ PISM_PATH = sys.argv[1]
 PISMR = os.path.join(PISM_PATH, "pismr")
 PISMS = os.path.join(PISM_PATH, "pisms")
 
+def run(command):
+    print command
+    return os.system(command)
+
 files = ["pisms-output.nc",
          "pismr-output.nc",
          "both-consistent.nc",
@@ -16,7 +20,7 @@ files = ["pisms-output.nc",
          "both-double-mismatch.nc"]
 
 # create an input file
-os.system(PISMS + " -verbose 1 -Mx 3 -My 3 -Mz 5 -y 10 -o pisms-output.nc")
+run(PISMS + " -verbose 1 -Mx 3 -My 3 -Mz 5 -y 10 -o pisms-output.nc")
 
 # add the PROJ.4 string
 nc = Dataset("pisms-output.nc", "a")
@@ -24,7 +28,7 @@ nc.proj4 = "+init=epsg:3413"
 nc.close()
 
 print "Test running PISM initialized from a file w/o mapping  but with proj4..."
-assert os.system(PISMR + " -verbose 1 -i pisms-output.nc -y 10 -o both-consistent.nc") == 0
+assert run(PISMR + " -verbose 1 -i pisms-output.nc -y 10 -o both-consistent.nc") == 0
 
 print "Test that the mapping variable was initialized using the proj4 attribute..."
 nc = Dataset("both-consistent.nc", "r")
@@ -33,7 +37,7 @@ assert mapping.grid_mapping_name == "polar_stereographic"
 nc.close()
 
 print "Test re-starting PISM with consistent proj4 and mapping..."
-assert os.system(PISMR + " -verbose 1 -i both-consistent.nc -o pismr-output.nc") == 0
+assert run(PISMR + " -verbose 1 -i both-consistent.nc -o pismr-output.nc") == 0
 
 # remove a required string attribute
 shutil.copy("both-consistent.nc", "both-string-missing.nc")
@@ -42,7 +46,7 @@ del nc.variables["mapping"].grid_mapping_name
 nc.close()
 
 print "Test that PISM stops if a required string attribute is missing..."
-assert os.system(PISMR + " -verbose 1 -i both-string-missing.nc -o pismr-output.nc") != 0
+assert run(PISMR + " -verbose 1 -i both-string-missing.nc -o pismr-output.nc") != 0
 
 # alter a required string sttribute
 shutil.copy("both-consistent.nc", "both-string-mismatch.nc")
@@ -51,7 +55,7 @@ nc.variables["mapping"].grid_mapping_name = "wrong"
 nc.close()
 
 print "Test that PISM stops if a required string attribute has a wrong value..."
-assert os.system(PISMR + " -verbose 1 -i both-string-mismatch.nc -o pismr-output.nc") != 0
+assert run(PISMR + " -verbose 1 -i both-string-mismatch.nc -o pismr-output.nc") != 0
 
 # remove a required double attribute
 shutil.copy("both-consistent.nc", "both-double-missing.nc")
@@ -60,7 +64,7 @@ del nc.variables["mapping"].standard_parallel
 nc.close()
 
 print "Test that PISM stops when a required double attribute is missing..."
-assert os.system(PISMR + " -verbose 1 -i both-double-missing.nc -o pismr-output.nc") != 0
+assert run(PISMR + " -verbose 1 -i both-double-missing.nc -o pismr-output.nc") != 0
 
 # alter a required double attribute
 shutil.copy("both-consistent.nc", "both-double-mismatch.nc")
@@ -69,7 +73,7 @@ nc.variables["mapping"].standard_parallel = 45.0
 nc.close()
 
 print "Test that PISM stops if a required double attribute has a wrong value..."
-assert os.system(PISMR + " -verbose 1 -i both-double-mismatch.nc -o pismr-output.nc") != 0
+assert run(PISMR + " -verbose 1 -i both-double-mismatch.nc -o pismr-output.nc") != 0
 
 # cleanup
 for f in files:
