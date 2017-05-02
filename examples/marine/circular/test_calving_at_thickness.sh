9346d1dabc7e373b9240ec468b124703d91d0415
Constantine Khroulev <ckhroulev@alaska.edu>
2017-05-01 17:18:25 -0800

    Update a test script.

--- examples/marine/circular/test_calving_at_thickness.sh
+++ examples/marine/circular/test_calving_at_thickness.sh
@@ -14,11 +14,11 @@ output_basename="test_calving_at_thickness"
 
 grid="-Mx $xx -My $yy -Mz 3 -Mbz 1 -Lz 1500 -Lbz 0"
 
-stressbalance="-ssa_method fd -stress_balance ssa -ssa_dirichlet_bc $pc"
+stressbalance="-ssa_method fem -stress_balance ssa -ssa_dirichlet_bc $pc"
 
 calving="-calving thickness_calving -thickness_calving_threshold 300"
 
-diagnostics="thk,mask,velbar_mag,Href,velbar,discharge_flux_cumulative"
+diagnostics="thk,mask,velbar_mag,Href,velbar"
 
 viewers="-view $diagnostics"
 
@@ -28,7 +28,7 @@ misc_options="-cfbc -part_grid -o_order zyx -energy none"
 
 pismopts="-i $infile -bootstrap $grid $stressbalance $calving $viewers $extra $misc_options"
 
-doit="mpiexec -n $N pismr $pismopts"
+doit="mpiexec -n $N pismr"
 
 set -x
 
