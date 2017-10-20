7beed4c8239b3e04b8bc16d16c8c833a3be9aa50
Constantine Khrulev <ckhroulev@alaska.edu>
2017-10-19 19:24:40 -0800

    Revert "Remove -ocean runoff_SMB,..."
    This reverts commit 8f130ad19d5b0c46ab104e7503e41e3af86c16be.

--- src/coupler/ocean/Factory.cc
+++ src/coupler/ocean/Factory.cc
@@ -28,6 +28,7 @@
 #include "Delta_SMB.hh"
 #include "Frac_MBP.hh"
 #include "Frac_SMB.hh"
+#include "Runoff_SMB.hh"
 #include "Cache.hh"
 #include "GivenTH.hh"
 
@@ -48,6 +49,7 @@ Factory::Factory(IceGrid::ConstPtr g)
   add_modifier<Delta_SMB>("delta_SMB");
   add_modifier<Frac_SMB>("frac_SMB");
   add_modifier<Delta_T>("delta_T");
+  add_modifier<Runoff_SMB>("runoff_SMB");
   add_modifier<Delta_MBP>("frac_MBP");
   add_modifier<Delta_SL>("delta_SL");
 }
