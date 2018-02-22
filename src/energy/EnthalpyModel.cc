4e4f6df57bc29444a11ee1d2c65e447e3c8bc2f2
Constantine Khrulev <ckhroulev@alaska.edu>
2018-02-22 11:47:01 -0900

    Re-introduce energy.advection_ice_thickness_limit (100 meters) in the enthalpy model.
    Ignore enthalpy advection and strain heating at grid points that have a neighbor with ice
    thickness below this limit.

--- src/energy/EnthalpyModel.cc
+++ src/energy/EnthalpyModel.cc
@@ -92,6 +92,33 @@ void EnthalpyModel::initialize_impl(const IceModelVec2S &basal_melt_rate,
   }
 }
 
+static bool marginal(const IceModelVec2S &thickness, int i, int j, double threshold) {
+  int
+    n = j + 1,
+    e = i + 1,
+    s = j - 1,
+    w = i - 1;
+
+  const double
+    N  = thickness(i, n),
+    E  = thickness(e, j),
+    S  = thickness(i, s),
+    W  = thickness(w, j),
+    NW = thickness(w, n),
+    SW = thickness(w, s),
+    NE = thickness(e, n),
+    SE = thickness(e, s);
+
+  return ((E  < threshold) or
+          (NE < threshold) or
+          (N  < threshold) or
+          (NW < threshold) or
+          (W  < threshold) or
+          (SW < threshold) or
+          (S  < threshold) or
+          (SE < threshold));
+}
+
 //! Update ice enthalpy field based on conservation of energy.
 /*!
 This method is documented by the page \ref bombproofenth and by [\ref
@@ -149,6 +176,8 @@ void EnthalpyModel::update_impl(double t, double dt, const Inputs &inputs) {
       &cell_type, &u3, &v3, &w3, &strain_heating3, &m_basal_melt_rate, &m_ice_enthalpy,
       &m_work};
 
+  double advection_threshold = m_config->get_double("energy.advection_ice_thickness_limit");
+
   unsigned int liquifiedCount = 0;
 
   ParallelSection loop(m_grid->com);
@@ -158,7 +187,9 @@ void EnthalpyModel::update_impl(double t, double dt, const Inputs &inputs) {
 
       const double H = ice_thickness(i, j);
 
-      system.init(i, j, false, H);
+      system.init(i, j,
+                  marginal(ice_thickness, i, j, advection_threshold),
+                  H);
 
       // enthalpy and pressures at top of ice
       const double
