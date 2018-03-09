47f7af3846f99e1ead91f630110713589dc001c3
Constantine Khrulev <ckhroulev@alaska.edu>
2018-03-09 07:10:14 -0900

    Include <algorithm> to get std::max_element.

--- src/coupler/ocean/PicoGeometry_new.cc
+++ src/coupler/ocean/PicoGeometry_new.cc
@@ -17,6 +17,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <algorithm>            // max_element
+
 #include "PicoGeometry.hh"
 #include "pism/calving/connected_components.hh"
 #include "pism/util/IceModelVec2CellType.hh"
