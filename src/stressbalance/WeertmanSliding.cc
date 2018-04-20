fd30ae9151e3eb2707b13949b84689735542c197
Constantine Khrulev <ckhroulev@alaska.edu>
2018-04-20 12:37:48 -0800

    Actually implement Weertman-style sliding
    Use -stress_balance weertman_sliding+sia to enable.
    
    Set stress_balance.weertman_sliding.A to change the sliding parameter and
    stress_balance.weertman_sliding.k to change the ratio of the basal water pressure to the
    overburden pressure.

--- src/stressbalance/WeertmanSliding.cc
+++ src/stressbalance/WeertmanSliding.cc
@@ -41,20 +41,76 @@ void WeertmanSliding::init_impl() {
   m_log->message(2, "* Initializing Weertman-style basal sliding...\n");
 }
 
+
+/*!
+ * Compute sliding velocity using a Weertman-style parameterization from [@ref Tomkin2007],
+ * equation 5.
+ *
+ * @f[ u_s = \frac{2 A_s \beta_c (\rho g H)^{n}}{N - P} \cdot |\nabla h|^{n-1} \cdot \nabla h,
+ * @f]
+ *
+ * where
+ *
+ * - @f$ A_s @f$ is the sliding parameter,
+ * - @f$ \beta_c @f$ is a parameter capturing the effect of the presence of valley walls
+ *   (set to 1 in this implementation),
+ * - @f$ \rho @f$ is the ice density,
+ * - @f$ H @f$ is the ice thickness,
+ * - @f$ h @f$ is the ice surface elevation ,
+ * - @f$ n @f$ is the flow law exponent (usually 3),
+ * - @f$ g @f$ is the acceleration due to gravity,
+ * - @f$ N @f$ is the ice overburden pressure,
+ * - @f$ P @f$ is the basal water pressure.
+ *
+ * With these modifications and noting that @f$ N = \rho g H @f$, the formula above
+ * becomes
+ *
+ * @f[ u_s = \frac{2 A_s}{1 - k} \cdot (N |\nabla h|)^{n-1} \cdot \nabla h,
+ * @f]
+ *
+ * where
+ * - @f$ N = \rho g H @f$,
+ * - @f$ P = k N @f$ (we assume that basal water pressure is a given fraction of overburden)
+ *
+ * This parameterization is used for areas of grounded ice where the base of the ice is
+ * temperate.
+ */
 void WeertmanSliding::update(const Inputs &inputs, bool full_update) {
 
   (void) full_update;
 
-  const IceModelVec2S &bed_elevation = inputs.geometry->bed_elevation;
+  const IceModelVec2S        &H         = inputs.geometry->ice_thickness;
+  const IceModelVec2S        &h         = inputs.geometry->ice_surface_elevation;
+  const IceModelVec2CellType &cell_type = inputs.geometry->cell_type;
+  const IceModelVec3         &enthalpy  = *inputs.enthalpy;
 
-  IceModelVec::AccessList list{&m_velocity};
+  double n   = m_flow_law->exponent();
+  double A_s = m_config->get_double("stress_balance.weertman_sliding.A");
+  double k   = m_config->get_double("stress_balance.weertman_sliding.k");
+
+  IceModelVec::AccessList list{&m_velocity, &H, &h, &enthalpy, &cell_type};
 
   ParallelSection loop(m_grid->com);
   try {
     for (Points p(*m_grid); p; p.next()) {
       const int i = p.i(), j = p.j();
 
-      m_velocity(i, j) = {0.0, 0.0};
+      double
+        P_o    = m_EC->pressure(H(i, j)),
+        E_base = enthalpy(i, j, 0);
+
+      if (not m_EC->is_temperate(E_base, P_o) or cell_type.ocean(i, j)) {
+        m_velocity(i, j) = {0.0, 0.0};
+        continue;
+      }
+
+      // Note: we may need to decide if we should use one-sided FD at ice margins.
+      Vector2 grad_h = {h.diff_x_p(i, j), h.diff_y_p(i, j)};
+
+      // Note: this could be optimized by computing this instead
+      // 2 * A_s / (1 - k) * pow(P * P * (h_x * h_x + h_y * h_y), (n - 1) / 2) * grad_h;
+      // ... but I'm not sure we need to and the current code is cleaner.
+      m_velocity(i, j) = 2.0 * A_s / (1.0 - k) * pow(P_o * grad_h.magnitude(), n - 1) * grad_h;
     }
   } catch (...) {
     loop.failed();
