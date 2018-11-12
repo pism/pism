138d96ae20cb9a12f669ba7cd3b0fb6f3fe4f71b
Constantine Khrulev <ckhroulev@alaska.edu>
2018-11-12 10:28:52 -0900

    Some work on regression tests for atmosphere models

--- test/regression/atmosphere_models.py
+++ test/regression/atmosphere_models.py
@@ -14,19 +14,15 @@ config = PISM.Context().config
 
 # reduce the grid size to speed this up
 config.set_double("grid.Mx", 3)
-config.set_double("grid.My", 3)
+config.set_double("grid.My", 6) # non-square grid
 config.set_double("grid.Mz", 5)
 
 seconds_per_year = 365 * 86400
 # ensure that this is the correct year length
 config.set_string("time.calendar", "365_day")
 
-log = PISM.Context().log
 # silence models' initialization messages
-log.set_threshold(1)
-
-options = PISM.PETSc.Options()
-
+log = PISM.Context().log.set_threshold(1)
 
 def create_geometry(grid):
     geometry = PISM.Geometry(grid)
@@ -45,12 +41,6 @@ def create_geometry(grid):
 
     return geometry
 
-
-def sample(vec):
-    with PISM.vec.Access(nocomm=[vec]):
-        return vec[0, 0]
-
-
 def create_dummy_forcing_file(filename, variable_name, units, value):
     f = netCDF4.Dataset(filename, "w")
     f.createDimension("time", 1)
@@ -62,7 +52,6 @@ def create_dummy_forcing_file(filename, variable_name, units, value):
     delta_T[0] = value
     f.close()
 
-
 def dummy_grid():
     "Create a dummy grid"
     ctx = PISM.Context()
@@ -70,46 +59,27 @@ def dummy_grid():
     params.ownership_ranges_from_options(ctx.size)
     return PISM.IceGrid(ctx.ctx, params)
 
-
 def check(vec, value):
-    "Check if values of vec are almost equal to value."
-    np.testing.assert_almost_equal(sample(vec), value)
-
+    "Check if vec[0,0] is almost equal to value."
+    np.testing.assert_almost_equal(vec.numpy()[0,0], value)
 
 def check_difference(A, B, value):
     "Check if the difference between A and B is almost equal to value."
-    np.testing.assert_almost_equal(sample(A) - sample(B), value)
+    delta = A.numpy() - B.numpy()
+    np.testing.assert_almost_equal(delta[0,0], value)
 
+def check_model(model, T, SMB):
+    check(model.mean_precipitation(), SMB)
+    check(model.mean_annual_temp(), T)
 
-def check_model(model, T, omega, SMB, mass=0.0, thickness=0.0):
-    check(model.mass_flux(), SMB)
-    check(model.temperature(), T)
-    check(model.liquid_water_fraction(), omega)
-    check(model.layer_mass(), mass)
-    check(model.layer_thickness(), thickness)
+def check_modifier(model, modifier, T=0.0, P=0.0):
+    check_difference(modifier.mean_precipitation(),
+                     model.mean_precipitation(),
+                     P)
 
-
-def check_modifier(model, modifier, T=0.0, omega=0.0, SMB=0.0, mass=0.0, thickness=0.0):
-    check_difference(modifier.mass_flux(),
-                     model.mass_flux(),
-                     SMB)
-
-    check_difference(modifier.temperature(),
-                     model.temperature(),
+    check_difference(modifier.mean_annual_temp(),
+                     model.mean_annual_temp(),
                      T)
-
-    check_difference(modifier.liquid_water_fraction(),
-                     model.liquid_water_fraction(),
-                     omega)
-
-    check_difference(modifier.layer_mass(),
-                     model.layer_mass(),
-                     mass)
-
-    check_difference(modifier.layer_thickness(),
-                     model.layer_thickness(),
-                     thickness)
-
 
 def create_given_input_file(filename, grid, temperature, mass_flux):
     PISM.util.prepare_output(filename)
@@ -124,12 +94,11 @@ def create_given_input_file(filename, grid, temperature, mass_flux):
     M.set(mass_flux)
     M.write(filename)
 
-
 class DeltaT(TestCase):
     def setUp(self):
         self.filename = "delta_T_input.nc"
         self.grid = dummy_grid()
-        self.model = PISM.SurfaceEISMINTII(self.grid, ord('A'))
+        self.model = PISM.AtmosphereUniform(self.grid)
         self.dT = -5.0
         self.geometry = create_geometry(self.grid)
 
@@ -138,7 +107,8 @@ class DeltaT(TestCase):
     def runTest(self):
         "Modifier Delta_T"
 
-        modifier = PISM.SurfaceDeltaT(self.grid, self.model)
+
+        modifier = PISM.AtmosphereDeltaT(self.grid, self.model)
 
         options.setValue("-surface_delta_T_file", self.filename)
 
