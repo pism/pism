3220eb0c87f3eea5ab14eac40931ff7d449f81bb
Constantine Khrulev <ckhroulev@alaska.edu>
2019-04-24 15:19:46 -0800

    Add profiling events

--- src/hydrology/Routing.cc
+++ src/hydrology/Routing.cc
@@ -29,6 +29,7 @@
 #include "pism/util/pism_utilities.hh"
 #include "pism/util/Vars.hh"
 #include "pism/geometry/Geometry.hh"
+#include "pism/util/Profiling.hh"
 
 namespace pism {
 namespace hydrology {
@@ -882,22 +883,28 @@ void Routing::update_impl(double t, double dt, const Inputs& inputs) {
 
     double maxKW = 0.0;
     // updates ghosts of m_Kstag
+    m_grid->ctx()->profiling().begin("routing_conductivity");
     compute_conductivity(m_Wstag,
                          subglacial_water_pressure(),
                          m_bottom_surface,
                          m_Kstag, maxKW);
+    m_grid->ctx()->profiling().end("routing_conductivity");
 
     // ghosts of m_Vstag are not updated
+    m_grid->ctx()->profiling().begin("routing_velocity");
     compute_velocity(m_Wstag,
                      subglacial_water_pressure(),
                      m_bottom_surface,
                      m_Kstag,
                      inputs.no_model_mask,
                      m_Vstag);
+    m_grid->ctx()->profiling().end("routing_velocity");
 
     // to get Q, W needs valid ghosts (ghosts of m_Vstag are not used)
     // updates ghosts of m_Qstag
+    m_grid->ctx()->profiling().begin("routing_flux");
     advective_fluxes(m_Vstag, m_W, m_Qstag);
+    m_grid->ctx()->profiling().end("routing_flux");
 
     m_Qstag_average.add(hdt, m_Qstag);
 
@@ -914,42 +921,51 @@ void Routing::update_impl(double t, double dt, const Inputs& inputs) {
     m_log->message(3, "  hydrology step %05d, dt = %f s\n", step_counter, hdt);
 
     // update Wtillnew from Wtill and input_rate
-    update_Wtill(hdt,
-                 m_Wtill,
-                 m_surface_input_rate,
-                 m_basal_melt_rate,
-                 m_Wtillnew);
-    // remove water in ice-free areas and account for changes
-    enforce_bounds(inputs.geometry->cell_type,
-                   inputs.no_model_mask,
-                   0.0,        // do not limit maximum thickness
-                   m_Wtillnew,
-                   m_grounded_margin_change,
-                   m_grounding_line_change,
-                   m_conservation_error_change,
-                   m_no_model_mask_change);
+    {
+      m_grid->ctx()->profiling().begin("routing_Wtill");
+      update_Wtill(hdt,
+                   m_Wtill,
+                   m_surface_input_rate,
+                   m_basal_melt_rate,
+                   m_Wtillnew);
+      // remove water in ice-free areas and account for changes
+      enforce_bounds(inputs.geometry->cell_type,
+                     inputs.no_model_mask,
+                     0.0,        // do not limit maximum thickness
+                     m_Wtillnew,
+                     m_grounded_margin_change,
+                     m_grounding_line_change,
+                     m_conservation_error_change,
+                     m_no_model_mask_change);
+      m_grid->ctx()->profiling().end("routing_Wtill");
+    }
 
     // update Wnew from W, Wtill, Wtillnew, Wstag, Q, input_rate
     // uses ghosts of m_W, m_Wstag, m_Qstag, m_Kstag
-    update_W(hdt,
-             m_surface_input_rate,
-             m_basal_melt_rate,
-             m_W, m_Wstag,
-             m_Wtill, m_Wtillnew,
-             m_Kstag, m_Qstag,
-             m_Wnew);
-    // remove water in ice-free areas and account for changes
-    enforce_bounds(inputs.geometry->cell_type,
-                   inputs.no_model_mask,
-                   0.0,        // do not limit maximum thickness
-                   m_Wnew,
-                   m_grounded_margin_change,
-                   m_grounding_line_change,
-                   m_conservation_error_change,
-                   m_no_model_mask_change);
+    {
+      m_grid->ctx()->profiling().begin("routing_W");
+      update_W(hdt,
+               m_surface_input_rate,
+               m_basal_melt_rate,
+               m_W, m_Wstag,
+               m_Wtill, m_Wtillnew,
+               m_Kstag, m_Qstag,
+               m_Wnew);
+      // remove water in ice-free areas and account for changes
+      enforce_bounds(inputs.geometry->cell_type,
+                     inputs.no_model_mask,
+                     0.0,        // do not limit maximum thickness
+                     m_Wnew,
+                     m_grounded_margin_change,
+                     m_grounding_line_change,
+                     m_conservation_error_change,
+                     m_no_model_mask_change);
 
-    // transfer new into old (updates ghosts of m_W)
-    m_W.copy_from(m_Wnew);
+      // transfer new into old (updates ghosts of m_W)
+      m_W.copy_from(m_Wnew);
+      m_grid->ctx()->profiling().end("routing_W");
+    }
+
     // m_Wtill has no ghosts
     m_Wtill.copy_from(m_Wtillnew);
   } // end of the time-stepping loop
