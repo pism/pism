dc1141ed4fb588ab749293c896d84dcce6f8caa1
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-12-17 09:29:45 -0500

    Added missing build file
    (cherry picked from commit 67de15fd46d4dc840d965095c13eada1e39a380d)

---
+++ CMake/FindEverytrace.cmake
@@ -0,0 +1,19 @@
+# Input Variables
+#    EVERYTRACE_ROOT
+# Produces:
+#    EVERYTRACE_LIBRARY
+#    EVERYTRACE_INCLUDE_DIR
+
+
+FIND_PATH(EVERYTRACE_INCLUDE_DIR everytrace.h
+	HINTS ${EVERYTRACE_ROOT}/include)
+
+get_filename_component(EVERYTRACE_ROOT ${EVERYTRACE_INCLUDE_DIR} DIRECTORY)
+
+FIND_LIBRARY(EVERYTRACE_LIBRARY NAMES everytrace
+	HINTS ${EVERYTRACE_ROOT}/lib)
+
+foreach (VARIANT c cf c_mpi cf_mpi)
+    FIND_FILE(EVERYTRACE_${VARIANT}_REFADDR NAMES everytrace_${VARIANT}_refaddr.o
+	    HINTS ${EVERYTRACE_ROOT}/lib)
+endforeach()
