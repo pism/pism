82511942c860f7c61fdd3812d485b757628bf841
Sebastian Hinck <sebastian.hinck@awi.de>
2020-01-27 17:47:12 +0100

    Forcing fields should be used as PIECEWISE_CONSTANT, instead of linearly interpolated

--- src/coupler/atmosphere/IndexForcing.cc
+++ src/coupler/atmosphere/IndexForcing.cc
@@ -75,7 +75,7 @@ IndexForcing::IndexForcing(IceGrid::ConstPtr grid)
                                        buffer_size,
                                        evaluations_per_year,
                                        periodic,
-                                       LINEAR);
+                                       PIECEWISE_CONSTANT);
     m_T0->set_attrs("climate_forcing", "air temperature at t0",
                     "Kelvin", "Kelvin", "", 0);
 
@@ -87,7 +87,7 @@ IndexForcing::IndexForcing(IceGrid::ConstPtr grid)
                                        buffer_size,
                                        evaluations_per_year,
                                        periodic,
-                                       LINEAR);
+                                       PIECEWISE_CONSTANT);
     m_T1->set_attrs("climate_forcing", "air temperature at t1",
                     "Kelvin", "Kelvin", "", 0);
 
@@ -98,7 +98,7 @@ IndexForcing::IndexForcing(IceGrid::ConstPtr grid)
                                        buffer_size,
                                        evaluations_per_year,
                                        periodic,
-                                       LINEAR);
+                                       PIECEWISE_CONSTANT);
     m_P0->set_attrs("climate_forcing", "precipitation at t0",
                     "kg m-2 second-1", "kg m-2 second-1", "", 0);
 
@@ -109,7 +109,7 @@ IndexForcing::IndexForcing(IceGrid::ConstPtr grid)
                                        buffer_size,
                                        evaluations_per_year,
                                        periodic,
-                                       LINEAR);
+                                       PIECEWISE_CONSTANT);
     m_P1->set_attrs("climate_forcing", "precipitation at t1",
                     "kg m-2 second-1", "kg m-2 second-1", "", 0);
   }
