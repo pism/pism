a19b96580efd124193e9b9cf6bf5a0a6de89c56a
Constantine Khrulev <ckhroulev@alaska.edu>
2020-05-05 17:15:50 -0800

    Use the yield stress model with the Blatter solver

--- src/icemodel/initialization.cc
+++ src/icemodel/initialization.cc
@@ -624,7 +624,7 @@ void IceModel::allocate_basal_yield_stress() {
   std::string model = m_config->get_string("stress_balance.model");
 
   // only these two use the yield stress (so far):
-  if (model == "ssa" || model == "ssa+sia") {
+  if (member(model, {"ssa", "ssa+sia", "blatter"})) {
     std::string yield_stress_model = m_config->get_string("basal_yield_stress.model");
 
     if (yield_stress_model == "constant") {
