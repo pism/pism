88c4572aa7a31ee365dcb7141a3b9659c53eb066
Constantine Khrulev <ckhroulev@alaska.edu>
2020-06-03 19:58:19 -0800

    Support both "+init=epsg:XXXX" and "epsg:XXXX"
    PROJ 6.3 (I think) and newer no longer supports the old syntax for creating CRS using an
    EPSG code. This commit updates PISM's code processing the global attribute "proj" to
    create CF-conforming "mapping" variables for supported EPSG codes: now we support both old
    and new ways.

--- test/regression/test_epsg_processing.py
+++ test/regression/test_epsg_processing.py
@@ -28,7 +28,7 @@ run(PISMS + " -verbose 1 -Mx 3 -My 3 -Mz 5 -y 10 -o pisms-output.nc")
 
 # add the PROJ string
 nc = Dataset("pisms-output.nc", "a")
-nc.proj = "+init=epsg:3413"
+nc.proj = "epsg:3413"
 nc.close()
 
 print("Test running PISM initialized from a file w/o mapping  but with proj...")
