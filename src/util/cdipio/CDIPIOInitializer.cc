69a8e7911f2cf3c204784152fbfcb3dbb0764d87
k202136 <k202136@mlogin105.hpc.dkrz.de>
2020-12-04 14:10:58 +0100

    new cdipio namespace

---
+++ src/util/cdipio/CDIPIOInitializer.cc
@@ -0,0 +1,57 @@
+#include "CDIPIOInitializer.hh"
+
+#include "pism/util/IceGrid.hh"
+
+#include "pism/util/error_handling.hh"
+
+#include <mpi.h>
+#if (Pism_USE_CDIPIO==1)
+extern "C"{
+#include "cdipio.h"
+//#include "yaxt.h"
+#include "cdi.h"
+}
+#endif
+
+
+
+namespace pism {
+namespace cdipio {
+
+Initializer::Initializer(int n_writers, int IOmode, MPI_Comm glob) {
+#if (Pism_USE_CDIPIO==1)
+	// Initialize YAXT library
+	xt_initialize(glob);
+	// Initialize CDI-PIO library
+	float partInflate = 1.0;
+	m_local_comm = pioInit(glob, n_writers, IOmode, &m_pioNamespace, partInflate, cdiPioNoPostCommSetup);
+#endif
+}
+
+Initializer::~Initializer() {
+#if (Pism_USE_CDIPIO==1)
+	// Finalize CDI-PIO library
+	pioFinalize();
+	// Finalize YAXT library
+	xt_finalize();
+#endif
+}
+
+MPI_Comm Initializer::get_comp_comm() {
+#if (Pism_USE_CDIPIO==1)
+	return m_local_comm;
+#endif
+}
+
+int Initializer::get_pionamespace() {
+#if (Pism_USE_CDIPIO==1)
+	return m_pioNamespace;
+#endif
+}
+
+void Initializer::activate_namespace() {
+	namespaceSetActive(m_pioNamespace);
+}
+
+}
+}
