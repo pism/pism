35dc8456434dcf2ff36da3d8bfa56b0bbec117cf
Constantine Khrulev <ckhroulev@alaska.edu>
2021-05-24 21:07:39 -0800

    Installs just fine, but one regression test fails

--- doc/sphinx/installation/code/pism.sh
+++ doc/sphinx/installation/code/pism.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+set -e
 
 prefix=$HOME/local/
 build_dir=$HOME/local/build/pism
@@ -8,21 +9,27 @@ build_dir=$HOME/local/build/pism
 mkdir -p ${build_dir}
 pushd ${build_dir}
 
-git clone https://github.com/pism/pism.git .
+# FIXME: remove "-b dev"
+git clone --depth=1 -b dev https://github.com/pism/pism.git .
 
 mkdir -p build
 pushd build
 
 export PETSC_DIR=/usr/lib/petsc
 
-CC="mpicc" CXX="mpicxx" cmake \
+CC=mpicc CXX=mpicxx cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${prefix}/pism \
     -DCMAKE_C_FLAGS="-march=native -mtune=native" \
     -DCMAKE_CXX_FLAGS="-march=native -mtune=native" \
     -DPism_BUILD_EXTRA_EXECS=YES \
-    -DPython_ADDITIONAL_VERSIONS=3 \
     -DPism_BUILD_PYTHON_BINDINGS=YES \
     -DPism_USE_PNETCDF=YES \
     -DPism_USE_PROJ=YES \
     ..
+
+make -j8 all
+PYTHONPATH=$PWD/site-packages:$PYTHONPATH ctest --output-on-failure
+make install
+popd
+popd
