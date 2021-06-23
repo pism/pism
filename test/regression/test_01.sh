42effefecb2e5544a0ccfb600cc3dc462316e261
Constantine Khrulev <ckhroulev@alaska.edu>
2021-06-23 12:58:57 -0800

    The pismr restartability test should actually use pismr

--- test/regression/test_01.sh
+++ test/regression/test_01.sh
@@ -13,11 +13,11 @@ set -e -x
 $PISM_PATH/pisms -energy enthalpy -Mx 6 -My 6 -Mz 5 -y 5000 -max_dt 500.0 -o baz-01.nc
 
 # run for ten years, fixed time step
-$PISM_PATH/pisms -i baz-01.nc $OPTS -y 10 -o foo-01.nc
+$PISM_PATH/pismr -i baz-01.nc $OPTS -ys 0 -y 10 -o foo-01.nc
 
 # chain two five year runs, fixed time step
-$PISM_PATH/pisms -i baz-01.nc $OPTS -y 5 -o joe-01.nc
-$PISM_PATH/pisms -i joe-01.nc $OPTS -y 5 -o bar-01.nc
+$PISM_PATH/pismr -i baz-01.nc $OPTS -ys 0 -y 5 -o joe-01.nc
+$PISM_PATH/pismr -i joe-01.nc $OPTS -y 5 -o bar-01.nc
 
 set +e
 
