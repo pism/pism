5013d641a5615bf389709ad4d26d52f4ccadd56b
Constantine Khrulev <ckhroulev@alaska.edu>
2021-08-03 08:33:30 -0800

    Remove python3-petsc4py because it is installed with PETSc

--- doc/sphinx/installation/code/install_libraries.sh
+++ doc/sphinx/installation/code/install_libraries.sh
@@ -1,1 +1,1 @@
-apt-get install -y cmake g++ git libfftw3-dev libgsl-dev libnetcdf-dev libudunits2-dev netcdf-bin petsc-dev cdo cmake-curses-gui libpnetcdf-dev libproj-dev libx11-dev nco ncview python3-dev python3-netcdf4 python3-nose python3-numpy python3-petsc4py python3-pyproj python3-scipy swig
+apt-get install -y cmake g++ git libfftw3-dev libgsl-dev libnetcdf-dev libudunits2-dev netcdf-bin petsc-dev cdo cmake-curses-gui libpnetcdf-dev libproj-dev libx11-dev nco ncview python3-dev python3-netcdf4 python3-nose python3-numpy python3-pyproj python3-scipy swig
