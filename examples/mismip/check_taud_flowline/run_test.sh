a3ac8c2ce276e782c18f5c50b8956c65d80e7295
Constantine Khrulev <ckhroulev@alaska.edu>
2021-10-28 13:04:47 -0800

    Remove "scheme" from the test script
    ... now that the new method is the default.

--- examples/mismip/check_taud_flowline/run_test.sh
+++ examples/mismip/check_taud_flowline/run_test.sh
@@ -1,42 +1,41 @@
+#!/bin/bash
 
 H0=800
 v0=300
 ca=1750e3
 N0=601
-#N0=1801
-#N0=3001
 
 ln -s ../mismip2d/MISMIP.py
 ln -s ../../preprocessing/PISMNC.py
-#python get_conf.py -e 1a --mode=1
 
-for scheme in "onesided" "centered" 
-do
 for per in "default" "p1" "p2" "p3"
 do
-rf=result_1a_A1_${H0}_${v0}_${ca}_${N0}_${scheme}_${per}.nc
-of=ICESHELF_1a_A1_${H0}_${v0}_${ca}_${N0}_${per}.nc
-python3 prepare_iceshelf.py -c $ca -v $v0 -H $H0 -o $of -N $N0 -p $per
+  rf=result_1a_A1_${H0}_${v0}_${ca}_${N0}_${per}.nc
+  of=ICESHELF_1a_A1_${H0}_${v0}_${ca}_${N0}_${per}.nc
+  python3 prepare_iceshelf.py -c $ca -v $v0 -H $H0 -o $of -N $N0 -p $per
 
-addopt=""
-if [[ "$scheme" == "centered" ]]; then
-  addopt="-taudfix"
-fi
+  pismr \
+    -Lz 6000 \
+    -Mx $N0 \
+    -My 3 \
+    -Mz 3 \
+    -bootstrap \
+    -cfbc \
+    -energy none \
+    -i $of \
+    -no_mass \
+    -o $rf -o_order zyx -o_size big \
+    -ssa_dirichlet_bc \
+    -ssa_flow_law isothermal_glen \
+    -ssa_method fd \
+    -stress_balance ssa \
+    -stress_balance.ssa.fd.flow_line_mode \
+    -y 1s \
+    > pism-${per}.log
+    # -ssafd_ksp_rtol 1e-7 \
 
-pismr -i $of -bootstrap -Mx $N0 -My 3 -Mz 15 -Lz 6000 \
--energy none -no_mass -shelf_base_melt_rate 0.0 \
--stress_balance ssa -ssa_dirichlet_bc -ssa_flow_law isothermal_glen \
--ssa_method fd -cfbc -part_grid -ssafd_ksp_rtol 1e-7 \
--yield_stress constant -tauc 7.624000e+06 -pseudo_plastic -pseudo_plastic_q 3.333333e-01 -pseudo_plastic_uthreshold 3.155693e+07 \
--front_retreat_file $of \
--config_override MISMIP_conf_1a_A1.nc \
--ys 0 -ye 0.0001 \
--o $rf -o_order zyx -o_size big \
-$addopt > pism-${scheme}-${per}.log
+  echo $rf $of $addopt
 
-echo $rf $of $addopt
-
-done
 done
 
 python3 plot_diff.py
