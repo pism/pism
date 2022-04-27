10d0f323e5d38474fd3bd03ba4c990f74b834453
Constantine Khrulev <ckhroulev@alaska.edu>
2022-04-27 15:01:34 -0800

    Download NetCDF 4.7.4 from GitHub

--- doc/sphinx/installation/code/netcdf.sh
+++ doc/sphinx/installation/code/netcdf.sh
@@ -12,15 +12,18 @@ hdf5=~/local/hdf5
 version=4.7.4
 prefix=$HOME/local/netcdf
 build_dir=~/local/build/netcdf
-url=ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-${version}.tar.gz
+url=https://github.com/Unidata/netcdf-c/archive/refs/tags/v${version}.tar.gz
 
 mkdir -p ${build_dir}
 pushd ${build_dir}
 
 wget -nc ${url}
-tar zxf netcdf-c-${version}.tar.gz
+tar zxf v${version}.tar.gz
 
 pushd netcdf-c-${version}
+
+export CFLAGS="-g -O0"
+unset CFLAGS
 
 CC=mpicc CPPFLAGS=-I${hdf5}/include LDFLAGS=-L${hdf5}/lib ./configure \
         --enable-netcdf4 \
