c0463c660a4551b55a4059db89cbc268e4809c78
Constantine Khrulev <ckhroulev@alaska.edu>
2022-05-09 13:11:43 -0800

    Add IceModelVec.local_part() to Python bindings
    This adds a method that returns a NumPy array view of the local part of an IceModelVec.
    This is not a copy and changes to this object have immediate effect on data stored in an
    IceModelVec.
    
    Note that the array returned by local_part() includes ghost points.

--- src/pythonbindings/IceModelVec.py
+++ src/pythonbindings/IceModelVec.py
@@ -10,7 +10,7 @@ def regrid(self, filename, critical=False, default_value=0.0):
 
 
 def numpy(self):
-    "Return a NumPy array containing data from this field (on rank 0)."
+    "Return a NumPy array (a copy) containing data from this field (on rank 0)."
     tmp = self.allocate_proc0_copy()
     self.put_on_proc0(tmp)
     import numpy
@@ -19,3 +19,22 @@ def numpy(self):
         return numpy.array(tmp.get()).reshape(self.shape())
     else:
         return None
+
+def local_part(self):
+    """NumPy array containing the local (sub-domain) part, ghosts and all.
+
+    This is not a copy. Modifications have immediate effect.
+    """
+
+    padding = 2 * self.stencil_width()
+    shape = self.shape()
+
+    xm = self.grid().xm() + padding
+    ym = self.grid().ym() + padding
+
+    if len(shape) == 2:
+        shape = (ym, xm)
+    else:
+        shape = (ym, xm, shape[2])
+
+    return self.vec().get().array.reshape(shape)
