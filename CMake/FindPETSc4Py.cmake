c72773faee52ee78886288d5676702f5807f9cbb
Constantine Khrulev <ckhroulev@alaska.edu>
2022-05-11 11:48:45 -0800

    Minor build system fixes
    - Update PROJ locations on MacOS using MacPorts
    
    - Discard Python output printed when "nose" is not found
    
    - Fix FindPETSc4Py.cmake
    
      This script runs the Python interpreter to find petsc4py. Its outputs (PETSC4PY_INCLUDES
      and PETSC4PY_VERSION) depend on PYTHONPATH. We need to cache them so that re-running
      cmake does not break settings discovered the first cmake was executed.

--- CMake/FindPETSc4Py.cmake
+++ CMake/FindPETSc4Py.cmake
@@ -9,15 +9,21 @@ if(PETSC4PY_INCLUDES)
   set(PETSC4PY_FIND_QUIETLY TRUE)
 endif(PETSC4PY_INCLUDES)
 
+if (NOT PETSC4PY_INCLUDES)
 execute_process(
   COMMAND ${PYTHON_EXECUTABLE} -c "import petsc4py; from sys import stdout; stdout.write(petsc4py.get_include())"
   OUTPUT_VARIABLE PETSC4PY_INCLUDES
   RESULT_VARIABLE PETSC4PY_NOT_FOUND)
 
+set(PETSC4PY_INCLUDES ${PETSC4PY_INCLUDES} CACHE PATH "petsc4py include directory")
+
 execute_process(
   COMMAND ${PYTHON_EXECUTABLE} -c "import petsc4py; from sys import stdout; stdout.write(petsc4py.__version__)"
   OUTPUT_VARIABLE PETSC4PY_VERSION
   RESULT_VARIABLE PETSC4PY_NOT_FOUND)
+
+set(PETSC4PY_VERSION ${PETSC4PY_VERSION} CACHE PATH "petsc4py version")
+endif()
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (PETSc4Py DEFAULT_MSG PETSC4PY_INCLUDES)
