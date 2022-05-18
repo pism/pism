723f6e19be88991b9516f75e1e48731c67c1687d
Constantine Khrulev <ckhroulev@alaska.edu>
2022-05-18 12:46:07 -0800

    Fix a couple of regression tests
    - Use proper indexes in the column interpolation test
    
    - Use PYTHON_EXECUTABLE to run a script
    
      This way we can test using python2 even though the shebang at the top of the script has
      python3.

--- test/regression/test_33.sh
+++ test/regression/test_33.sh
@@ -5,6 +5,7 @@
 PISM_PATH=$1
 MPIEXEC=$2
 PISM_SOURCE_DIR=$3
+PYTHON_EXECUTABLE=$5
 
 # List of files to remove when done:
 files="in.nc out.nc"
@@ -17,7 +18,7 @@ set -x
 # prepare input files with temp and liqfrac and with temp only
 $PISM_PATH/pismr -eisII A -y 100 -o in.nc
 
-$PISM_SOURCE_DIR/examples/python/sia_forward.py -i in.nc -o out.nc
+$PYTHON_EXECUTABLE $PISM_SOURCE_DIR/examples/python/sia_forward.py -i in.nc -o out.nc
 
 if [[ -f in.nc && -f out.nc ]];
 then
