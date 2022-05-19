571c0e74bb4ceae3480b47bdea3971b38e3d65f7
Constantine Khrulev <ckhroulev@alaska.edu>
2022-05-18 16:37:01 -0800

    Revert "Use PetscArraymove instead of interpolation when grids match"
    This reverts commit 9b8142c74c90e5ffdf8c3de577080743cd7b2627.
    
    This optimization did not work as expected.

--- src/util/ColumnInterpolation.cc
+++ src/util/ColumnInterpolation.cc
@@ -19,17 +19,13 @@
 
 #include "ColumnInterpolation.hh"
 
-#include <petscsys.h>
 #include <cmath>
 
 namespace pism {
 
 ColumnInterpolation::ColumnInterpolation(const std::vector<double> &new_z_coarse,
                                          const std::vector<double> &new_z_fine)
-  : m_z_fine(new_z_fine),
-    m_z_coarse(new_z_coarse),
-    m_use_linear_interpolation(false),
-    m_identical_grids(false) {
+  : m_z_fine(new_z_fine), m_z_coarse(new_z_coarse) {
   init_interpolation();
 }
 
@@ -40,18 +36,14 @@ std::vector<double> ColumnInterpolation::coarse_to_fine(const std::vector<double
   return result;
 }
 
-void ColumnInterpolation::coarse_to_fine(const double *input, unsigned int ks, double *result) const {
-  if (m_identical_grids) {
-    PetscArraymove(result, input, Mz_fine());
-    return;
-  }
-
+void ColumnInterpolation::coarse_to_fine(const double *input,
+                                         unsigned int ks,
+                                         double *result) const {
   if (m_use_linear_interpolation) {
     coarse_to_fine_linear(input, ks, result);
-    return;
+  } else {
+    coarse_to_fine_quadratic(input, ks, result);
   }
-
-  coarse_to_fine_quadratic(input, ks, result);
 }
 
 void ColumnInterpolation::coarse_to_fine_linear(const double *input, unsigned int ks,
@@ -137,11 +129,6 @@ std::vector<double> ColumnInterpolation::fine_to_coarse(const std::vector<double
 }
 
 void ColumnInterpolation::fine_to_coarse(const double *input, double *result) const {
-  if (m_identical_grids) {
-    PetscArraymove(result, input, Mz_fine());
-    return;
-  }
-
   const unsigned int N = Mz_coarse();
 
   for (unsigned int k = 0; k < N - 1; ++k) {
@@ -217,17 +204,6 @@ static std::vector<unsigned int> init_interpolation_indexes(const std::vector<do
 }
 
 void ColumnInterpolation::init_interpolation() {
-
-  if (m_z_coarse.size() == m_z_fine.size()) {
-
-    PetscBool identical = PETSC_FALSE;
-    PetscArraycmp(m_z_coarse.data(), m_z_fine.data(), m_z_fine.size(), &identical);
-
-    if (identical == PETSC_TRUE) {
-      m_identical_grids = static_cast<bool>(identical);
-      return;
-    }
-  }
 
   // coarse -> fine
   m_coarse2fine = init_interpolation_indexes(m_z_coarse, m_z_fine);
