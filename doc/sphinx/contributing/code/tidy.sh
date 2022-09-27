fc318d357c82156b392d31950fcc1634d724e280
Constantine Khrulev <ckhroulev@alaska.edu>
2022-09-27 11:55:40 -0800

    Remove a readability check from clang-tidy

--- doc/sphinx/contributing/code/tidy.sh
+++ doc/sphinx/contributing/code/tidy.sh
@@ -9,8 +9,5 @@ build_dir=~/local/build/pism
 # Extract compiler options specifying MPI's include directories
 mpi=$(mpicc -show | grep -E -o -e "-I[^ ]+")
 
-# Requested checks
-checks="bugprone*,clang-analyzer*,mpi*,performance*,portability*,readability*,-readability-isolate-declaration"
-
 # Run clang-tidy
-clang-tidy --extra-arg=${mpi} -p ${build_dir} --checks=${checks} $@
+clang-tidy --extra-arg=${mpi} -p ${build_dir} $@
