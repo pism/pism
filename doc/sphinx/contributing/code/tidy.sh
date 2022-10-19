c99425afcf9d758c172f87685a2806f800355ce8
Constantine Khrulev <ckhroulev@alaska.edu>
2022-10-18 16:02:40 -0800

    Improve tidy.sh

--- doc/sphinx/contributing/code/tidy.sh
+++ doc/sphinx/contributing/code/tidy.sh
@@ -3,11 +3,11 @@
 set -e
 set -u
 
-# PISM's build directory
-build_dir=~/local/build/pism
+build_dir=${1:?ERROR: PISM build directory not specified.}
+input_file=${2:?ERROR: Input file not specified.}
 
 # Extract compiler options specifying MPI's include directories
 mpi=$(mpicc -show | grep -E -o -e "-I[^ ]+")
 
 # Run clang-tidy
-clang-tidy --extra-arg=${mpi} -p ${build_dir} $@
+clang-tidy --extra-arg=${mpi} -p ${build_dir} ${input_file}
