1fbc4073a2b03227961d1a34d9fba829abc3bc31
Constantine Khrulev <ckhroulev@alaska.edu>
2023-11-07 09:26:17 -0900

    Use ALBMAPv1 instead of the SeaRISE-Antarctica dataset

--- examples/searise-antarctica/preprocess.sh
+++ examples/searise-antarctica/preprocess.sh
@@ -1,63 +1,72 @@
 #!/bin/bash
+# Copyright (C) 2009-2018, 2020, 2023 PISM authors
+#
+# downloads the ALBMAPv1 dataset and prepares it for PISM
+#
+# depends on wget, unzip, NCO (ncap2, ncks)
+#
+# Reference:
+#
+# A. M. L. Brocq, A. J. Payne, and A. Vieli, “An improved Antarctic dataset for high
+# resolution numerical ice sheet models (ALBMAP v1),” Earth System Science Data, vol. 2,
+# no. 2, pp. 247–260, Oct. 2010, doi: 10.5194/essd-2-247-2010.
+#
+# Dataset DOI: 10.1594/PANGAEA.734145
 
-# Copyright (C) 2009-2018, 2020  PISM authors
+# exit on error
+set -e
+# stop if a variable is not defined
+set -u
 
-# downloads development version of SeaRISE "Present Day Antarctica" master
-# dataset NetCDF file, adjusts metadata, breaks up, saves under new names,
-# ready for PISM
+DATANAME=ALBMAPv1.nc
 
-# depends on wget and NCO (ncrename, ncap2, ncatted, ncpdq, ncks)
-
-set -e  # exit on error
-
-# get file; see page http://websrv.cs.umt.edu/isis/index.php/Present_Day_Antarctica
-DATAVERSION=dev1.0
-DATANAME=Antarctica_5km_$DATAVERSION.nc
 echo
-echo "downloading master $DATANAME ..."
-wget -nc http://websrv.cs.umt.edu/isis/images/4/4d/$DATANAME
+echo "downloading '$DATANAME'..."
+wget -nc https://store.pangaea.de/Publications/LeBrocq_et_al_2010/$DATANAME.zip
+unzip -o $DATANAME.zip
 
-echo "making PISM-readable file by copying parts of $DATANAME"
+echo "making a PISM-readable file by copying parts of $DATANAME"
 echo "  and adjusting metadata ..."
-PISMVERSION=pism_Antarctica_5km.nc
-cp $DATANAME $PISMVERSION
 
-# following use NCO (http://nco.sourceforge.net/)
-# rename dimensions
-ncrename -O -v x1,x -v y1,y -d x1,x -d y1,y $PISMVERSION
-ncrename -O -v time,t -d time,t $PISMVERSION
-# fix polar stereographic parameter
-ncatted -O -a standard_parallel,mapping,m,d,-71.0 $PISMVERSION
-# rename usurf for convenience
-ncrename -O -v usrf,usurf $PISMVERSION
-# fix surface temperature name and make K
-ncap2 -O -s "air_temp=temp+273.15" $PISMVERSION $PISMVERSION
-ncatted -O -a units,air_temp,m,c,"K" $PISMVERSION
-# choose Van de Berg et al version of accumulation; will treat as
-# ice-equivalent snow rate and convert from an ice-equivalent
-# thickness rate ("m year-1") to "kg m-2 year-1" by assuming ice
-# density of 910 kg m-3
-ncap2 -O -s "precipitation=accr*910.0" $PISMVERSION $PISMVERSION
-ncatted -O -a units,precipitation,m,c,"kg m-2 year-1" $PISMVERSION
-# use bheatflx_shapiro as the default bheatflx data
-ncrename -O -v bheatflx_shapiro,bheatflx $PISMVERSION
-ncatted -O -a units,bheatflx,m,c,"W m-2" $PISMVERSION
-# delete incorrect standard_name attribute from bheatflx; there is no known standard_name
-ncatted -O -a standard_name,bheatflx,d,, $PISMVERSION
-# keep only the fields we actually use at bootstrapping
-ncks -O -v x,y,lat,lon,bheatflx,topg,thk,precipitation,air_temp,mapping \
-     $PISMVERSION $PISMVERSION
-# remove the time dimension
-ncwa -O -a t $PISMVERSION $PISMVERSION
-# remove the time (t) coordinate variable
-ncks -O -x -v t $PISMVERSION $PISMVERSION
-# generate the maximum extent mask
-ncap2 -A \
-      -s 'land_ice_area_fraction_retreat=0 * thk' \
-      -s 'where(thk > 0 || topg > 0) land_ice_area_fraction_retreat=1' \
-      -s 'land_ice_area_fraction_retreat@units="1"' \
-      $PISMVERSION $PISMVERSION
-ncatted -a standard_name,land_ice_area_fraction_retreat,d,, $PISMVERSION
+PISMVERSION=pism_Antarctica_5km.nc
+
+script='
+thk=usrf-lsrf;
+thk@units="meter";
+thk@long_name="ice thickness";
+thk@standard_name="land_ice_thickness";
+
+// choose Van de Berg et al version of accumulation; will treat as
+// ice-equivalent snow rate and convert from an ice-equivalent
+// thickness rate ("m year-1") to "kg m-2 year-1" by assuming ice
+// density of 918 kg m-3 (see the reference)
+precipitation[$y1, $x1]=0.0;
+where(accr > -9999) precipitation=accr*918.0;
+precipitation@units="kg m-2 year-1";
+precipitation@long_name = "precipitation";
+
+air_temp=temp;
+air_temp@units="Celsius";
+
+where(topg == -9999) topg = -5300;
+
+// Use the Shapiro & Ritzwoller geothermal flux
+bheatflx=ghfsr;
+bheatflx@units="mW m-2";
+
+land_ice_area_fraction_retreat[$y1, $x1]=0;
+where(thk > 0 || topg > 0) land_ice_area_fraction_retreat=1;
+land_ice_area_fraction_retreat@long_name="maximum ice extent mask";
+land_ice_area_fraction_retreat@units="1";
+
+// The ALBMAPv1 paper states that the dataset uses the EIGEN-GL04C geoid, but unfortunately
+// it is not easy to use. Hopefully it is okay to use WGS84 instead.
+global@proj = "EPSG:3031";
+'
+
+ncap2 -O --script "$script" $DATANAME $PISMVERSION
+# remove unneeded variables
+ncks -O -v thk,topg,precipitation,air_temp,bheatflx,land_ice_area_fraction_retreat $PISMVERSION $PISMVERSION
 
 echo "  PISM-readable file $PISMVERSION created; only has fields"
 echo "    used in bootstrapping."
