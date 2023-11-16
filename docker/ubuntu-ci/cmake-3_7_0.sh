e219ecf0438d20dca57a91b3a0070128336677ca
Constantine Khrulev <ckhroulev@alaska.edu>
2023-11-16 08:12:05 -0900

    Update the Docker image used for testing
    - Switch to Ubuntu 22.04 LTS
    - Switch to the current PETSc version (3.20.1)
    - Add more compiler versions (GCC 9, 10, 11, 12 and Clang 11, 12, 13, 14, 15)

--- docker/ubuntu-ci/cmake-3_7_0.sh
+++ docker/ubuntu-ci/cmake-3_7_0.sh
@@ -20,6 +20,10 @@ tar xzf cmake-${version}.tar.gz
 
 cd cmake-${version}
 
+# Use GCC 10 to build old CMake: it requires modifications (adding #include <limits>) to
+# build with more recent versions.
+export CC=gcc-10
+export CXX=g++-10
 ./configure --prefix=${prefix}
 
 make -j8 all
