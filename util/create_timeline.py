d590abd2436121acf02e41ebde86c4c254aa562e
Andy Aschwanden <andy.aschwanden@alaska.edu>
2023-11-17 14:32:46 -0900

    Remove netCDF4python dependency
    We switch to xarray instead. This also makes the code shorter.

--- util/create_timeline.py
+++ util/create_timeline.py
@@ -16,21 +16,14 @@
 # \verbatim $ create_timeline.py --start_date '1991-1-1' -end_date '2001-1-1'
 # time_1991-2000.nc \endverbatim
 
-import os
 from argparse import ArgumentParser
+from datetime import datetime
+
+import cftime
+import numpy as np
+import xarray as xr
 from dateutil import rrule
 from dateutil.parser import parse
-from datetime import datetime
-import time
-import numpy as np
-
-try:
-    import netCDF4 as netCDF
-except:
-    print("netCDF4 is not installed!")
-    sys.exit(1)
-NC = netCDF.Dataset
-import cftime
 
 # Set up the option parser
 parser = ArgumentParser()
@@ -54,10 +47,18 @@ parser.add_argument(
     default="monthly",
 )
 parser.add_argument(
-    "-a", "--start_date", dest="start_date", help="""Start date in ISO format. Default=1989-1-1""", default="1989-1-1"
+    "-a",
+    "--start_date",
+    dest="start_date",
+    help="""Start date in ISO format. Default=1980-1-1""",
+    default="1980-1-1",
 )
 parser.add_argument(
-    "-e", "--end_date", dest="end_date", help="""End date in ISO format. Default=2012-1-1""", default="2012-1-1"
+    "-e",
+    "--end_date",
+    dest="end_date",
+    help="""End date in ISO format. Default=2020-1-1""",
+    default="2020-1-1",
 )
 parser.add_argument(
     "-i",
@@ -71,12 +72,16 @@ parser.add_argument(
     "-u",
     "--ref_unit",
     dest="ref_unit",
-    help="""Reference unit. Default=days. Use of months or
+    help="""Reference unit. Default=seconds. Use of months or
                     years is NOT recommended.""",
-    default="days",
+    default="seconds",
 )
 parser.add_argument(
-    "-d", "--ref_date", dest="ref_date", help="""Reference date. Default=1960-1-1""", default="1960-1-1"
+    "-d",
+    "--ref_date",
+    dest="ref_date",
+    help="""Reference date. Default=1980-1-1""",
+    default="1980-1-1",
 )
 
 options = parser.parse_args()
@@ -89,13 +94,10 @@ ref_date = options.ref_date
 args = options.FILE
 infile = args[0]
 
-# Check if file exists. If True, append, otherwise create it.
-if os.path.isfile(infile):
-    nc = NC(infile, "a")
-else:
-    nc = NC(infile, "w", format="NETCDF3_CLASSIC")
-
-time_units = "%s since %s" % (ref_unit, ref_date)
+"""
+Generate offset file using xarray
+"""
+time_units = f"{ref_unit} since {ref_date}"
 calendar = options.calendar
 
 # create a dictionary so that we can supply the periodicity as a
@@ -119,46 +121,41 @@ refdate = datetime(int(r[0]), int(r[1]), int(r[2]))
 bnds_datelist = list(rrule.rrule(prule, dtstart=start_date, until=end_date))
 
 # calculate the days since refdate, including refdate, with time being the
-bnds_interval_since_refdate = cftime.date2num(bnds_datelist, time_units, calendar=calendar)
+bnds_interval_since_refdate = cftime.date2num(
+    bnds_datelist, time_units, calendar=calendar
+)
 if interval_type == "mid":
     # mid-point value:
     # time[n] = (bnds[n] + bnds[n+1]) / 2
-    time_interval_since_refdate = bnds_interval_since_refdate[0:-1] + np.diff(bnds_interval_since_refdate) / 2
+    time_interval_since_refdate = (
+        bnds_interval_since_refdate[0:-1] + np.diff(bnds_interval_since_refdate) / 2
+    )
 elif interval_type == "start":
     time_interval_since_refdate = bnds_interval_since_refdate[:-1]
 else:
     time_interval_since_refdate = bnds_interval_since_refdate[1:]
 
-# create a new dimension for bounds only if it does not yet exist
-time_dim = "time"
-if time_dim not in list(nc.dimensions.keys()):
-    nc.createDimension(time_dim)
+time_array = np.array(time_interval_since_refdate)
+time_bounds = np.zeros((len(time_array), 2))
+time_bounds[:, 0] = bnds_interval_since_refdate[0:-1]
+time_bounds[:, 1] = bnds_interval_since_refdate[1::]
 
-# create a new dimension for bounds only if it does not yet exist
-bnds_dim = "nb2"
-if bnds_dim not in list(nc.dimensions.keys()):
-    nc.createDimension(bnds_dim, 2)
-
-# variable names consistent with PISM
-time_var_name = "time"
-bnds_var_name = "time_bnds"
-
-# create time variable
-time_var = nc.createVariable(time_var_name, "d", dimensions=(time_dim))
-time_var[:] = time_interval_since_refdate
-time_var.bounds = bnds_var_name
-time_var.units = time_units
-time_var.calendar = calendar
-time_var.standard_name = time_var_name
-time_var.axis = "T"
-
-# create time bounds variable
-time_bnds_var = nc.createVariable(bnds_var_name, "d", dimensions=(time_dim, bnds_dim))
-time_bnds_var[:, 0] = bnds_interval_since_refdate[0:-1]
-time_bnds_var[:, 1] = bnds_interval_since_refdate[1::]
-
-# writing global attributes
-script_command = " ".join([time.ctime(), ":", __file__.split("/")[-1], " ".join([str(x) for x in args])])
-nc.history = script_command
-nc.Conventions = "CF 1.5"
-nc.close()
+ds = xr.Dataset(
+    data_vars=dict(  # pylint: disable=R1735
+        time_bounds=(["time", "bnds"], time_bounds, {"_FillValue": False}),
+    ),
+    coords=dict(  # pylint: disable=R1735
+        time=(
+            "time",
+            time_array,
+            {
+                "units": time_units,
+                "axis": "T",
+                "calendar": calendar,
+                "bounds": "time_bounds",
+                "_FillValue": False,
+            },
+        )
+    ),
+)
+ds.to_netcdf(infile)
