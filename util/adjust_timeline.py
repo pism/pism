2817773ab1a0367f37a81eec343e1222ae6505c2
Andy Aschwanden <andy.aschwanden@alaska.edu>
2023-11-22 10:48:42 -0900

    converted scripts from netcdf4python to xarray
    Dealing with the time axis in xarray can be a challenge, in particular when
    we need to use cftime. There are always rounding issues of the form
    date "1980-01-01 00:00:0.0000006
    or similar. Ideally we write some test scripts.

--- util/adjust_timeline.py
+++ util/adjust_timeline.py
@@ -14,21 +14,14 @@
 # \verbatim $ adjust_timeline.py --start_date '1991-1-1'
 # time_1991-2000.nc \endverbatim
 
-import os
 from argparse import ArgumentParser
-from dateutil import rrule
-from dateutil.parser import parse
 from datetime import datetime
-import time
-import numpy as np
+from pathlib import Path
 
-try:
-    import netCDF4 as netCDF
-except:
-    print("netCDF4 is not installed!")
-    sys.exit(1)
-NC = netCDF.Dataset
 import cftime
+import numpy as np
+import xarray as xr
+from dateutil.parser import parse
 
 # Set up the option parser
 parser = ArgumentParser()
@@ -40,15 +33,15 @@ parser.add_argument(
     "-p",
     "--periodicity",
     dest="periodicity",
-    help="""periodicity, e.g. monthly, daily, etc. Default=monthly""",
-    default="monthly",
+    help="""periodicity, e.g. monthly, daily, etc. Default=MS""",
+    default="MS",
 )
 parser.add_argument(
     "-a",
     "--start_date",
     dest="start_date",
-    help="""Start date in ISO format. Default=1989-1-1""",
-    default="1989-1-1",
+    help="""Start date in ISO format. Default=1980-1-1""",
+    default="1980-1-1",
 )
 parser.add_argument(
     "-c",
@@ -72,7 +65,7 @@ parser.add_argument(
     dest="ref_unit",
     help="""Reference unit. Default=days. Use of months or
                     years is NOT recommended.""",
-    default="days",
+    default="seconds",
 )
 parser.add_argument(
     "-d",
@@ -89,87 +82,66 @@ start_date = parse(options.start_date)
 ref_unit = options.ref_unit
 ref_date = options.ref_date
 args = options.FILE
-infile = args[0]
+infile = Path(args[0])
 
-time1 = time.time()
-
-
-nc = NC(infile, "a")
-nt = len(nc.variables["time"])
+time_var_name = "time"
+time_bnds_var_name = "time_bounds"
+bnds_var_name = "bnds"
 
 time_units = "%s since %s" % (ref_unit, ref_date)
 calendar = options.calendar
 
-# create a dictionary so that we can supply the periodicity as a
-# command-line argument.
-pdict = {}
-pdict["SECONDLY"] = rrule.SECONDLY
-pdict["MINUTELY"] = rrule.MINUTELY
-pdict["HOURLY"] = rrule.HOURLY
-pdict["DAILY"] = rrule.DAILY
-pdict["WEEKLY"] = rrule.WEEKLY
-pdict["MONTHLY"] = rrule.MONTHLY
-pdict["YEARLY"] = rrule.YEARLY
-prule = pdict[periodicity]
+with xr.open_dataset(infile) as ds:
+    nt = len(ds[time_var_name])
 
+date_range = xr.cftime_range(
+    start_date, periods=nt + 1, freq=periodicity, calendar=calendar
+)
+dates = np.array(cftime.date2num(date_range, time_units, calendar=calendar))
 # reference date from command-line argument
 r = time_units.split(" ")[2].split("-")
 refdate = datetime(int(r[0]), int(r[1]), int(r[2]))
 
-# create list with dates from start_date for nt counts
-# periodicity prule.
-bnds_datelist = list(rrule.rrule(prule, dtstart=start_date, count=nt + 1))
-
-# calculate the days since refdate, including refdate, with time being the
-bnds_interval_since_refdate = cftime.date2num(bnds_datelist, time_units, calendar=calendar)
 if interval_type == "mid":
     # mid-point value:
     # time[n] = (bnds[n] + bnds[n+1]) / 2
-    time_interval_since_refdate = bnds_interval_since_refdate[0:-1] + np.diff(bnds_interval_since_refdate) / 2
+    time = dates[0:-1] + (dates[1::] - dates[0:-1]) / 2
 elif interval_type == "start":
-    time_interval_since_refdate = bnds_interval_since_refdate[:-1]
+    time = dates[0:-1]
 else:
-    time_interval_since_refdate = bnds_interval_since_refdate[1:]
+    time = dates[1::]
 
-# create a new dimension for bounds only if it does not yet exist
-time_dim = "time"
-if time_dim not in list(nc.dimensions.keys()):
-    nc.createDimension(time_dim)
+time_bounds = np.zeros((nt, 2))
+time_bounds[:, 0] = dates[0:-1]
+time_bounds[:, 1] = dates[1::]
 
-# create a new dimension for bounds only if it does not yet exist
-bnds_dim = "nb2"
-if bnds_dim not in list(nc.dimensions.keys()):
-    nc.createDimension(bnds_dim, 2)
+time = np.floor(time)
+time_bounds = np.floor(time_bounds)
 
-# variable names consistent with PISM
-time_var_name = "time"
-bnds_var_name = "time_bnds"
-
-# create time variable
-if time_var_name not in nc.variables:
-    time_var = nc.createVariable(time_var_name, "d", dimensions=(time_dim))
+ds[time_var_name] = (
+    [time_var_name],
+    time,
+    {
+        "bounds": time_bnds_var_name,
+        "_FillValue": False,
+        "units": time_units,
+        "calendar": calendar,
+        "axis": "T",
+        "long_name": "time",
+        "standard_name": "time",
+    },
+)
+if hasattr(ds[time_var_name], "bounds"):
+    bounds_var = ds[time_var_name].bounds
+    ds[bounds_var] = (
+        [time_var_name, bnds_var_name],
+        time_bounds,
+        {"_FillValue": False},
+    )
 else:
-    time_var = nc.variables[time_var_name]
-time_var[:] = time_interval_since_refdate
-time_var.bounds = bnds_var_name
-time_var.units = time_units
-time_var.calendar = calendar
-time_var.standard_name = time_var_name
-time_var.axis = "T"
-
-# create time bounds variable
-if bnds_var_name not in nc.variables:
-    time_bnds_var = nc.createVariable(bnds_var_name, "d", dimensions=(time_dim, bnds_dim))
-else:
-    time_bnds_var = nc.variables[bnds_var_name]
-time_bnds_var[:, 0] = bnds_interval_since_refdate[0:-1]
-time_bnds_var[:, 1] = bnds_interval_since_refdate[1::]
-
-# writing global attributes
-script_command = " ".join([time.ctime(), ":", __file__.split("/")[-1], " ".join([str(x) for x in args])])
-nc.history = script_command
-nc.Conventions = "CF 1.5"
-nc.close()
-
-time2 = time.time()
-print("adjust_timeline.py took {:2.1f}s".format(time2 - time1))
+    ds[time_bnds_var_name] = (
+        [time_var_name, bnds_var_name],
+        time_bounds,
+        {"_FillValue": False},
+    )
+ds.to_netcdf(infile, mode="a")
