51acac342b5d4097c1696fe6d7c73f8f450fcbf0
Constantine Khrulev <ckhroulev@alaska.edu>
2024-06-04 12:13:38 -0800

    Add scripts that build YAXT and YAC

---
+++ doc/sphinx/installation/code/yac.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -u
+set -e
+set -x
+
+rm -rf yac
+
+# manual-begin
+version=3.2.0_a_p2
+prefix=$HOME/local/yac
+
+git clone -b release-${version} https://gitlab.dkrz.de/dkrz-sw/yac.git
+
+cd yac
+
+autoreconf -i
+
+export CC=mpicc FC=mpifort CFLAGS="-O3 -g -march=native"
+
+./configure --prefix=${prefix} \
+            --with-yaxt-root=${prefix} \
+            --disable-netcdf \
+            --disable-examples \
+            --disable-tools \
+            --with-pic
+
+make all && make install
+# manual-end
