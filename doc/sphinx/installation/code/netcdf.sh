bab5ed6c25b4af6def0179298ff692a391594dfe
Constantine Khrulev <ckhroulev@alaska.edu>
2024-12-05 12:18:03 -0900

    Use the latest NetCDF version (4.9.2)

--- doc/sphinx/installation/code/netcdf.sh
+++ doc/sphinx/installation/code/netcdf.sh
@@ -9,7 +9,7 @@ set -x
 
 hdf5=~/local/hdf5
 
-version=4.7.4
+version=4.9.2
 prefix=$HOME/local/netcdf
 build_dir=~/local/build/netcdf
 url=https://github.com/Unidata/netcdf-c/archive/refs/tags/v${version}.tar.gz
@@ -25,7 +25,7 @@ pushd netcdf-c-${version}
 export CFLAGS="-g -O0"
 unset CFLAGS
 
-CC=mpicc CPPFLAGS=-I${hdf5}/include LDFLAGS=-L${hdf5}/lib ./configure \
+./configure CC=mpicc CPPFLAGS=-I${hdf5}/include LDFLAGS=-L${hdf5}/lib \
         --enable-netcdf4 \
         --disable-dap \
         --prefix=${prefix} 2>&1 | tee netcdf_configure.log
