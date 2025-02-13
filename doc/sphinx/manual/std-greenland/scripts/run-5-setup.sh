4dc83a89a435295f46d8d5175850d84c836484a7
Constantine Khrulev <ckhroulev@alaska.edu>
2025-02-13 09:39:01 -0900

    Add missing "." in "ln -s" commands

--- doc/sphinx/manual/std-greenland/scripts/run-5-setup.sh
+++ doc/sphinx/manual/std-greenland/scripts/run-5-setup.sh
@@ -3,8 +3,8 @@
 cd examples/std-greenland/           # g10km_gridseq.nc should be in this directory
 mkdir paramstudy
 cd paramstudy
-ln -s ../g10km_gridseq.nc            # these four lines make links to ...
-ln -s ../pism_Greenland_5km_v1.1.nc  #
-ln -s ../spinup.sh                   #
-ln -s ../param.sh                    # ... existing files in examples/std-greenland/
+ln -s ../g10km_gridseq.nc .            # these four lines make links to ...
+ln -s ../pism_Greenland_5km_v1.1.nc .  #
+ln -s ../spinup.sh .                   #
+ln -s ../param.sh .                    # ... existing files in examples/std-greenland/
 ./param.sh
