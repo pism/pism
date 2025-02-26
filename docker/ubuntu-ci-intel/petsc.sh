c15bdaba63fab107992f366d1e06c882f9edc69d
Constantine Khrulev <ckhroulev@alaska.edu>
2025-02-26 10:26:58 -0900

    Build PETSc with petsc4py

--- docker/ubuntu-ci-intel/petsc.sh
+++ docker/ubuntu-ci-intel/petsc.sh
@@ -7,8 +7,8 @@ set -x
 # Install PETSc in /opt/petsc using /var/tmp/build/petsc as the build
 # directory.
 
-MPICC=${MPICC:-mpicc}
-MPICXX=${MPICXX:-mpicxx}
+MPICC=${MPICC:-mpiicx}
+MPICXX=${MPICXX:-mpiicx}
 COPTFLAGS=${COPTFLAGS:--O3 -march=native -mtune=native -fp-model=precise}
 
 build_dir=${build_dir:-/var/tmp/build/petsc}
@@ -16,7 +16,7 @@ prefix=${prefix:-/opt/petsc}
 
 mkdir -p ${build_dir}
 cd ${build_dir}
-version=${version:-3.21.1}
+version=${version:-3.22.2}
 
 wget -nc \
      https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-${version}.tar.gz
@@ -33,9 +33,11 @@ python3 ./configure \
         --prefix=${prefix} \
         --with-cc="${MPICC}" \
         --with-cxx="${MPICXX}" \
-        --with-fc=0 \
-        --with-shared-libraries \
         --with-debugging=0 \
+        --with-fc=0 \
+        --with-petsc4py \
+        --with-shared-libraries \
+        --with-x=0 \
         --download-f2cblaslapack
 
 make all
