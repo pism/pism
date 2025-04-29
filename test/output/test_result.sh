f9ec4afaea6e70e3e81e6defc7d3a2e76b35f35e
Constantine Khrulev <ckhroulev@alaska.edu>
2025-04-29 14:24:59 -0800

    Slightly improve tests of the output code

--- test/output/test_result.sh
+++ test/output/test_result.sh
@@ -6,4 +6,5 @@ set -e
 
 eis2_grid="-Mx 3 -My 3 -Mz 3 -Lz 5000"
 
-pism -eisII A -y 1000 ${eis2_grid} -o result.nc
+# set output.use_MKS just to exercise one more code branch
+pism -eisII A -y 1000 ${eis2_grid} -output.use_MKS true -o result.nc
