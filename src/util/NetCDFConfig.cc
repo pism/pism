9a8e6265bbffbeaa71964f0f53d21d10d61193e1
Constantine Khrulev <ckhroulev@alaska.edu>
2025-07-01 11:28:12 -0800

    Stop with an error message if the string encoding Config is too long to write

--- src/util/NetCDFConfig.cc
+++ src/util/NetCDFConfig.cc
@@ -195,13 +195,14 @@ void NetCDFConfig::read_impl(const File &file) {
   m_config_filename = file.name();
 }
 
+static const int max_length = 32768;
+
 //! Write a config variable to a file (with all its attributes).
 void NetCDFConfig::define_impl(const OutputFile &file) const {
-  int max_length = 32768;
 
   file.define_dimension("cfg", max_length);
 
-  std::vector<std::string> dims = {"cfg"};
+  std::vector<std::string> dims = { "cfg" };
 
   if (not get_string("output.experiment_id").empty()) {
     auto exp_id_name = get_string("output.experiment_id_dimension");
@@ -211,8 +212,9 @@ void NetCDFConfig::define_impl(const OutputFile &file) const {
     dims.insert(dims.cbegin(), exp_id_name);
   }
 
-  VariableMetadata config(m_data.get_name(), m_data.unit_system());
+  auto config = m_data;
   config.set_output_type(io::PISM_CHAR);
+
   file.define_variable(config, dims);
 }
 
@@ -221,6 +223,13 @@ void NetCDFConfig::write_impl(const OutputFile &file) const {
   define(file);
 
   std::string data = json();
+
+  if (data.size() + 1 > max_length) {
+    throw RuntimeError::formatted(
+        PISM_ERROR_LOCATION,
+        "unable to save configuration parameters to a file: JSON string length exceeds %d",
+        max_length);
+  }
 
   std::vector<unsigned int> start = { 0 };
   std::vector<unsigned int> count = { (unsigned int)data.size() + 1 };
