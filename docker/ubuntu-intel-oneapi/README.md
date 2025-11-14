82ae98dccdc16f37b67a2c25384914bff0830d6e
Constantine Khrulev <ckhroulev@alaska.edu>
2025-11-13 15:39:55 -0900

    Update the Dockerfile setup for the oneAPI-based container
    Now it uses the *current* PISM source code instead of cloning PISM's repository to get the
    sources.

--- docker/ubuntu-intel-oneapi/README.md
+++ docker/ubuntu-intel-oneapi/README.md
@@ -4,10 +4,11 @@ Based on Ubuntu 24.04 LTS.
 
 Uses FFTW, GSL, libfyaml, PROJ and UDUNITS from Ubuntu packages.
 
-HDF5, NetCDF, YAXT, YAC and PETSc are built from sources using Intel's
-C/C++ and Fortran compilers.
+HDF5, NetCDF (with parallel I/O support), YAXT, YAC and PETSc are built from sources using
+Intel's C/C++ and Fortran compilers.
 
-PETSc is built with 32 bit indexes, MUMPS, ScaLAPACK and Intel MKL as
-the BLAS/LAPACK implementation. See `petsc.sh` for details.
+PETSc is built with 32 bit indexes, MUMPS, ScaLAPACK and Intel MKL as the BLAS/LAPACK
+implementation. See `petsc.sh` for details.
 
-PISM is built with 
+PISM is built with PROJ, YAXT and YAC to be able to use flexible interpolation of gridded
+inputs.
--- docker/ubuntu-intel-oneapi/pism.sh
+++ docker/ubuntu-intel-oneapi/pism.sh
@@ -9,9 +9,6 @@ set -x
 N=${N:-8}
 opt_flags="-O3 -axCORE-AVX512,CORE-AVX2,SSE4.2 -fp-model=precise"
 
-# run as pism_version=v2.0 ./pism.sh to build v2.0, etc
-pism_version=${pism_version:-dev}
-
 # Prerequisites:
 export PETSC_DIR=${PETSC_DIR:-/opt/petsc}
 yaxt_prefix=${yaxt_prefix:-/opt/yaxt}
@@ -24,17 +21,13 @@ prefix=${prefix:-/opt/pism}
 build_dir=${build_dir:-/var/tmp/build/pism}
 
 mkdir -p ${build_dir}
-cd ${build_dir}
-
-git clone https://github.com/pism/pism.git src
-git -C src checkout ${pism_version}
 
 export CC=mpiicx
 export CXX=mpiicpx
 
 cmake \
-    -B ${build_dir}/build \
-    -S ${build_dir}/src \
+    -B ${build_dir} \
+    -S ${src_dir} \
     -DCMAKE_PREFIX_PATH="${yaxt_prefix};${yac_prefix};${hdf5_prefix};${netcdf_prefix}" \
     -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_CXX_FLAGS="${opt_flags} -diag-disable=cpu-dispatch,10006,2102" \
@@ -44,4 +37,4 @@ cmake \
     -DPism_USE_PARALLEL_NETCDF4=YES \
     -DPism_USE_PROJ=YES  
 
-make -j $N -C ${build_dir}/build install
+make -j $N -C ${build_dir} install
