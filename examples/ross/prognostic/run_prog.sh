45a2529825ad45bb21d7400d25ec814c3fc65a4e
Constantine Khrulev <ckhroulev@alaska.edu>
2025-12-04 10:09:41 -0900

    Add a variable to adjust KSP rtol in examples/ross/prognostic

--- examples/ross/prognostic/run_prog.sh
+++ examples/ross/prognostic/run_prog.sh
@@ -14,6 +14,8 @@ RUN_LENGTH=${RUN_LENGTH:-3000}
 
 PISMPREFIX=""
 
+KSP_RTOL=${KSP_RTOL:-1e-5}
+
 # use these for more robust SSA solves
 # STRONGKSP="-ssafd_ksp_type gmres -ssafd_ksp_norm_type unpreconditioned -ssafd_ksp_pc_side right -ssafd_pc_type asm -ssafd_sub_pc_type lu"
 # STRONGKSP=""
@@ -23,7 +25,7 @@ STARTNAME=startfile_Mx${M}.nc
 cmd_diag="mpiexec -n $N_PROC ${PISMPREFIX}pism -regional -i ../Ross_combined.nc -bootstrap -Mx $M -My $M \
   -Mz 61 -Lz 3000 -z_spacing equal -surface given -stress_balance ssa \
   -yield_stress constant -tauc 1e6 -pik -ssa_dirichlet_bc -ssa_e $SSAE \
-  $STRONGKSP -y 0.1 -ys 0.0 -o $STARTNAME -o_order zyx "
+  $STRONGKSP ${KSP_RTOL} -y 0.1 -ys 0.0 -o $STARTNAME"
 echo "running command:"
 echo
 echo "$cmd_diag"
@@ -39,7 +41,7 @@ cmd_prog="mpiexec -n $N_PROC ${PISMPREFIX}pism -regional -i $STARTNAME \
   -surface given -stress_balance ssa -yield_stress constant -tauc 1e6 -pik \
   -calving eigen_calving,thickness_calving -eigen_calving_K $ECALV -front_retreat_cfl \
   -ssa_dirichlet_bc -ssa_e $SSAE -ys 0 -y $RUN_LENGTH -o $NAME -o_size big \
-  -thickness_calving_threshold $CTHICK $STRONGKSP \
+  -thickness_calving_threshold $CTHICK $STRONGKSP ${KSP_RTOL} \
   -ts_file ts-${NAME} -ts_times 0:monthly:${RUN_LENGTH} \
   -extra_file ex-${NAME} -extra_times 0:${exdt}:${RUN_LENGTH} -extra_vars ${exvars} \
   -options_left"
