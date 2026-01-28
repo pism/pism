e1f5a0348e13d54201e0a4e6118c06fdff1dbed0
Andy Aschwanden <andy.aschwanden@gmail.com>
2026-01-28 13:53:02 -0900

    Clean up of PICO/P

--- examples/mismip_plus/pico.sh
+++ examples/mismip_plus/pico.sh
@@ -22,6 +22,8 @@ ocean_file=ocean.nc
 spatial_vars="beta,bmelt,mask,topg,usurf,thk,velsurf_mag,velbase_mag,climatic_mass_balance,taub_mag,velsurf,velbase"
 pico_spatial_vars="pico_box_mask,pico_shelf_mask,pico_contshelf_mask,pico_ice_rise_mask,pico_T_star,pico_overturning,pico_temperature,pico_salinity,pico_basal_melt_rate"
 
+ncgen -o config.nc config.cdl
+
 run_length=1s
 sb="ssa+sia"
 resolution="2km"
@@ -38,28 +40,6 @@ mpirun -np $NN pism -config_override config.nc \
        -output.sizes.medium uvel,vvel,sftgif,velsurf_mag,mask,usurf,bmelt,velbar \
        -output.extra.file spatial_$out \
        -output.extra.times 1s \
-       -output.extra.vars $pico_spatial_vars,$spatial_vars \
-       -output.file state_$out \
-       -surface.given.file $climate_file \
-       -time.start 0 \
-       -time.run_length $run_length
-
-run_length=100yr
-sb="ssa+sia"
-resolution="2km"
-out=pico_g${resolution}_${sb}_${run_length}.nc
-mpirun -np $NN pism -config_override config.nc \
-       -stress_balance.model $sb \
-       -geometry.front_retreat.prescribed.file $boot_file \
-       -grid.dx $resolution \
-       -grid.dy $resolution \
-       -input.file $infile \
-       -ocean.models pico \
-       -ocean.pico.file $ocean_file \
-       -o_size medium \
-       -output.sizes.medium uvel,vvel,sftgif,velsurf_mag,mask,usurf,bmelt,velbar \
-       -output.extra.file spatial_$out \
-       -output.extra.times 1yr \
        -output.extra.vars $pico_spatial_vars,$spatial_vars \
        -output.file state_$out \
        -surface.given.file $climate_file \
--- examples/mismip_plus/picop.sh
+++ examples/mismip_plus/picop.sh
@@ -23,6 +23,8 @@ spatial_vars="beta,bmelt,mask,topg,usurf,thk,velsurf_mag,velbase_mag,climatic_ma
 picop_spatial_vars="picop_basal_melt_rate,picop_grounding_line_slope,picop_grounding_line_elevation"
 regrid_vars="litho_temp,enthalpy,tillwat,bmelt,ice_area_specific_volume,thk"
 
+ncgen -o config.nc config.cdl
+
 run_length=1s
 sb="ssa+sia"
 resolution="2km"
@@ -39,28 +41,6 @@ mpirun -np $NN pism -config_override config.nc \
        -output.sizes.medium uvel,vvel,sftgif,velsurf_mag,mask,usurf,bmelt,velbar \
        -output.extra.file spatial_$out \
        -output.extra.times 1s \
-       -output.extra.vars $picop_spatial_vars,$spatial_vars \
-       -output.file state_$out \
-       -surface.given.file $climate_file \
-       -time.start 0 \
-       -time.run_length $run_length
-
-run_length=100yr
-sb="ssa+sia"
-resolution="2km"
-out=picop_g${resolution}_${sb}_${run_length}.nc
-mpirun -np $NN pism -config_override config.nc \
-       -stress_balance.model $sb \
-       -geometry.front_retreat.prescribed.file $boot_file \
-       -grid.dx $resolution \
-       -grid.dy $resolution \
-       -input.file $infile \
-       -ocean.models picop \
-       -ocean.pico.file $ocean_file \
-       -o_size medium \
-       -output.sizes.medium uvel,vvel,sftgif,velsurf_mag,mask,usurf,bmelt,velbar \
-       -output.extra.file spatial_$out \
-       -output.extra.times 1yr \
        -output.extra.vars $picop_spatial_vars,$spatial_vars \
        -output.file state_$out \
        -surface.given.file $climate_file \
