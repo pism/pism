05abc9682ac5b3ab763efcb44cbf85566a52512e
Constantine Khrulev <ckhroulev@alaska.edu>
2026-02-06 15:10:46 -0900

    Automate running the regression test of asynchronous output

--- test/regression/async_io/run_test.sh
+++ test/regression/async_io/run_test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-pism_dir=${pism_dir:-$HOME/github/pism/pism}
+set -e
+set -u
+set -x
 
-run="python3"
-# run="coverage run"
+#################################################
+# Test asynchronous output using YAC-based code #
+#################################################
+
+PISM_PATH=$1
+MPIEXEC=$2
+PISM_SOURCE_DIR=$3
+PYTHON=$5
 
 # Note: async_io_test.py should use "-n X" for some X > 1. This is needed to make sure
-# that asynchronous_output_server.py uses the code assembling parts of the grid received
-# from different ranks (on the PISM side).
-mpirun -n 3 python3 ./async_io_test.py ${arg:-} :\
-       -n 1 ${run} ${pism_dir}/util/pism_async_writer -d
-
-# coverage html
+# that pism_async_writer uses the code assembling parts of the grid received from
+# different ranks (on the PISM side).
+${MPIEXEC} -n 3 ${PYTHON} ${PISM_SOURCE_DIR}/test/regression/async_io/async_io_test.py ${arg:-} :\
+           -n 1 ${PYTHON} ${PISM_SOURCE_DIR}/util/pism_async_writer -d
