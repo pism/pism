9539931f4832a016566e5a15b660bc07f09e987f
Andy Aschwanden <andy.aschwanden@gmail.com>
2026-02-25 15:24:09 -0900

    Fixed tidy script
    Fixed an issue with mpi and arguments expanding.

--- doc/sphinx/contributing/code/tidy.sh
+++ doc/sphinx/contributing/code/tidy.sh
@@ -6,8 +6,9 @@ set -u
 build_dir=${1:?ERROR: PISM build directory not specified.}
 input_file=${2:?ERROR: Input file not specified.}
 
-# Extract compiler options specifying MPI's include directories
-mpi=$(mpicc -show | grep -E -o -e "-I[^ ]+")
+# Extract compiler options specifying MPI's include directories and
+# pass each one as a separate --extra-arg to clang-tidy
+extra_args=$(mpicc -show | grep -E -o -e "-I[^ ]+" | sed 's/^/--extra-arg=/')
 
 # Run clang-tidy
-clang-tidy --extra-arg=${mpi} -p ${build_dir} ${input_file}
+clang-tidy ${extra_args} -p ${build_dir} ${input_file}
