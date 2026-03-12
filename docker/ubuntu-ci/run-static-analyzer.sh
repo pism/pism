dfd344c5f5d7826845498c66ab16b8423d107c11
Constantine Khrulev <ckhroulev@alaska.edu>
2026-03-12 12:33:36 -0800

    Add a script running Clang's static analyzer in a Docker container
    We run this check using CircleCI; this script allows us to run it locally.

---
+++ docker/ubuntu-ci/run-static-analyzer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+pism_dir=${pism_dir:?Please set pism_dir}
+
+container_pism_dir=/home/worker/project
+
+cmd="cd ${container_pism_dir}/docker/ubuntu-ci/ && ./static-analyzer.sh"
+
+docker run --rm -it \
+       -v ${pism_dir}:${container_pism_dir} \
+       -e N=${N:-8} \
+       -e source_dir=${container_pism_dir} \
+       ckhrulev/pism-ubuntu:0.1.16 \
+       bash -c "${cmd}"
