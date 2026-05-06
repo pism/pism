dbde0a21c270f38b25fd373485797aa7625c5521
Constantine Khrulev <ckhroulev@alaska.edu>
2026-05-06 09:31:46 -0800

    Update Docker image version in docker/ubuntu-ci/build-and-test.sh

--- docker/ubuntu-ci/build-and-test.sh
+++ docker/ubuntu-ci/build-and-test.sh
@@ -48,5 +48,5 @@ docker run --rm -it \
        -e N=${N} \
        -e source_dir=${container_pism_dir} \
        -e target="${target}" \
-       ckhrulev/pism-ubuntu:0.1.17 \
+       ckhrulev/pism-ubuntu:0.1.18 \
        bash -c "${cmd}"
