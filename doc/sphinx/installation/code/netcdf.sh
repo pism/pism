3a1f113eb3b39bc676b7d68bea589d442e5f5c64
Constantine Khrulev <ckhroulev@alaska.edu>
2026-05-06 09:30:49 -0800

    Install all prerequisites in $HOME/local/

--- doc/sphinx/installation/code/netcdf.sh
+++ doc/sphinx/installation/code/netcdf.sh
@@ -11,7 +11,7 @@ set -x
 lib_dir=${lib_dir:-$HOME/local}
 
 version=4.9.3
-prefix=${prefix:-$HOME/local/netcdf}
+prefix=${prefix:-$HOME/local/}
 build_dir=${build_dir:-$HOME/local/build/netcdf}
 url=https://github.com/Unidata/netcdf-c/archive/refs/tags/v${version}.tar.gz
 
