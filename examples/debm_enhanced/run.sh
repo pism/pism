333f966d1154d930dcab6f98dfa010b37314c6da
Andy Aschwanden <andy.aschwanden@gmail.com>
2026-06-17 15:31:14 -0800

    Fixed test script params

--- examples/debm_enhanced/run.sh
+++ examples/debm_enhanced/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in bootfile_RGI2000-v7.0-C-01-04374.nc era5_wgs84_RGI2000-v7.0-C-01-04374.nc insolation_RGI2000-v7.0-C-01-04374.nc; do
+for f in bootfile_RGI2000-v7.0-C-01-04374.nc era5_wgs84_RGI2000-v7.0-C-01-04374.nc; do
     wget -nc https://pism-cloud-data.s3.amazonaws.com/debm_enhanced/$f;
 done
 
@@ -22,7 +22,8 @@ mpirun -np 8 pism -i bootfile_RGI2000-v7.0-C-01-04374.nc \
        -time.calendar standard \
        -output.spatial.file pdd.nc \
        -output.spatial.vars mass_fluxes,pdd_fluxes,effective_air_temp,effective_precipitation,climatic_mass_balance \
-       -output.spatial.times yearly
+       -output.spatial.times weekly
+cdo yearmean pdd.nc pdd_ym.nc 
 
 mpirun -np 8 pism -i bootfile_RGI2000-v7.0-C-01-04374.nc \
        -input.bootstrap yes  \
@@ -50,36 +51,8 @@ mpirun -np 8 pism -i bootfile_RGI2000-v7.0-C-01-04374.nc \
        -time.calendar standard \
        -output.spatial.file debm_simple.nc \
        -output.spatial.vars mass_fluxes,pdd_fluxes,effective_air_temp,effective_precipitation,climatic_mass_balance,insolation \
-       -output.spatial.times yearly
-
-
-mpirun -np 8 pism -i bootfile_RGI2000-v7.0-C-01-04374.nc \
-       -energy.model none \
-       -grid.dx 500m \
-       -grid.dy 500m \
-       -input.bootstrap yes  \
-       -input.forcing.time_extrapolation yes \
-       -stress_balance.model none \
-       -atmosphere.models given \
-       -atmosphere.given.file era5_wgs84_RGI2000-v7.0-C-01-04374.nc \
-       -atmosphere.given.periodic yes \
-       -surface debm_enhanced \
-       -surface.debm_enhanced.file insolation_RGI2000-v7.0-C-01-04374.nc \
-       -surface.debm_simple.std_dev.file era5_wgs84_RGI2000-v7.0-C-01-04374.nc \
-       -surface.debm_simple.std_dev.periodic yes \
-       -surface.debm_simple.interpret_precip_as_snow no \
-       -surface.debm_simple.c1 25 \
-       -surface.debm_simple.c2 -103 \
-       -surface.debm_simple.refreeze  0.50 \
-       -surface.debm_simple.refreeze_ice_melt no \
-       -surface.debm_simple.air_temp_all_precip_as_snow 271.65 \
-       -surface.debm_simple.air_temp_all_precip_as_rain 275.0\
-       -time.start 2000-01-01 \
-       -time.end 2001-01-01 \
-       -time.calendar standard \
-       -output.spatial.file debm_enhanced_solshade.nc \
-       -output.spatial.vars mass_fluxes,pdd_fluxes,effective_air_temp,effective_precipitation,climatic_mass_balance,insolation \
-       -output.spatial.times yearly
+       -output.spatial.times weekly
+cdo yearmean debm_simple.nc debm_simple_ym.nc
 
 for df in 0 0.2 0.4; do
 mpirun -np 8 pism -i bootfile_RGI2000-v7.0-C-01-04374.nc \
@@ -92,7 +65,7 @@ mpirun -np 8 pism -i bootfile_RGI2000-v7.0-C-01-04374.nc \
        -atmosphere.models given \
        -atmosphere.given.file era5_wgs84_RGI2000-v7.0-C-01-04374.nc \
        -atmosphere.given.periodic yes \
-       -surface debm_enhanced \
+       -surface debm_enhanced,elevation_change \
        -surface.debm_enhanced.diffuse_fraction $df \
        -surface.debm_simple.std_dev.file era5_wgs84_RGI2000-v7.0-C-01-04374.nc \
        -surface.debm_simple.std_dev.periodic yes \
@@ -108,5 +81,6 @@ mpirun -np 8 pism -i bootfile_RGI2000-v7.0-C-01-04374.nc \
        -time.calendar standard \
        -output.spatial.file debm_enhanced_df_${df}.nc \
        -output.spatial.vars mass_fluxes,pdd_fluxes,effective_air_temp,effective_precipitation,climatic_mass_balance,insolation \
-       -output.spatial.times yearly
+       -output.spatial.times weekly
+cdo yearmean debm_enhanced_df_${df}.nc debm_enhanced_df_${df}_ym.nc
 done
